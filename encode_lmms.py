bb72ccf38c8d2eb4a97abd88bae65818eaac91c1
colbydray <colbydray@gmail.com>
2022-05-03 08:09:27 -0400

    Update encode_lmms.py

--- encode_lmms.py
+++ encode_lmms.py
@@ -9,6 +9,8 @@ parser.add_argument("lmmsout")
 
 args = parser.parse_args()
 
+print('--- ConvJson OTMN to LMMS---')
+
 def onetime2lmmstime(input):
 	return int(round(float(input * 48)))
 
@@ -16,6 +18,8 @@ def oneto100(input):
 	return round(float(input) * 100)
 
 def lmms_encode_notelist(xmltag, json_notelist):
+	print('\t\tNotes:', end=' ')
+	printcountpat = 0
 	for json_note in json_notelist:
 		xml_pattern = ET.SubElement(xmltag, "note")
 		key = json_note['key']
@@ -28,6 +32,9 @@ def lmms_encode_notelist(xmltag, json_notelist):
 		xml_pattern.set('pan', str(pan))
 		xml_pattern.set('len', str(int(round(duration))))
 		xml_pattern.set('vol', str(vol))
+		printcountpat += 1
+		print(str(printcountpat), end=' ')
+	print(' ')
 
 def lmms_encode_plugin(xmltag, json_singletrack):
 	pluginname = json_singletrack['instrumentdata']['plugin']
@@ -63,11 +70,15 @@ def lmms_encode_inst_track(xmltag, json_singletrack):
 	if json_singletrack['vol'] is not None:
 		xml_instrumenttrack.set('vol', str(oneto100(json_singletrack['vol'])))
 	lmms_encode_plugin(xml_instrumenttrack, json_singletrack)
+	print('Audio Track Name: ' + json_singletrack['name'])
 
 	#placements
 	json_placementlist = json_singletrack['placements']
 	tracksnum = 0
+	printcountplace = 0
 	while tracksnum <= len(json_placementlist)-1:
+		print('\tPlacement ' + str(printcountplace) + ': ')
+		printcountplace += 1
 		json_placement = json_placementlist[tracksnum-1]
 		json_notelist = json_placement['notelist']
 		xml_pattern = ET.SubElement(xmltag, "pattern")
@@ -95,16 +106,22 @@ def lmms_encode_audio_track(xmltag, json_singletrack):
 		xml_sampletrack.set('vol', str(oneto100(json_singletrack['vol'])))
 	xmltag.set('name', json_singletrack['name'])
 	xmltag.set('muted', str(json_singletrack['muted']))
+	print('Audio Track Name: ' + json_singletrack['name'])
 
 	#placements
+	printcountplace = 0
+	print('\tPlacements:', end=' ')
 	json_placementlist = json_singletrack['placements']
 	while tracksnum <= len(json_placementlist)-1:
+		printcountplace += 1
+		print(str(printcountplace), end=' ')
 		json_placement = json_placementlist[tracksnum-1]
 		xml_pattern = ET.SubElement(xmltag, "sampletco")
 		xml_pattern.set('pos', str(int(round(json_placement['position'] * 48))))
 		xml_pattern.set('len', str(int(round(json_placement['duration'] * 48))))
 		xml_pattern.set('src', str(json_placement['file']))
 		tracksnum += 1
+	print('')
 
 def lmms_encode_tracks(xmltag, json_tracks):
 	for json_singletrack in json_tracks:

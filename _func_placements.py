b4f60801d319ee6fc54cadd5681d3c470998c298
Colby Ray <109001272+colbydray1@users.noreply.github.com>
2022-08-12 07:51:27 -0400

    Add files via upload

---
+++ _func_placements.py
@@ -0,0 +1,31 @@
+import json
+
+def removewarping(song):
+	for trackdata in song['tracks']:
+		#print(trackdata['name'])
+		newplacements = []
+		for placementdata in trackdata['placements']:
+			trackposition = placementdata['position']
+			tracknotelist = placementdata['notelist']
+			#print(json.dumps(placementdata, indent=2))
+			if 'noteloop' in placementdata:
+				currentpos = 0
+				duration = placementdata['noteloop']['duration']
+				startpoint = placementdata['noteloop']['startpoint']
+				endpoint = placementdata['noteloop']['endpoint']
+				remainingduration = duration
+				print(str(duration))
+				if duration != endpoint:
+					while currentpos <= duration:
+						newplacementdata = {}
+						part_position = currentpos
+						part_duration = endpoint
+						print(str(part_position) + ' ' + str(min(part_duration,remainingduration)))
+						newplacementdata['position'] = trackposition + part_position
+						newplacementdata['notelist'] = tracknotelist
+						newplacements.append(newplacementdata)
+						remainingduration -= endpoint 
+						currentpos += endpoint
+				else:
+					newplacements.append(placementdata)
+		trackdata['placements'] = newplacements

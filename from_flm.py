7ea2f8bf154eda43c13af42b7dca5778b26397c4
Colby Ray <109001272+colbydray1@users.noreply.github.com>
2022-08-12 05:54:00 -0400

    Add files via upload

---
+++ from_flm.py
@@ -0,0 +1,126 @@
+from io import BytesIO
+
+import json
+import _func_riff
+import struct
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("flm")
+parser.add_argument("cvpj")
+args = parser.parse_args()
+
+def parse_evn2_notelist(evn2bytes):
+	notelistdata = BytesIO()
+	notelistdata.write(evn2bytes)
+	fileobject.seek(0,2)
+	notelistdata_filesize = notelistdata.tell()
+	notelistdata.seek(0)
+	something = int.from_bytes(notelistdata.read(2), "little")
+	notelist = []
+	while notelistdata.tell() < notelistdata_filesize:
+		notejson = {}
+		notejson['position'] = int.from_bytes(notelistdata.read(4), "little")/128
+		notejson['duration'] = struct.unpack('d', notelistdata.read(8))[0]
+		notejson['key'] = int.from_bytes(notelistdata.read(1), "little") - 72
+		notejson['vol'] = 1.0
+		notejson['pan'] = 0.0
+		notelistdata.read(6)
+		notelist.append(notejson)
+	#for note in notelist:
+	#	print(str(note))
+	return notelist
+
+def parse_evn2_test(evn2bytes):
+	notelistdata = BytesIO()
+	notelistdata.write(evn2bytes)
+	fileobject.seek(0,2)
+	notelistdata_filesize = notelistdata.tell()
+	notelistdata.seek(0)
+	something = int.from_bytes(notelistdata.read(2), "little")
+	while notelistdata.tell() < notelistdata_filesize:
+		print(bytes(notelistdata.read(19)).hex())
+
+fileobject = open(args.flm, 'rb')
+fileobject.seek(0,2)
+filesize = fileobject.tell()
+fileobject.seek(0)
+headername = fileobject.read(4)
+
+riffobjects = _func_riff.readriffdata_debug(fileobject, 4)
+
+tracklist = []
+
+for riffobject in riffobjects:
+	if riffobject[0] == b'HEAD':
+		headerdata = BytesIO()
+		headerdata.write(riffobject[1])
+		headerdata.seek(8)
+		songname = headerdata.read(256).decode("utf-8").split('\x00',1)[0]
+		bpm = struct.unpack('d', headerdata.read(8))[0]
+
+	if riffobject[0] == b'CHNL':
+		chnldata = _func_riff.readriffdata(riffobject[1],8)
+		placements = []
+
+		riffobjects_trkh = chnldata[1][1]
+		trkhdata = _func_riff.readriffdata(riffobjects_trkh,0)
+		for trkhobj in trkhdata:
+			print(str(trkhobj[0])+ " " + str(len(trkhobj[1])))
+			if trkhobj[0] == b'DESc':
+				TrackType = trkhobj[1][0]
+			if trkhobj[0] == b'CLIP':
+				clipobject = _func_riff.bytearray2BytesIO(trkhobj[1])
+				clipobject.seek(0)
+				notelist_position = int.from_bytes(clipobject.read(4), "little")/128
+				riff_clip_inside = _func_riff.readriffdata(clipobject,8)
+				placement_json = {}
+				for riff_clip_inside_part in riff_clip_inside:
+					if TrackType == 0:
+						if riff_clip_inside_part[0] == b'CLHd':
+							placement_json['duration'] = struct.unpack('d', riff_clip_inside_part[1][0:8])[0]
+							placement_json['endpoint'] = struct.unpack('d', riff_clip_inside_part[1][8:16])[0]
+							placement_json['startpoint'] = struct.unpack('d', riff_clip_inside_part[1][16:24])[0]
+						if riff_clip_inside_part[0] == b'EVN2':
+							placement_json['position'] = notelist_position
+							placement_json['notelist'] = parse_evn2_notelist(riff_clip_inside_part[1])
+				if TrackType != {}:
+					placements.append(placement_json)
+
+
+		riffobjects_chhd = chnldata[0][1]
+		TrackName = riffobjects_chhd.split(b'\x00' * 1)[0].decode("utf-8")
+		print('Track Name: ' + TrackName)
+		trackdata_json = {}
+		trackdata_json['type'] = "instrument"
+		
+		trackdata_json['placements'] = placements
+		if TrackType == 0:
+			trackdata_json['type'] = "instrument"
+		if TrackType == 2:
+			trackdata_json['type'] = "audio"
+		trackdata_json['name'] = TrackName
+		trackdata_json['vol'] = 1.0
+		trackdata_json['pan'] = 0.0
+		trackdata_json['muted'] = 0
+		instrumentdata_json = {}
+		instrumentdata_json['plugin'] = "orgyana"
+		trackdata_json['instrumentdata'] = instrumentdata_json
+		plugindata_json = {}
+		trackdata_json['plugindata'] = plugindata_json
+		tracklist.append(trackdata_json)
+
+
+
+
+json_root = {}
+json_root['mastervol'] = 1.0
+json_root['timesig_numerator'] = 4
+json_root['timesig_denominator'] = 4
+json_root['bpm'] = bpm
+json_root['title'] = songname
+json_root['tracks'] = tracklist
+
+with open(args.cvpj + '.cvpj', 'w') as outfile:
+    outfile.write(json.dumps(json_root, indent=2))

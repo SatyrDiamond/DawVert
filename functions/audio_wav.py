01ef6347aebb8c88336ec590b1a533a73d229bdf
Colby Ray <colbydray@gmail.com>
2022-11-30 04:44:56 -0500

    it sample fix + wav

--- functions/audio_wav.py
+++ functions/audio_wav.py
@@ -4,6 +4,56 @@
 from functions import data_bytes
 from io import BytesIO
 
+def makesmpl(instdata):
+	if instdata != None:
+		chunk_wavdata_smpl = BytesIO()
+		temp_manufacturer = 0
+		temp_product = 0
+		temp_sampleperiod = 22676
+		temp_midinote = 60
+		temp_midipitchfraction = 0
+		temp_smpteformat = 0
+		temp_smpteoffset = 0
+		temp_num_loops = 0
+		temp_num_data = 0
+
+		if 'loop' in instdata: 
+			loopentry = instdata['loop']
+			instdata['loops'] = {'1886351212': {'type': 0, 'start': loopentry[0], 'end': loopentry[1], 'fraction': 0, 'num_times': 0}}
+
+		if 'manufacturer' in instdata: temp_manufacturer = instdata['manufacturer']
+		if 'product' in instdata: temp_product = instdata['product']
+		if 'sampleperiod' in instdata: temp_sampleperiod = instdata['sampleperiod']
+		if 'midinote' in instdata: temp_midinote = instdata['midinote']
+		if 'midipitchfraction' in instdata: temp_midipitchfraction = instdata['midipitchfraction']
+		if 'smpteformat' in instdata: temp_smpteformat = instdata['smpteformat']
+		if 'smpteoffset' in instdata: temp_smpteoffset = instdata['smpteoffset']
+		chunk_wavdata_smpl.write(temp_manufacturer.to_bytes(4, 'little'))
+		chunk_wavdata_smpl.write(temp_product.to_bytes(4, 'little'))
+		chunk_wavdata_smpl.write(temp_sampleperiod.to_bytes(4, 'little'))
+		chunk_wavdata_smpl.write(temp_midinote.to_bytes(4, 'little'))
+		chunk_wavdata_smpl.write(temp_midipitchfraction.to_bytes(4, 'little'))
+		chunk_wavdata_smpl.write(temp_smpteformat.to_bytes(4, 'little'))
+		chunk_wavdata_smpl.write(temp_smpteoffset.to_bytes(4, 'little'))
+		if 'loops' in instdata: 
+			temp_num_loops = len(instdata['loops'])
+		chunk_wavdata_smpl.write(temp_num_loops.to_bytes(4, 'little'))
+		chunk_wavdata_smpl.write(temp_num_data.to_bytes(4, 'little'))
+		if 'loops' in instdata: 
+			looplist = instdata['loops']
+			for loopentry in looplist:
+				loopdata = looplist[loopentry]
+				chunk_wavdata_smpl.write(int(loopentry).to_bytes(4, 'little'))
+				chunk_wavdata_smpl.write(loopdata['type'].to_bytes(4, 'little'))
+				chunk_wavdata_smpl.write(loopdata['start'].to_bytes(4, 'little'))
+				chunk_wavdata_smpl.write(loopdata['end'].to_bytes(4, 'little'))
+				chunk_wavdata_smpl.write(loopdata['fraction'].to_bytes(4, 'little'))
+				chunk_wavdata_smpl.write(loopdata['num_times'].to_bytes(4, 'little'))
+
+		chunk_wavdata_smpl.seek(0)
+		wav_CHUNK_smpl = chunk_wavdata_smpl.read()
+		return [b'smpl',wav_CHUNK_smpl]
+
 def decode(wavfile):
 	#print("[audio-wav] Decode Sample")
 	out_wavinfo = {}
@@ -79,54 +129,34 @@ def generate(file, data, channels, freq, bits, instdata):
 	# ----- data -----
 	table_chunks.append([b'data',data])
 	# ----- smpl -----
-	if instdata != None:
-		chunk_wavdata_smpl = BytesIO()
-		temp_manufacturer = 0
-		temp_product = 0
-		temp_sampleperiod = 22676
-		temp_midinote = 60
-		temp_midipitchfraction = 0
-		temp_smpteformat = 0
-		temp_smpteoffset = 0
-		temp_num_loops = 0
-		temp_num_data = 0
+	wav_CHUNK_smpl = makesmpl(instdata)
+	if wav_CHUNK_smpl != None:
+		table_chunks.append(wav_CHUNK_smpl)
 
-		if 'loop' in instdata: 
-			loopentry = instdata['loop']
-			instdata['loops'] = {'1886351212': {'type': 0, 'start': loopentry[0], 'end': loopentry[1], 'fraction': 0, 'num_times': 0}}
+	for table_chunk in table_chunks:
+		print(table_chunk[0])
 
-		if 'manufacturer' in instdata: temp_manufacturer = instdata['manufacturer']
-		if 'product' in instdata: temp_product = instdata['product']
-		if 'sampleperiod' in instdata: temp_sampleperiod = instdata['sampleperiod']
-		if 'midinote' in instdata: temp_midinote = instdata['midinote']
-		if 'midipitchfraction' in instdata: temp_midipitchfraction = instdata['midipitchfraction']
-		if 'smpteformat' in instdata: temp_smpteformat = instdata['smpteformat']
-		if 'smpteoffset' in instdata: temp_smpteoffset = instdata['smpteoffset']
-		chunk_wavdata_smpl.write(temp_manufacturer.to_bytes(4, 'little'))
-		chunk_wavdata_smpl.write(temp_product.to_bytes(4, 'little'))
-		chunk_wavdata_smpl.write(temp_sampleperiod.to_bytes(4, 'little'))
-		chunk_wavdata_smpl.write(temp_midinote.to_bytes(4, 'little'))
-		chunk_wavdata_smpl.write(temp_midipitchfraction.to_bytes(4, 'little'))
-		chunk_wavdata_smpl.write(temp_smpteformat.to_bytes(4, 'little'))
-		chunk_wavdata_smpl.write(temp_smpteoffset.to_bytes(4, 'little'))
-		if 'loops' in instdata: 
-			temp_num_loops = len(instdata['loops'])
-		chunk_wavdata_smpl.write(temp_num_loops.to_bytes(4, 'little'))
-		chunk_wavdata_smpl.write(temp_num_data.to_bytes(4, 'little'))
-		if 'loops' in instdata: 
-			looplist = instdata['loops']
-			for loopentry in looplist:
-				loopdata = looplist[loopentry]
-				chunk_wavdata_smpl.write(int(loopentry).to_bytes(4, 'little'))
-				chunk_wavdata_smpl.write(loopdata['type'].to_bytes(4, 'little'))
-				chunk_wavdata_smpl.write(loopdata['start'].to_bytes(4, 'little'))
-				chunk_wavdata_smpl.write(loopdata['end'].to_bytes(4, 'little'))
-				chunk_wavdata_smpl.write(loopdata['fraction'].to_bytes(4, 'little'))
-				chunk_wavdata_smpl.write(loopdata['num_times'].to_bytes(4, 'little'))
+	chunk_data_bytes = data_bytes.riff_make(table_chunks)
+	bytes_wavdata = b'WAVE' + chunk_data_bytes
+	chunks_main = data_bytes.riff_make([[b'RIFF',bytes_wavdata]])
 
-		chunk_wavdata_smpl.seek(0)
-		wav_CHUNK_smpl = chunk_wavdata_smpl.read()
-		table_chunks.append([b'smpl',wav_CHUNK_smpl])
+	file_object.write(chunks_main)
+
+def inject_smpl(instdata):
+	print("[audio-wav] Injecting 'smpl' chunk")
+	file_wav = open(wavfile, 'rb')
+	chunks_main = data_bytes.riff_read(file_wav, 0)
+	bytes_wavdata = chunks_main[0][1]
+	chunk_data_bytes = bytes_wavdata[0:4]
+	if chunk_data_bytes == b'WAVE':
+		chunks_wavdata = data_bytes.riff_read(bytes_wavdata, 4)
+		wav_CHUNK_smpl = makesmpl(instdata)
+		if wav_CHUNK_smpl != None:
+			table_chunks.append(wav_CHUNK_smpl)
+
+		for table_chunk in table_chunks:
+			print(table_chunk[0])
+
 	chunk_data_bytes = data_bytes.riff_make(table_chunks)
 	bytes_wavdata = b'WAVE' + chunk_data_bytes
 	chunks_main = data_bytes.riff_make([[b'RIFF',bytes_wavdata]])

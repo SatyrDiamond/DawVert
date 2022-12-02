07cb2a61785ad6e69adbcfc166650afe9b8ebeb7
Colby Ray <colbydray@gmail.com>
2022-12-02 15:43:15 -0500

    m8bp vst

--- functions/plugin_convert.py
+++ functions/plugin_convert.py
@@ -19,150 +19,154 @@ vst3path_loaded = False
 
 # -------------------- VST --------------------
 
+def magical8bitplug_addvalue(xmltag, name, value):
+	temp_xml = ET.SubElement(xmltag, 'PARAM')
+	temp_xml.set('id', str(name))
+	temp_xml.set('value', str(value))
 def grace_addvalue(xmltag, name, value):
-    temp_xml = ET.SubElement(xmltag, name)
-    if value != None:
-        temp_xml.text = value
+	temp_xml = ET.SubElement(xmltag, name)
+	if value != None:
+		temp_xml.text = value
 def grace_create_main():
-    gx_root = ET.Element("root")
+	gx_root = ET.Element("root")
 
-    gx_GlobalParameters = ET.SubElement(gx_root, "GlobalParameters")
-    grace_addvalue(gx_GlobalParameters, 'VoiceMode', 'Poly')
-    grace_addvalue(gx_GlobalParameters, 'VoiceGlide', '0')
+	gx_GlobalParameters = ET.SubElement(gx_root, "GlobalParameters")
+	grace_addvalue(gx_GlobalParameters, 'VoiceMode', 'Poly')
+	grace_addvalue(gx_GlobalParameters, 'VoiceGlide', '0')
 
-    gx_SampleGroup = ET.SubElement(gx_root, "SampleGroup")
-    grace_addvalue(gx_SampleGroup, 'Name', 'Group 1')
-    gx_VoiceParameters = ET.SubElement(gx_SampleGroup, "VoiceParameters")
-    grace_addvalue(gx_VoiceParameters, 'AmpAttack', '0')
-    grace_addvalue(gx_VoiceParameters, 'AmpDecay', '0')
-    grace_addvalue(gx_VoiceParameters, 'AmpEnvSnap', 'SnapOff')
-    grace_addvalue(gx_VoiceParameters, 'AmpHold', '0')
-    grace_addvalue(gx_VoiceParameters, 'AmpRelease', '0')
-    grace_addvalue(gx_VoiceParameters, 'AmpSustain', '1')
-    grace_addvalue(gx_VoiceParameters, 'AmpVelocityDepth', 'Vel80')
-    grace_addvalue(gx_VoiceParameters, 'Filter1KeyFollow', '0.5')
-    grace_addvalue(gx_VoiceParameters, 'Filter1Par1', '0.5')
-    grace_addvalue(gx_VoiceParameters, 'Filter1Par2', '0.5')
-    grace_addvalue(gx_VoiceParameters, 'Filter1Par3', '0.5')
-    grace_addvalue(gx_VoiceParameters, 'Filter1Par4', '1')
-    grace_addvalue(gx_VoiceParameters, 'Filter1Type', 'ftNone')
-    grace_addvalue(gx_VoiceParameters, 'Filter2KeyFollow', '0.5')
-    grace_addvalue(gx_VoiceParameters, 'Filter2Par1', '0.5')
-    grace_addvalue(gx_VoiceParameters, 'Filter2Par2', '0.5')
-    grace_addvalue(gx_VoiceParameters, 'Filter2Par3', '0.5')
-    grace_addvalue(gx_VoiceParameters, 'Filter2Par4', '1')
-    grace_addvalue(gx_VoiceParameters, 'Filter2Type', 'ftNone')
-    grace_addvalue(gx_VoiceParameters, 'FilterOutputBlend', '1')
-    grace_addvalue(gx_VoiceParameters, 'FilterRouting', 'Serial')
-    grace_addvalue(gx_VoiceParameters, 'Lfo1Par1', '0.5')
-    grace_addvalue(gx_VoiceParameters, 'Lfo1Par2', '0.5')
-    grace_addvalue(gx_VoiceParameters, 'Lfo1Par3', '0.5')
-    grace_addvalue(gx_VoiceParameters, 'Lfo2Par1', '0.5')
-    grace_addvalue(gx_VoiceParameters, 'Lfo2Par2', '0.5')
-    grace_addvalue(gx_VoiceParameters, 'Lfo2Par3', '0.5')
-    grace_addvalue(gx_VoiceParameters, 'LfoFreqMode1', 'Fixed100Millisecond')
-    grace_addvalue(gx_VoiceParameters, 'LfoFreqMode2', 'Fixed100Millisecond')
-    grace_addvalue(gx_VoiceParameters, 'LfoShape1', 'Triangle')
-    grace_addvalue(gx_VoiceParameters, 'LfoShape2', 'Triangle')
-    grace_addvalue(gx_VoiceParameters, 'LoopEnd', '1')
-    grace_addvalue(gx_VoiceParameters, 'LoopStart', '0')
-    grace_addvalue(gx_VoiceParameters, 'ModAttack', '0')
-    grace_addvalue(gx_VoiceParameters, 'ModDecay', '0.300000011920929')
-    grace_addvalue(gx_VoiceParameters, 'ModEnvSnap', 'SnapOff')
-    grace_addvalue(gx_VoiceParameters, 'ModHold', '0')
-    grace_addvalue(gx_VoiceParameters, 'ModRelease', '0.300000011920929')
-    grace_addvalue(gx_VoiceParameters, 'ModSustain', '0.300000011920929')
-    grace_addvalue(gx_VoiceParameters, 'ModVelocityDepth', 'Vel80')
-    grace_addvalue(gx_VoiceParameters, 'OutputGain', '0.5')
-    grace_addvalue(gx_VoiceParameters, 'OutputPan', '0.504999995231628')
-    grace_addvalue(gx_VoiceParameters, 'PitchTracking', 'Note')
-    grace_addvalue(gx_VoiceParameters, 'SampleEnd', '1')
-    grace_addvalue(gx_VoiceParameters, 'SampleReset', 'None')
-    grace_addvalue(gx_VoiceParameters, 'SamplerLoopBounds', 'LoopPoints')
-    grace_addvalue(gx_VoiceParameters, 'SamplerTriggerMode', 'LoopOff')
-    grace_addvalue(gx_VoiceParameters, 'SampleStart', '0')
-    grace_addvalue(gx_VoiceParameters, 'Seq1Clock', 'Div_4')
-    grace_addvalue(gx_VoiceParameters, 'Seq1Direction', 'Forwards')
-    grace_addvalue(gx_VoiceParameters, 'Seq2Clock', 'Div_4')
-    grace_addvalue(gx_VoiceParameters, 'Seq2Direction', 'Forwards')
-    grace_addvalue(gx_VoiceParameters, 'StepSeq1Length', 'Eight')
-    grace_addvalue(gx_VoiceParameters, 'StepSeq2Length', 'Eight')
-    grace_addvalue(gx_VoiceParameters, 'VoicePitchOne', '0.5')
-    grace_addvalue(gx_VoiceParameters, 'VoicePitchTwo', '0.5')
+	gx_SampleGroup = ET.SubElement(gx_root, "SampleGroup")
+	grace_addvalue(gx_SampleGroup, 'Name', 'Group 1')
+	gx_VoiceParameters = ET.SubElement(gx_SampleGroup, "VoiceParameters")
+	grace_addvalue(gx_VoiceParameters, 'AmpAttack', '0')
+	grace_addvalue(gx_VoiceParameters, 'AmpDecay', '0')
+	grace_addvalue(gx_VoiceParameters, 'AmpEnvSnap', 'SnapOff')
+	grace_addvalue(gx_VoiceParameters, 'AmpHold', '0')
+	grace_addvalue(gx_VoiceParameters, 'AmpRelease', '0')
+	grace_addvalue(gx_VoiceParameters, 'AmpSustain', '1')
+	grace_addvalue(gx_VoiceParameters, 'AmpVelocityDepth', 'Vel80')
+	grace_addvalue(gx_VoiceParameters, 'Filter1KeyFollow', '0.5')
+	grace_addvalue(gx_VoiceParameters, 'Filter1Par1', '0.5')
+	grace_addvalue(gx_VoiceParameters, 'Filter1Par2', '0.5')
+	grace_addvalue(gx_VoiceParameters, 'Filter1Par3', '0.5')
+	grace_addvalue(gx_VoiceParameters, 'Filter1Par4', '1')
+	grace_addvalue(gx_VoiceParameters, 'Filter1Type', 'ftNone')
+	grace_addvalue(gx_VoiceParameters, 'Filter2KeyFollow', '0.5')
+	grace_addvalue(gx_VoiceParameters, 'Filter2Par1', '0.5')
+	grace_addvalue(gx_VoiceParameters, 'Filter2Par2', '0.5')
+	grace_addvalue(gx_VoiceParameters, 'Filter2Par3', '0.5')
+	grace_addvalue(gx_VoiceParameters, 'Filter2Par4', '1')
+	grace_addvalue(gx_VoiceParameters, 'Filter2Type', 'ftNone')
+	grace_addvalue(gx_VoiceParameters, 'FilterOutputBlend', '1')
+	grace_addvalue(gx_VoiceParameters, 'FilterRouting', 'Serial')
+	grace_addvalue(gx_VoiceParameters, 'Lfo1Par1', '0.5')
+	grace_addvalue(gx_VoiceParameters, 'Lfo1Par2', '0.5')
+	grace_addvalue(gx_VoiceParameters, 'Lfo1Par3', '0.5')
+	grace_addvalue(gx_VoiceParameters, 'Lfo2Par1', '0.5')
+	grace_addvalue(gx_VoiceParameters, 'Lfo2Par2', '0.5')
+	grace_addvalue(gx_VoiceParameters, 'Lfo2Par3', '0.5')
+	grace_addvalue(gx_VoiceParameters, 'LfoFreqMode1', 'Fixed100Millisecond')
+	grace_addvalue(gx_VoiceParameters, 'LfoFreqMode2', 'Fixed100Millisecond')
+	grace_addvalue(gx_VoiceParameters, 'LfoShape1', 'Triangle')
+	grace_addvalue(gx_VoiceParameters, 'LfoShape2', 'Triangle')
+	grace_addvalue(gx_VoiceParameters, 'LoopEnd', '1')
+	grace_addvalue(gx_VoiceParameters, 'LoopStart', '0')
+	grace_addvalue(gx_VoiceParameters, 'ModAttack', '0')
+	grace_addvalue(gx_VoiceParameters, 'ModDecay', '0.300000011920929')
+	grace_addvalue(gx_VoiceParameters, 'ModEnvSnap', 'SnapOff')
+	grace_addvalue(gx_VoiceParameters, 'ModHold', '0')
+	grace_addvalue(gx_VoiceParameters, 'ModRelease', '0.300000011920929')
+	grace_addvalue(gx_VoiceParameters, 'ModSustain', '0.300000011920929')
+	grace_addvalue(gx_VoiceParameters, 'ModVelocityDepth', 'Vel80')
+	grace_addvalue(gx_VoiceParameters, 'OutputGain', '0.5')
+	grace_addvalue(gx_VoiceParameters, 'OutputPan', '0.504999995231628')
+	grace_addvalue(gx_VoiceParameters, 'PitchTracking', 'Note')
+	grace_addvalue(gx_VoiceParameters, 'SampleEnd', '1')
+	grace_addvalue(gx_VoiceParameters, 'SampleReset', 'None')
+	grace_addvalue(gx_VoiceParameters, 'SamplerLoopBounds', 'LoopPoints')
+	grace_addvalue(gx_VoiceParameters, 'SamplerTriggerMode', 'LoopOff')
+	grace_addvalue(gx_VoiceParameters, 'SampleStart', '0')
+	grace_addvalue(gx_VoiceParameters, 'Seq1Clock', 'Div_4')
+	grace_addvalue(gx_VoiceParameters, 'Seq1Direction', 'Forwards')
+	grace_addvalue(gx_VoiceParameters, 'Seq2Clock', 'Div_4')
+	grace_addvalue(gx_VoiceParameters, 'Seq2Direction', 'Forwards')
+	grace_addvalue(gx_VoiceParameters, 'StepSeq1Length', 'Eight')
+	grace_addvalue(gx_VoiceParameters, 'StepSeq2Length', 'Eight')
+	grace_addvalue(gx_VoiceParameters, 'VoicePitchOne', '0.5')
+	grace_addvalue(gx_VoiceParameters, 'VoicePitchTwo', '0.5')
 
-    grace_addvalue(gx_root, 'PatchFileFormatVersion', '4')
-    grace_addvalue(gx_root, 'PatchFileType', 'LucidityPatchFile')
+	grace_addvalue(gx_root, 'PatchFileFormatVersion', '4')
+	grace_addvalue(gx_root, 'PatchFileType', 'LucidityPatchFile')
 
-    gx_PresetInfo = ET.SubElement(gx_root, "PresetInfo")
-    grace_addvalue(gx_PresetInfo, 'PresetName', 'Defualt')
-    grace_addvalue(gx_PresetInfo, 'PresetFileName', None)
+	gx_PresetInfo = ET.SubElement(gx_root, "PresetInfo")
+	grace_addvalue(gx_PresetInfo, 'PresetName', 'Defualt')
+	grace_addvalue(gx_PresetInfo, 'PresetFileName', None)
 
-    gx_MidiMap = ET.SubElement(gx_root, "MidiMap")
+	gx_MidiMap = ET.SubElement(gx_root, "MidiMap")
 
-    return gx_root
+	return gx_root
 def grace_create_region(gx_root, regionparams):
-    gx_SampleGroup = gx_root.findall('SampleGroup')[0]
-    gx_Region = ET.SubElement(gx_SampleGroup, "Region")
-    gx_RegionProp = ET.SubElement(gx_Region, "RegionProperties")
-    gx_SampleProp = ET.SubElement(gx_Region, "SampleProperties")
+	gx_SampleGroup = gx_root.findall('SampleGroup')[0]
+	gx_Region = ET.SubElement(gx_SampleGroup, "Region")
+	gx_RegionProp = ET.SubElement(gx_Region, "RegionProperties")
+	gx_SampleProp = ET.SubElement(gx_Region, "SampleProperties")
 
-    FileName = ''
+	FileName = ''
 
-    if 'filename' in regionparams: FileName = regionparams['filename']
-    grace_addvalue(gx_SampleProp, 'SampleFileName', str(FileName))
+	if 'filename' in regionparams: FileName = regionparams['filename']
+	grace_addvalue(gx_SampleProp, 'SampleFileName', str(FileName))
 
-    LowNote = 0
-    HighNote = 127
-    LowVelocity = 0
-    HighVelocity = 127
-    RootNote = 60
-    SampleStart = 0
-    SampleEnd = 173181
-    LoopStart = -1
-    LoopEnd = -1
-    
-    if 'note' in regionparams:
-        LowNote = regionparams['note'][0] + 60
-        HighNote = regionparams['note'][1] + 60
+	LowNote = 0
+	HighNote = 127
+	LowVelocity = 0
+	HighVelocity = 127
+	RootNote = 60
+	SampleStart = 0
+	SampleEnd = 173181
+	LoopStart = -1
+	LoopEnd = -1
+	
+	if 'note' in regionparams:
+		LowNote = regionparams['note'][0] + 60
+		HighNote = regionparams['note'][1] + 60
 
-    if 'velocity' in regionparams:
-        LowVelocity = int(regionparams['velocity'][0]*127)
-        HighVelocity = int(regionparams['velocity'][1]*127)
+	if 'velocity' in regionparams:
+		LowVelocity = int(regionparams['velocity'][0]*127)
+		HighVelocity = int(regionparams['velocity'][1]*127)
 
-    if 'loop' in regionparams:
-        LoopStart = regionparams['loop'][0]
-        LoopEnd = regionparams['loop'][1]
+	if 'loop' in regionparams:
+		LoopStart = regionparams['loop'][0]
+		LoopEnd = regionparams['loop'][1]
 
-    if 'middlenote' in regionparams: RootNote = regionparams['middlenote'] + 60
-    if 'start' in regionparams: SampleStart = regionparams['start']
-    if 'length' in regionparams: SampleEnd = regionparams['length'] - 1
+	if 'middlenote' in regionparams: RootNote = regionparams['middlenote'] + 60
+	if 'start' in regionparams: SampleStart = regionparams['start']
+	if 'length' in regionparams: SampleEnd = regionparams['length'] - 1
 
-    grace_addvalue(gx_RegionProp, 'LowNote', str(LowNote))
-    grace_addvalue(gx_RegionProp, 'HighNote', str(HighNote))
-    grace_addvalue(gx_RegionProp, 'LowVelocity', str(LowVelocity))
-    grace_addvalue(gx_RegionProp, 'HighVelocity', str(HighVelocity))
-    grace_addvalue(gx_RegionProp, 'RootNote', str(RootNote))
-    grace_addvalue(gx_RegionProp, 'SampleStart', str(SampleStart))
-    grace_addvalue(gx_RegionProp, 'SampleEnd', str(SampleEnd))
-    grace_addvalue(gx_SampleProp, 'SampleFrames', str(SampleEnd))
-    grace_addvalue(gx_RegionProp, 'LoopStart', str(LoopStart))
-    grace_addvalue(gx_RegionProp, 'LoopEnd', str(LoopEnd))
+	grace_addvalue(gx_RegionProp, 'LowNote', str(LowNote))
+	grace_addvalue(gx_RegionProp, 'HighNote', str(HighNote))
+	grace_addvalue(gx_RegionProp, 'LowVelocity', str(LowVelocity))
+	grace_addvalue(gx_RegionProp, 'HighVelocity', str(HighVelocity))
+	grace_addvalue(gx_RegionProp, 'RootNote', str(RootNote))
+	grace_addvalue(gx_RegionProp, 'SampleStart', str(SampleStart))
+	grace_addvalue(gx_RegionProp, 'SampleEnd', str(SampleEnd))
+	grace_addvalue(gx_SampleProp, 'SampleFrames', str(SampleEnd))
+	grace_addvalue(gx_RegionProp, 'LoopStart', str(LoopStart))
+	grace_addvalue(gx_RegionProp, 'LoopEnd', str(LoopEnd))
 
-    SampleVolume = 0
-    SamplePan = 0
-    SampleTune = 0
-    SampleFine = 0
+	SampleVolume = 0
+	SamplePan = 0
+	SampleTune = 0
+	SampleFine = 0
 
-    grace_addvalue(gx_RegionProp, 'SampleVolume', str(SampleVolume))
-    grace_addvalue(gx_RegionProp, 'SamplePan', str(SamplePan))
-    grace_addvalue(gx_RegionProp, 'SampleTune', str(SampleTune))
-    grace_addvalue(gx_RegionProp, 'SampleFine', str(SampleFine))
+	grace_addvalue(gx_RegionProp, 'SampleVolume', str(SampleVolume))
+	grace_addvalue(gx_RegionProp, 'SamplePan', str(SamplePan))
+	grace_addvalue(gx_RegionProp, 'SampleTune', str(SampleTune))
+	grace_addvalue(gx_RegionProp, 'SampleFine', str(SampleFine))
 
-    SampleBeats = 4
+	SampleBeats = 4
 
-    grace_addvalue(gx_RegionProp, 'SampleBeats', str(SampleBeats))
+	grace_addvalue(gx_RegionProp, 'SampleBeats', str(SampleBeats))
 
-    return gx_root
+	return gx_root
 
 # -------------------- VST List --------------------
 def replace_vst(instdata, name, data):
@@ -272,6 +276,50 @@ def convplug_inst(instdata, dawname):
 				vst2data = b'VC2!' + len(xmlout).to_bytes(4, "little") + xmlout
 				replace_vst(instdata, 'juicysfplugin', vst2data)
 
+			# -------------------- famistudio > vst2 (magical8bitplug) --------------------
+			elif pluginname == 'famistudio':
+				fsd_data = instdata['plugindata']
+				m8p_root = ET.Element("root")
+				m8p_params = ET.SubElement(m8p_root, "Params")
+				magical8bitplug_addvalue(m8p_params, "arpeggioDirection", 0.0)
+				magical8bitplug_addvalue(m8p_params, "arpeggioTime", 0.02999999932944775)
+				magical8bitplug_addvalue(m8p_params, "attack", 0.0)
+				magical8bitplug_addvalue(m8p_params, "bendRange", 12.0)
+				magical8bitplug_addvalue(m8p_params, "colorScheme", 1.0)
+				magical8bitplug_addvalue(m8p_params, "decay", 0.0)
+				magical8bitplug_addvalue(m8p_params, "duty", 0.0)
+				magical8bitplug_addvalue(m8p_params, "gain", 0.5)
+				magical8bitplug_addvalue(m8p_params, "isAdvancedPanelOpen_raw", 1.0)
+				magical8bitplug_addvalue(m8p_params, "isArpeggioEnabled_raw", 0.0)
+				m8p_dutyEnv = ET.SubElement(m8p_root, "dutyEnv")
+				if 'EnvDutyCycle' in fsd_data: 
+					magical8bitplug_addvalue(m8p_params, "isDutySequenceEnabled_raw", 1.0)
+					m8p_dutyEnv.text = fsd_data['EnvDutyCycle']
+				else: magical8bitplug_addvalue(m8p_params, "isDutySequenceEnabled_raw", 0.0)
+				m8p_pitchEnv = ET.SubElement(m8p_root, "pitchEnv")
+				m8p_volumeEnv = ET.SubElement(m8p_root, "volumeEnv")
+				if 'EnvVolume' in fsd_data: 
+					magical8bitplug_addvalue(m8p_params, "isVolumeSequenceEnabled_raw", 1.0)
+					m8p_volumeEnv.text = fsd_data['EnvVolume']
+				else: magical8bitplug_addvalue(m8p_params, "isVolumeSequenceEnabled_raw", 0.0)
+				magical8bitplug_addvalue(m8p_params, "maxPoly", 8.0)
+				magical8bitplug_addvalue(m8p_params, "noiseAlgorithm_raw", 0.0)
+				if fsd_data['wave'] == 'Square': magical8bitplug_addvalue(m8p_params, "osc", 0.0)
+				if fsd_data['wave'] == 'Triangle': magical8bitplug_addvalue(m8p_params, "osc", 1.0)
+				if fsd_data['wave'] == 'Noise': magical8bitplug_addvalue(m8p_params, "osc", 2.0)
+				magical8bitplug_addvalue(m8p_params, "pitchSequenceMode_raw", 0.0)
+				magical8bitplug_addvalue(m8p_params, "release", 0.0)
+				magical8bitplug_addvalue(m8p_params, "restrictsToNESFrequency_raw", 0.0)
+				magical8bitplug_addvalue(m8p_params, "suslevel", 1.0)
+				magical8bitplug_addvalue(m8p_params, "sweepInitialPitch", 0.0)
+				magical8bitplug_addvalue(m8p_params, "sweepTime", 0.1000000014901161)
+				magical8bitplug_addvalue(m8p_params, "vibratoDelay", 0.2999999821186066)
+				magical8bitplug_addvalue(m8p_params, "vibratoDepth", 0.0)
+				magical8bitplug_addvalue(m8p_params, "vibratoIgnoresWheel_raw", 1.0)
+				magical8bitplug_addvalue(m8p_params, "vibratoRate", 0.1500000059604645)
+				xmlout = ET.tostring(m8p_root, encoding='utf-8')
+				vst2data = b'VC2!' + len(xmlout).to_bytes(4, "little") + xmlout
+				replace_vst(instdata, 'Magical 8bit Plug 2', vst2data)
 			# -------------------- zynaddsubfx - from lmms --------------------
 			elif pluginname == 'zynaddsubfx' and dawname != 'lmms':
 				zasfxdata = instdata['plugindata']['data']

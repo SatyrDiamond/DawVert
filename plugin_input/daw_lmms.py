85c3174717a78765863cce9b2451ab04d31849ca
SatyrDiamond <satyrdiamond@gmail.com>
2022-12-06 10:56:25 -0500

    update

--- plugin_input/daw_lmms.py
+++ plugin_input/daw_lmms.py
@@ -407,12 +407,6 @@ def lmms_decode_effectslot(fxslotX):
     if fxpluginname != 'ladspaeffect':
         fxxml_plugin = fxslotX.findall(fxlist[fxpluginname])[0]
         fxslotJ['enabled'] = int(fxslotX.get('on'))
-        fxslotJ['plugin'] = 'native-lmms'
-        fxcvpj_l_plugin = {}
-        fxcvpj_l_plugin['name'] = fxpluginname
-        fxcvpj_l_plugin['data'] = {}
-        for name, value in fxxml_plugin.attrib.items(): fxcvpj_l_plugin['data'][name] = value
-        for name in fxxml_plugin: fxcvpj_l_plugin['data'][name.tag] = name.get('value')
         wet = float(fxslotX.get('wet'))
         print('['+fxpluginname,end='] ')
         if wet < 0:
@@ -421,6 +415,14 @@ def lmms_decode_effectslot(fxslotX):
         else:
             fxslotJ['add_dry_minus_wet'] = 0
             fxslotJ['wet'] = wet
+
+
+        fxslotJ['plugin'] = 'native-lmms'
+        fxcvpj_l_plugin = {}
+        fxcvpj_l_plugin['name'] = fxpluginname
+        fxcvpj_l_plugin['data'] = {}
+        for name, value in fxxml_plugin.attrib.items(): fxcvpj_l_plugin['data'][name] = value
+        for name in fxxml_plugin: fxcvpj_l_plugin['data'][name.tag] = name.get('value')
         fxslotJ['plugindata'] = fxcvpj_l_plugin
         return fxslotJ
     else:

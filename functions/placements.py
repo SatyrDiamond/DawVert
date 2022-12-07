98cd47fcea408d10be36a1dcb5052763582505c7
Colby Ray <colbydray@gmail.com>
2022-12-07 16:31:41 -0500

    lanes

--- functions/placements.py
+++ functions/placements.py
@@ -1,4 +1,40 @@
 
+def removelanes(projJ):
+    old_trackdata = projJ['trackdata']
+    old_trackordering = projJ['trackordering']
+    new_trackdata = {}
+    new_trackordering = []
+
+    for trackid in old_trackordering:
+        if trackid in old_trackdata:
+            lr_t_trdata = old_trackdata[trackid]
+            if 'laned' in lr_t_trdata:
+                trackbase = lr_t_trdata.copy()
+                del trackbase['laned']
+                del trackbase['lanedata']
+                del trackbase['laneordering']
+                lr_t_lanedata = lr_t_trdata['lanedata']
+                lr_t_laneordering = lr_t_trdata['laneordering']
+                for laneid in lr_t_laneordering:
+                    lr_t_l_data = lr_t_lanedata[laneid]
+                    splitnameid = trackid+'_Lane'+laneid
+                    if 'name' in lr_t_l_data: lr_t_l_name = lr_t_l_data['name']
+                    else: lr_t_l_name = ''
+                    if 'placements' in lr_t_l_data: lr_t_l_placements = lr_t_l_data['placements']
+                    else: lr_t_l_placements = []
+                    if 'name' in lr_t_trdata: ntp_name = lr_t_trdata['name']+' ['+lr_t_l_name+']'
+                    else: ntp_name = '['+lr_t_l_name+']'
+                    part_track_data = trackbase.copy()
+                    part_track_data['name'] = ntp_name
+                    part_track_data['placements'] = lr_t_l_placements
+                    new_trackdata[splitnameid] = part_track_data
+                    new_trackordering.append(splitnameid)
+            else:
+                new_trackdata[trackid] = lr_t_trdata
+                new_trackordering.append(trackid)
+    projJ['trackdata'] = new_trackdata
+    projJ['trackordering'] = new_trackordering
+
 def get_timesig(patternLength, notesPerBeat):
     MaxFactor = 1024
     factor = 1

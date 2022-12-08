d6fd5edb6983673de08e49d296fe24a8a8ebabea
Colby Ray <colbydray@gmail.com>
2022-12-07 19:11:11 -0500

    cvpj file magic

--- plugin_input/cvpj_r.py
+++ plugin_input/cvpj_r.py
@@ -7,12 +7,12 @@ class input_cvpj_r(plugin_input.base):
     def getshortname(self): return 'cvpj_r'
     def getname(self): return 'cvpj_r'
     def gettype(self): return 'r'
-    def supported_autodetect(self): return False
+    def supported_autodetect(self): return True
     def detect(self, input_file):
         bytestream = open(input_file, 'rb')
         bytestream.seek(0)
-        bytesdata = bytestream.read(4)
-        if bytesdata == b'CONVPROJ': return True
+        bytesdata = bytestream.read(12)
+        if bytesdata == b'CONVPROJ___R': return True
         else: return False
     def parse(self, input_file, extra_param):
         bytestream = open(input_file, 'r')

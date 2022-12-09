0be61cd897150ca92be9f3effa31af98d4ed642c
Colby Ray <colbydray@gmail.com>
2022-12-09 09:39:28 -0500

    remove lines

--- plugin_output/cvpj_mi.py
+++ plugin_output/cvpj_mi.py
@@ -5,18 +5,10 @@ import plugin_output
 import json
 
 class output_cvpj(plugin_output.base):
-    def __init__(self):
-        pass
-
-    def getname(self):
-        return 'DEBUG'
-
-    def getshortname(self):
-        return 'cvpj_mi'
-
-    def gettype(self):
-        return 'mi'
-
+    def __init__(self): pass
+    def getname(self): return 'DEBUG'
+    def getshortname(self): return 'cvpj_mi'
+    def gettype(self): return 'mi'
     def parse(self, convproj_json, output_file):
         projJ = json.loads(convproj_json)
         with open(output_file, "w") as fileout:

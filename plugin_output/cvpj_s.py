08bc594c6a175f68594ce5717e3273ffd332f632
SatyrDiamond <satyrdiamond@gmail.com>
2022-12-13 15:15:57 -0500

    'any' datatype now called separate

---
+++ plugin_output/cvpj_s.py
@@ -0,0 +1,16 @@
+# SPDX-FileCopyrightText: 2022 SatyrDiamond
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import plugin_output
+import json
+
+class output_cvpj(plugin_output.base):
+    def __init__(self): pass
+    def getname(self): return 'DEBUG'
+    def getshortname(self): return 'cvpj_s'
+    def gettype(self): return 's'
+    def parse(self, convproj_json, output_file):
+        projJ = json.loads(convproj_json)
+        with open(output_file, "w") as fileout:
+            fileout.write("CONVPROJ___S\n")
+            json.dump(projJ, fileout, indent=4, sort_keys=True)

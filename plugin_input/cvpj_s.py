0ca3547cd4d6f49b674ad0a99fc5b12860d1ef68
SatyrDiamond <satyrdiamond@gmail.com>
2022-12-13 15:15:29 -0500

    'any' datatype now called 'separate'

---
+++ plugin_input/cvpj_s.py
@@ -0,0 +1,25 @@
+# SPDX-FileCopyrightText: 2022 SatyrDiamond
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from functions import data_bytes
+import plugin_input
+import json
+
+class input_cvpj_r(plugin_input.base):
+    def __init__(self): pass
+    def getshortname(self): return 'cvpj_s'
+    def getname(self): return 'cvpj_s'
+    def gettype(self): return 's'
+    def supported_autodetect(self): return True
+    def detect(self, input_file):
+        bytestream = open(input_file, 'rb')
+        bytestream.seek(0)
+        bytesdata = bytestream.read(12)
+        if bytesdata == b'CONVPROJ___S': return True
+        else: return False
+    def parse(self, input_file, extra_param):
+        bytestream = open(input_file, 'r')
+        file_data = bytestream.read()
+        data = ''.join(file_data.split('\n')[1:])
+        return data
+

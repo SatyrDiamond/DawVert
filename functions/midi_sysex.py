2ea04282ab4efe7135076a34980c19d54ef52d6d
SatyrDiamond <satyrdiamond@gmail.com>
2022-12-21 05:14:13 -0500

    midi

---
+++ functions/midi_sysex.py
@@ -0,0 +1,132 @@
+
+import struct
+from functions import data_bytes
+
+sysex_brand = {}
+sysex_brand[int("01",16)] = "Sequential Circuits"
+sysex_brand[int("02",16)] = "IDP"
+sysex_brand[int("03",16)] = "Voyetra Turtle Beach, Inc."
+sysex_brand[int("04",16)] = "Moog Music"
+sysex_brand[int("05",16)] = "Passport Designs"
+sysex_brand[int("06",16)] = "Lexicon Inc."
+sysex_brand[int("07",16)] = "Kurzweil / Young Chang"
+sysex_brand[int("08",16)] = "Fender"
+sysex_brand[int("09",16)] = "MIDI9"
+sysex_brand[int("0A",16)] = "AKG Acoustics"
+sysex_brand[int("0B",16)] = "Voyce Music"
+sysex_brand[int("0C",16)] = "WaveFrame (Timeline)"
+sysex_brand[int("0D",16)] = "ADA Signal Processors, Inc."
+sysex_brand[int("0E",16)] = "Garfield Electronics"
+sysex_brand[int("0F",16)] = "Ensoniq"
+sysex_brand[int("10",16)] = "Oberheim / Gibson Labs"
+sysex_brand[int("11",16)] = "Apple"
+sysex_brand[int("12",16)] = "Grey Matter Response"
+sysex_brand[int("13",16)] = "Digidesign Inc."
+sysex_brand[int("14",16)] = "Palmtree Instruments"
+sysex_brand[int("15",16)] = "JLCooper Electronics"
+sysex_brand[int("16",16)] = "Lowrey Organ Company"
+sysex_brand[int("17",16)] = "Adams-Smith"
+sysex_brand[int("18",16)] = "E-mu"
+sysex_brand[int("19",16)] = "Harmony Systems"
+sysex_brand[int("1A",16)] = "ART"
+sysex_brand[int("1B",16)] = "Baldwin"
+sysex_brand[int("1C",16)] = "Eventide"
+sysex_brand[int("1D",16)] = "Inventronics"
+sysex_brand[int("1E",16)] = "Key Concepts"
+sysex_brand[int("1F",16)] = "Clarity"
+sysex_brand[int("20",16)] = "Passac"
+sysex_brand[int("21",16)] = "Proel Labs (SIEL)"
+sysex_brand[int("22",16)] = "Synthaxe (UK)"
+sysex_brand[int("23",16)] = "Stepp"
+sysex_brand[int("24",16)] = "Hohner"
+sysex_brand[int("25",16)] = "Twister"
+sysex_brand[int("26",16)] = "Ketron s.r.l."
+sysex_brand[int("27",16)] = "Jellinghaus MS"
+sysex_brand[int("28",16)] = "Southworth Music Systems"
+sysex_brand[int("29",16)] = "PPG (Germany)"
+sysex_brand[int("2A",16)] = "JEN"
+sysex_brand[int("2B",16)] = "Solid State Logic Organ Systems"
+sysex_brand[int("2C",16)] = "Audio Veritrieb-P. Struven"
+sysex_brand[int("2D",16)] = "Neve"
+sysex_brand[int("2E",16)] = "Soundtracs Ltd."
+sysex_brand[int("2F",16)] = "Elka"
+sysex_brand[int("30",16)] = "Dynacord"
+sysex_brand[int("31",16)] = "Viscount International Spa (Intercontinental Electronics)"
+sysex_brand[int("32",16)] = "Drawmer"
+sysex_brand[int("33",16)] = "Clavia Digital Instruments"
+sysex_brand[int("34",16)] = "Audio Architecture"
+sysex_brand[int("35",16)] = "Generalmusic Corp SpA"
+sysex_brand[int("36",16)] = "Cheetah Marketing"
+sysex_brand[int("37",16)] = "C.T.M."
+sysex_brand[int("38",16)] = "Simmons UK"
+sysex_brand[int("39",16)] = "Soundcraft Electronics"
+sysex_brand[int("3A",16)] = "Steinberg Media Technologies GmbH"
+sysex_brand[int("3B",16)] = "Wersi Gmbh"
+sysex_brand[int("3C",16)] = "AVAB Niethammer AB"
+sysex_brand[int("3D",16)] = "Digigram"
+sysex_brand[int("3E",16)] = "Waldorf Electronics GmbH"
+sysex_brand[int("3F",16)] = "Quasimidi"
+sysex_brand[int("40",16)] = "Kawai Musical Instruments MFG. CO. Ltd"
+sysex_brand[int("41",16)] = "Roland Corporation"
+sysex_brand[int("42",16)] = "Korg Inc."
+sysex_brand[int("43",16)] = "Yamaha Corporation"
+sysex_brand[int("44",16)] = "Casio Computer Co. Ltd"
+sysex_brand[int("46",16)] = "Kamiya Studio Co. Ltd"
+sysex_brand[int("47",16)] = "Akai Electric Co. Ltd."
+sysex_brand[int("48",16)] = "Victor Company of Japan, Ltd."
+sysex_brand[int("4B",16)] = "Fujitsu Limited"
+sysex_brand[int("4C",16)] = "Sony Corporation"
+sysex_brand[int("4E",16)] = "Teac Corporation"
+sysex_brand[int("50",16)] = "Matsushita Electric Industrial Co. , Ltd"
+sysex_brand[int("51",16)] = "Fostex Corporation"
+sysex_brand[int("52",16)] = "Zoom Corporation"
+sysex_brand[int("54",16)] = "Matsushita Communication Industrial Co., Ltd."
+sysex_brand[int("55",16)] = "Suzuki Musical Instruments MFG. Co., Ltd."
+sysex_brand[int("56",16)] = "Fuji Sound Corporation Ltd."
+sysex_brand[int("57",16)] = "Acoustic Technical Laboratory, Inc."
+sysex_brand[int("59",16)] = "Faith, Inc."
+sysex_brand[int("5A",16)] = "Internet Corporation"
+sysex_brand[int("5C",16)] = "Seekers Co. Ltd."
+sysex_brand[int("5F",16)] = "SD Card Association"
+
+global sysexvals
+
+sysexvals = {}
+
+def parse_sysex(msg, time):
+    sysexdata = msg.data
+
+    contents = data_bytes.bytearray2BytesIO(struct.pack("b"*len(sysexdata),*sysexdata))
+
+    print('[debug] -------------- sysex, T:', time)
+    manufac = int.from_bytes(contents.read(1), "little")
+
+    if manufac != 0:
+        model = int.from_bytes(contents.read(1), "little")
+        devid = int.from_bytes(contents.read(1), "little")
+        cmd = int.from_bytes(contents.read(1), "little")
+        code = contents.read()
+
+        if manufac in sysex_brand: brandname = sysex_brand[manufac]+' ('+str(manufac)+')'
+        else: brandname = 'unknonwn ('+str(hex(manufac))+')'
+
+        print('[debug] Manu:',brandname, hex(manufac))
+        print('[debug] Model:',model, hex(model))
+        print('[debug] Dev:',devid, hex(devid))
+        print('[debug] CMD:',cmd, hex(cmd))
+        print('[debug] Data:',code.hex())
+
+        if manufac == 65:
+            # Device ----------------------------- SC-88/Pro -----------------------------
+            if [model, devid] == [16, 66]:
+                if 'sc88' not in sysexvals: sysexvals['sc88'] = {}
+                if cmd == 18:
+                    sc88data = {}
+                    sc88data['address'] = code[0:3].hex()
+                    sc88data['data'] = code[3]
+                    print('test -------', code[0:3].hex(), code[3])
+                    if time not in sysexvals['sc88']: sysexvals['sc88'][time] = []
+                    sysexvals['sc88'][time].append(sc88data)
+
+def get_sysexvals():
+    return sysexvals
--- plugin_input/midi.py
+++ plugin_input/midi.py
@@ -7,6 +7,7 @@ import json
 import mido
 from mido import MidiFile
 from functions import note_convert
+from functions import midi_sysex
 
 MIDIInstNames = ["Acoustic Grand Piano", "Bright Acoustic Piano", "Electric Grand Piano", "Honky-tonk Piano", "Electric Piano 1", "Electric Piano 2", "Harpsichord", "Clavi", "Celesta", "Glockenspiel", "Music Box", "Vibraphone", "Marimba", "Xylophone", "Tubular Bells", "Dulcimer", "Drawbar Organ", "Percussive Organ", "Rock Organ", "Church Organ", "Reed Organ", "Accordion", "Harmonica", "Tango Accordion", "Acoustic Guitar (nylon)", "Acoustic Guitar (steel)", "Electric Guitar (jazz)", "Electric Guitar (clean)", "Electric Guitar (muted)", "Overdriven Guitar", "Distortion Guitar", "Guitar harmonics", "Acoustic Bass", "Electric Bass (finger)", "Electric Bass (pick)", "Fretless Bass", "Slap Bass 1", "Slap Bass 2", "Synth Bass 1", "Synth Bass 2", "Violin", "Viola", "Cello", "Contrabass", "Tremolo Strings", "Pizzicato Strings", "Orchestral Harp", "Timpani", "String Ensemble 1", "String Ensemble 2", "SynthStrings 1", "SynthStrings 2", "Choir Aahs", "Voice Oohs", "Synth Voice", "Orchestra Hit", "Trumpet", "Trombone", "Tuba", "Muted Trumpet", "French Horn", "Brass Section", "SynthBrass 1", "SynthBrass 2", "Soprano Sax", "Alto Sax", "Tenor Sax", "Baritone Sax", "Oboe", "English Horn", "Bassoon", "Clarinet", "Piccolo", "Flute", "Recorder", "Pan Flute", "Blown Bottle", "Shakuhachi", "Whistle", "Ocarina", "Lead 1 (square)", "Lead 2 (sawtooth)", "Lead 3 (calliope)", "Lead 4 (chiff)", "Lead 5 (charang)", "Lead 6 (voice)", "Lead 7 (fifths)", "Lead 8 (bass + lead)", "Pad 1 (new age)", "Pad 2 (warm)", "Pad 3 (polysynth)", "Pad 4 (choir)", "Pad 5 (bowed)", "Pad 6 (metallic)", "Pad 7 (halo)", "Pad 8 (sweep)", "FX 1 (rain)", "FX 2 (soundtrack)", "FX 3 (crystal)", "FX 4 (atmosphere)", "FX 5 (brightness)", "FX 6 (goblins)", "FX 7 (echoes)", "FX 8 (sci-fi)", "Sitar", "Banjo", "Shamisen", "Koto", "Kalimba", "Bag pipe", "Fiddle", "Shanai", "Tinkle Bell", "Agogo", "Steel Drums", "Woodblock", "Taiko Drum", "Melodic Tom", "Synth Drum", "Reverse Cymbal", "Guitar Fret Noise", "Breath Noise", "Seashore", "Bird Tweet", "Telephone Ring", "Helicopter", "Applause", "Gunshot"]
 MIDIDrumNames = {0:'Drums', 8:'Room Drums', 16:'Power Drums', 24:'Elec Drums', 25:'TR808 Drums', 32:'Jazz Drums', 40:'Brush Drums', 48:'Orchestra Drums', 56:'Sound FX', 127:'MT-32 Drums'}
@@ -272,8 +273,6 @@ MIDIInstColors = [[0.10, 0.11, 0.11], #Acoustic Grand Piano
 
 
 
-
-
 def addtoalltables(contents):
     global t_chan_timednote
     for TimedNoteChannel in t_chan_timednote:
@@ -347,6 +346,7 @@ class input_midi(plugin_input.base):
                 t_chan_timednote[midi_channum].append('instrument;' + str(0))
 
             time_signature_pos = 0
+            sysex_pos = 0
 
             for msg in track:
                 if msg.type == 'track_name' and cmd_before_note == 1:
@@ -397,10 +397,9 @@ class input_midi(plugin_input.base):
                     if t_chan_used[msg.channel] == 0:
                         t_chan_initial[msg.channel][msg.control] = msg.value
 
-                #print(msg)
-                #if msg.type == 'control_change':
-
-
+                #if msg.type == 'sysex':
+                #    sysex_pos += msg.time
+                #    midi_sysex.parse_sysex(msg, sysex_pos)
 
             for midi_channum in range(16):
                 s_chan_initial = t_chan_initial[midi_channum]
@@ -448,6 +447,9 @@ class input_midi(plugin_input.base):
                     if cvpj_trackid not in cvpj_l_instrumentsorder:
                         cvpj_l_instruments[cvpj_trackid] = cvpj_trackdata
                         cvpj_l_instrumentsorder.append(cvpj_trackid)
+
+
+        #print(midi_sysex.get_sysexvals())
 
         cvpj_l_fxrack["0"] = {}
         cvpj_l_fxrack["0"]["name"] = "Master"

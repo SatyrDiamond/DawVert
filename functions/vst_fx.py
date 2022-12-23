e5946fc83de05e188ed53f691c83bb1148f095d7
Colby Ray <colbydray@gmail.com>
2022-12-22 19:09:01 -0500

    vst

---
+++ functions/vst_fx.py
@@ -0,0 +1,28 @@
+# -------------------- wolfshaper --------------------
+
+def wolfshaper_init():
+	global wolfshapergraph
+	global wolfshaperparams
+	wolfshapergraph = {}
+	wolfshapergraph['graph'] = ''
+	wolfshaperparams = {}
+	wolfshaperparams['pregain'] = 2.000000
+	wolfshaperparams['wet'] = 1.000000
+	wolfshaperparams['postgain'] = 1.000000
+	wolfshaperparams['removedc'] = 1.000000
+	wolfshaperparams['oversample'] = 0.000000
+	wolfshaperparams['bipolarmode'] = 0.000000
+	wolfshaperparams['warptype'] = 0.000000
+	wolfshaperparams['warpamount'] = 0.000000
+	wolfshaperparams['vwarptype'] = 0.000000
+	wolfshaperparams['vwarpamount'] = 0.000000
+def wolfshaper_setvalue(name, value):
+	global wolfshaperparams
+	wolfshaperparams[name] = value
+def wolfshaper_addpoint(posX,posY,tension,pointtype):
+	global wolfshapergraph
+	wolfshapergraph['graph'] += float.hex(posX)+','+float.hex(posY)+','+float.hex(tension)+','+str(pointtype)+';'
+def wolfshaper_get():
+	global wolfshapergraph
+	global wolfshaperparams
+	return [wolfshapergraph, wolfshaperparams]

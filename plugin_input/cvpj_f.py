e91ce25c79d66749ec507edbde24dfdabcd68475
SatyrDiamond <satyrdiamond@gmail.com>
2023-01-01 04:10:29 -0500

    cvpj_f

---
+++ plugin_input/cvpj_f.py
@@ -0,0 +1,25 @@
+# SPDX-FileCopyrightText: 2022 SatyrDiamond
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from functions import data_bytes
+import plugin_input
+import json
+
+class input_cvpj_f(plugin_input.base):
+    def __init__(self): pass
+    def getshortname(self): return 'cvpj_f'
+    def getname(self): return 'DEBUG, cvpj_f'
+    def gettype(self): return 'f'
+    def supported_autodetect(self): return True
+    def detect(self, input_file):
+        bytestream = open(input_file, 'rb')
+        bytestream.seek(0)
+        bytesdata = bytestream.read(12)
+        if bytesdata == b'CONVPROJ___F': return True
+        else: return False
+    def parse(self, input_file, extra_param):
+        bytestream = open(input_file, 'r')
+        file_data = bytestream.read()
+        data = ''.join(file_data.split('\n')[1:])
+        return data
+

2ba82f8a3fc64e718a1ecdd436306dc338124fb6
Colby Ray <colbydray@gmail.com>
2023-01-01 04:10:29 -0500

    cvpj_f

---
+++ plugin_output/cvpj_f.py
@@ -0,0 +1,16 @@
+# SPDX-FileCopyrightText: 2022 Colby Ray
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import plugin_output
+import json
+
+class output_cvpj_f(plugin_output.base):
+    def __init__(self): pass
+    def getname(self): return 'DEBUG'
+    def getshortname(self): return 'cvpj_f'
+    def gettype(self): return 'f'
+    def parse(self, convproj_json, output_file):
+        projJ = json.loads(convproj_json)
+        with open(output_file, "w") as fileout:
+            fileout.write("CONVPROJ___F\n")
+            json.dump(projJ, fileout, indent=4, sort_keys=True)

086002d9c591cd45cfa09be8df5d0ded9424656c
Colby Ray <colbydray@gmail.com>
2023-01-02 13:31:56 -0500

    simsynth to vital

--- functions/native_fl_inst.py
+++ functions/native_fl_inst.py
@@ -3,8 +3,13 @@ import struct
 from functions import data_bytes
 from functions import list_vst
 from functions import params_vst
+from functions import params_vital
+from functions import params_vital_wavetable
 
 simsynth_shapes = {0.4: 'noise', 0.3: 'sine', 0.2: 'square', 0.1: 'saw', 0.0: 'triangle'}
+
+def simsynth_time(value): return pow(value*2, 3)
+def simsynth_2time(value): return pow(value*2, 3)
 
 def convert(instdata):
 	pluginname = instdata['plugin']
@@ -150,58 +155,81 @@ def convert(instdata):
 		osc2_pw, osc2_crs, osc2_fine, osc2_lvl, osc2_lfo, osc2_env, osc2_shape = struct.unpack('ddddddd', fl_plugstr.read(56))
 		osc3_pw, osc3_crs, osc3_fine, osc3_lvl, osc3_lfo, osc3_env, osc3_shape = struct.unpack('ddddddd', fl_plugstr.read(56))
 		lfo_del, lfo_rate, unused, lfo_shape = struct.unpack('dddd', fl_plugstr.read(32))
-		UNK, svf_cut, svf_emph, svf_env = struct.unpack('dddd', fl_plugstr.read(32))
-		svf_lfo, svf_kb, UNK, svf_high = struct.unpack('dddd', fl_plugstr.read(32))
-		svf_band, UNK, amp_att, amp_dec = struct.unpack('dddd', fl_plugstr.read(32))
-		amp_sus, amp_rel, amp_lvl, UNK = struct.unpack('dddd', fl_plugstr.read(32))
+		UNK1, svf_cut, svf_emph, svf_env = struct.unpack('dddd', fl_plugstr.read(32))
+		svf_lfo, svf_kb, UNK2, svf_high = struct.unpack('dddd', fl_plugstr.read(32))
+		svf_band, UNK3, amp_att, amp_dec = struct.unpack('dddd', fl_plugstr.read(32))
+		amp_sus, amp_rel, amp_lvl, UNK4 = struct.unpack('dddd', fl_plugstr.read(32))
 		svf_att, svf_dec, svf_sus, svf_rel = struct.unpack('dddd', fl_plugstr.read(32))
 		fl_plugstr.read(64)
 		fl_plugstr.read(12)
 		osc1_on, osc1_o1, osc1_o2, osc1_warm = struct.unpack('IIII', fl_plugstr.read(16))
 		osc2_on, osc2_o1, osc2_o2, osc2_warm = struct.unpack('IIII', fl_plugstr.read(16))
 		osc3_on, osc3_o1, osc3_o2, osc3_warm = struct.unpack('IIII', fl_plugstr.read(16))
-		lfo_on, lfo_retrugger, svf_on, UNK = struct.unpack('IIII', fl_plugstr.read(16))
-		lfo_trackamp, UNK, chorus_on, UNK = struct.unpack('IIII', fl_plugstr.read(16))
+		lfo_on, lfo_retrugger, svf_on, UNK5 = struct.unpack('IIII', fl_plugstr.read(16))
+		lfo_trackamp, UNK6, chorus_on, UNK7 = struct.unpack('IIII', fl_plugstr.read(16))
 
-		#params_vital.create()
+		params_vital.create()
 
-		#vital_osc1_shape = []
-		#for num in range(2048): vital_osc1_shape.append(tripleoct(num/2048, simsynth_shapes[osc1_shape], osc1_pw, osc1_o1, osc1_o2))
-		#params_vital.replacewave(0, vital_osc1_shape)
-		#params_vital.setvalue('osc_1_on', osc1_on)
-		#params_vital.setvalue('osc_1_transpose', (osc1_crs-0.5)*48)
-		#params_vital.setvalue('osc_1_tune', (osc1_fine-0.5)*2)
-		#params_vital.setvalue('osc_1_level', osc1_lvl/2)
-		#if osc1_warm == 1:
-			#params_vital.setvalue('osc_1_unison_detune', 2.2)
-			#params_vital.setvalue('osc_1_unison_voices', 6)
+		# ------------ OSC 1 ------------
+		vital_osc1_shape = []
+		for num in range(2048): vital_osc1_shape.append(params_vital_wavetable.tripleoct(num/2048, simsynth_shapes[osc1_shape], osc1_pw, osc1_o1, osc1_o2))
+		params_vital.replacewave(0, vital_osc1_shape)
+		params_vital.setvalue('osc_1_on', osc1_on)
+		params_vital.setvalue('osc_1_transpose', (osc1_crs-0.5)*48)
+		params_vital.setvalue('osc_1_tune', (osc1_fine-0.5)*2)
+		params_vital.setvalue('osc_1_level', osc1_lvl)
+		if osc1_warm == 1:
+			params_vital.setvalue('osc_1_unison_detune', 2.2)
+			params_vital.setvalue('osc_1_unison_voices', 6)
 
-		#vital_osc2_shape = []
-		#for num in range(2048): vital_osc2_shape.append(tripleoct(num/2048, simsynth_shapes[osc2_shape], osc2_pw, osc2_o1, osc2_o2))
-		#params_vital.replacewave(1, vital_osc2_shape)
-		#params_vital.setvalue('osc_2_on', osc2_on)
-		#params_vital.setvalue('osc_2_transpose', (osc2_crs-0.5)*48)
-		#params_vital.setvalue('osc_2_tune', (osc2_fine-0.5)*2)
-		#params_vital.setvalue('osc_2_level', osc2_lvl/2)
-		#if osc2_warm == 1:
-			#params_vital.setvalue('osc_2_unison_detune', 2.2)
-			#params_vital.setvalue('osc_2_unison_voices', 6)
+		# ------------ OSC 2 ------------
+		vital_osc2_shape = []
+		for num in range(2048): vital_osc2_shape.append(params_vital_wavetable.tripleoct(num/2048, simsynth_shapes[osc2_shape], osc2_pw, osc2_o1, osc2_o2))
+		params_vital.replacewave(1, vital_osc2_shape)
+		params_vital.setvalue('osc_2_on', osc2_on)
+		params_vital.setvalue('osc_2_transpose', (osc2_crs-0.5)*48)
+		params_vital.setvalue('osc_2_tune', (osc2_fine-0.5)*2)
+		params_vital.setvalue('osc_2_level', osc2_lvl)
+		if osc2_warm == 1:
+			params_vital.setvalue('osc_2_unison_detune', 2.2)
+			params_vital.setvalue('osc_2_unison_voices', 6)
 
-		#vital_osc3_shape = []
-		#for num in range(2048): vital_osc3_shape.append(tripleoct(num/2048, simsynth_shapes[osc3_shape], osc3_pw, osc3_o1, osc3_o2))
-		#params_vital.replacewave(2, vital_osc3_shape)
-		#params_vital.setvalue('osc_3_on', osc3_on)
-		#params_vital.setvalue('osc_3_transpose', (osc3_crs-0.5)*48)
-		#params_vital.setvalue('osc_3_tune', (osc3_fine-0.5)*2)
-		#params_vital.setvalue('osc_3_level', osc3_lvl/2)
-		#if osc3_warm == 1:
-			#params_vital.setvalue('osc_3_unison_detune', 2.2)
-			#params_vital.setvalue('osc_3_unison_voices', 6)
+		# ------------ OSC 3 ------------
+		vital_osc3_shape = []
+		for num in range(2048): vital_osc3_shape.append(params_vital_wavetable.tripleoct(num/2048, simsynth_shapes[osc3_shape], osc3_pw, osc3_o1, osc3_o2))
+		params_vital.replacewave(2, vital_osc3_shape)
+		params_vital.setvalue('osc_3_on', osc3_on)
+		params_vital.setvalue('osc_3_transpose', (osc3_crs-0.5)*48)
+		params_vital.setvalue('osc_3_tune', (osc3_fine-0.5)*2)
+		params_vital.setvalue('osc_3_level', osc3_lvl)
+		if osc3_warm == 1:
+			params_vital.setvalue('osc_3_unison_detune', 2.2)
+			params_vital.setvalue('osc_3_unison_voices', 6)
 
-		#params_vital.setvalue('env_1_attack', ss_asdr(amp_att)*4)
-		#params_vital.setvalue('env_1_decay', ss_asdr(amp_dec)*4)
-		#params_vital.setvalue('env_1_sustain', amp_sus)
-		#params_vital.setvalue('env_1_release', ss_asdr(amp_rel)*4)
+		# ------------ AMP ------------
+		params_vital.setvalue_timed('env_1_attack', simsynth_time(amp_att)*3.5)
+		params_vital.setvalue_timed('env_1_decay', simsynth_2time(amp_dec)*3.5)
+		params_vital.setvalue('env_1_sustain', amp_sus)
+		params_vital.setvalue_timed('env_1_release', simsynth_2time(amp_rel)*3.5)
 
-		#vitaldata = params_vital.getdata()
-		#list_vst.replace_data(instdata, 'Vital', vitaldata.encode('utf-8'))
+		# ------------ SVF ------------
+		params_vital.setvalue_timed('env_2_attack', simsynth_time(svf_att)*7)
+		params_vital.setvalue_timed('env_2_decay', simsynth_2time(svf_dec)*7)
+		params_vital.setvalue('env_2_sustain', svf_sus)
+		params_vital.setvalue_timed('env_2_release', simsynth_2time(svf_rel)*7)
+
+		outfilter = 80
+		outfilter += (svf_cut-0.5)*10
+		outfilter += (svf_kb-0.5)*10
+
+		params_vital.setvalue('filter_fx_resonance', svf_emph*0.8)
+		params_vital.setvalue('filter_fx_cutoff', outfilter)
+		params_vital.setvalue('filter_fx_on', 1)
+		params_vital.set_modulation(1, 'env_2', 'filter_fx_cutoff', svf_env*0.34, 0, 0, 0, 0)
+
+		vitaldata = params_vital.getdata()
+		list_vst.replace_data(instdata, 'Vital', vitaldata.encode('utf-8'))
+
+		# ------------ Chorus ------------
+		if chorus_on == 1: params_vital.setvalue('chorus_on', 1.0)
+		

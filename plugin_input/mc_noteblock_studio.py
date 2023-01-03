5d8ff441e4e1b8270e5c6755cea0e0c3a90e7f87
SatyrDiamond <satyrdiamond@gmail.com>
2023-01-03 06:33:37 -0500

    mc_noteblock_studio add layer name

--- plugin_input/mc_noteblock_studio.py
+++ plugin_input/mc_noteblock_studio.py
@@ -8,25 +8,18 @@ import math
 
 def getstring(nbs_file):
     stringlen = int.from_bytes(nbs_file.read(4), "little")
-    if stringlen != 0:
-        return nbs_file.read(stringlen).decode("utf-8")
+    if stringlen != 0: return nbs_file.read(stringlen).decode("utf-8")
     else: return None
 
 def nbs_parsekey(nbs_file, nbs_newformat):
     nbs_inst = int.from_bytes(nbs_file.read(1), "little")
-    #print('I:'+str(nbs_inst), end=' ')
     nbs_key = int.from_bytes(nbs_file.read(1), "little")-51
-    #print('K:'+str(nbs_key), end=' ')
     if nbs_newformat == 1:
         nbs_velocity = int.from_bytes(nbs_file.read(1), "little")
-        #print('V:'+str(nbs_velocity), end=' ')
         nbs_pan = int.from_bytes(nbs_file.read(1), "little")
-        #print('P:'+str(nbs_pan), end=' ')
         nbs_pitch = int.from_bytes(nbs_file.read(2), "little", signed="True")
-        #print('F:'+str(nbs_pitch))
         return [nbs_key, nbs_inst, [nbs_velocity, nbs_pan, nbs_pitch]]
-    else:
-        return [nbs_key, nbs_inst, None]
+    else: return [nbs_key, nbs_inst, None]
 
 class input_gt_mnbs(plugin_input.base):
     def __init__(self): pass
@@ -44,6 +37,9 @@ class input_gt_mnbs(plugin_input.base):
         song_message = ""
 
         nbs_file = open(input_file, 'rb')
+        nbs_file.seek(0,2)
+        nbs_len = nbs_file.tell()
+        nbs_file.seek(0)
 
         noteblockinst = {}
         noteblockinst[0] = "Piano (Air)"
@@ -92,19 +88,6 @@ class input_gt_mnbs(plugin_input.base):
         gmmidi[10] = 12
         gmmidi[14] = 106
 
-        for instnum in range(16):
-            instid = 'NoteBlock'+str(instnum)
-            cvpj_l_instruments[instid] = {}
-            cvpj_l_instruments[instid]['name'] = noteblockinst[instnum]
-            cvpj_l_instruments[instid]['color'] = colors[instnum]
-            cvpj_l_instruments[instid]['instdata'] = {}
-            if instnum in gmmidi:
-                cvpj_l_instruments[instid]['instdata']['plugin'] = 'general-midi'
-                cvpj_l_instruments[instid]['instdata']['plugindata'] = {'bank':0, 'inst':gmmidi[instnum]-1}
-            else:
-                cvpj_l_instruments[instid]['instdata']['plugin'] = 'none'
-            cvpj_l_instrumentsorder.append(instid)
-
         # PART 1: HEADER
         nbs_startbyte = int.from_bytes(nbs_file.read(2), "little")
         if nbs_startbyte == 0: 
@@ -126,7 +109,6 @@ class input_gt_mnbs(plugin_input.base):
         for playlistid in range(nbs_layercount):
             cvpj_l_playlist[playlistid+1] = {}
             cvpj_l_playlist[playlistid+1]['color'] = [0.23, 0.23, 0.23]
-            cvpj_l_playlist[playlistid+1]['name'] = 'Layer ' + str(playlistid+1)
             cvpj_l_playlist[playlistid+1]['placements'] = []
             nbs_notes[playlistid+1] = {}
 
@@ -167,7 +149,7 @@ class input_gt_mnbs(plugin_input.base):
         # PART 2: NOTE BLOCKS
         notes_done = 0
         note_tick = -1
-        split_duration = timesig_numerator*4
+        split_duration = (timesig_numerator*4)*2
         print_notes = 0
 
         while notes_done == 0:
@@ -179,8 +161,6 @@ class input_gt_mnbs(plugin_input.base):
                     note_layer = nbs_jump_layer
                     layer_done = 0
                     while layer_done == 0:
-                        #print('T:'+str(note_tick), end=' ')
-                        #print('L:'+str(note_layer), end=' ')
                         nbs_notes[note_layer][note_tick] = nbs_parsekey(nbs_file, nbs_newformat)
                         print_notes += 1
                         nbs_jump_layer = int.from_bytes(nbs_file.read(2), "little")
@@ -192,16 +172,14 @@ class input_gt_mnbs(plugin_input.base):
 
         for nbs_layer in nbs_notes:
             nbs_layerdata = nbs_notes[nbs_layer]
-            #print(nbs_layer, len(nbs_layerdata))
             layer_placements = {}
             for note in nbs_layerdata:
                 nbs_notedata = nbs_layerdata[note]
                 placementnum = math.floor(note/split_duration)*split_duration
-                if placementnum not in layer_placements:
-                    layer_placements[placementnum] = []
+                if placementnum not in layer_placements: layer_placements[placementnum] = []
                 cvpj_notedata = {}
                 cvpj_notedata['position'] = note-placementnum
-                cvpj_notedata['duration'] = 1
+                cvpj_notedata['duration'] = 2
                 cvpj_notedata['instrument'] = 'NoteBlock'+str(nbs_notedata[1])
                 cvpj_notedata['key'] = nbs_notedata[0]
                 if nbs_notedata[2] != None:
@@ -209,7 +187,6 @@ class input_gt_mnbs(plugin_input.base):
                     cvpj_notedata['pan'] = (nbs_notedata[2][1]/100)-1
                     cvpj_notedata['finepitch'] = nbs_notedata[2][2]
                 layer_placements[placementnum].append(cvpj_notedata)
-                #rint(placementnum, note-placementnum, nbs_notedata)
             for placenum in layer_placements:
                 cvpj_pl_data = {}
                 cvpj_pl_data['type'] = 'instruments'
@@ -218,6 +195,25 @@ class input_gt_mnbs(plugin_input.base):
                 cvpj_pl_data['notelist'] = layer_placements[placenum]
                 cvpj_l_playlist[nbs_layer]['placements'].append(cvpj_pl_data)
             print('[input-mnbs] Layer '+str(nbs_layer)+' Placements: '+str(len(cvpj_l_playlist[nbs_layer]['placements'])))
+
+        # PART 3: LAYERS
+        if nbs_file.tell() <= nbs_len:
+            for layernum in range(nbs_layercount):
+                cvpj_l_playlist[layernum+1]['name'] = getstring(nbs_file)
+                if nbs_newformat == 1: nbs_file.read(3)
+
+        # OUTPUT
+        for instnum in range(16):
+            instid = 'NoteBlock'+str(instnum)
+            cvpj_l_instruments[instid] = {}
+            cvpj_l_instruments[instid]['name'] = noteblockinst[instnum]
+            cvpj_l_instruments[instid]['color'] = colors[instnum]
+            cvpj_l_instruments[instid]['instdata'] = {}
+            if instnum in gmmidi:
+                cvpj_l_instruments[instid]['instdata']['plugin'] = 'general-midi'
+                cvpj_l_instruments[instid]['instdata']['plugindata'] = {'bank':0, 'inst':gmmidi[instnum]-1}
+            else: cvpj_l_instruments[instid]['instdata']['plugin'] = 'none'
+            cvpj_l_instrumentsorder.append(instid)
 
         cvpj_l['message'] = {}
         cvpj_l['message']['type'] = 'text'

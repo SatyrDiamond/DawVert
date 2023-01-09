4cd0c948dba13c60e2d158a181b8595efd20a426
SatyrDiamond <satyrdiamond@gmail.com>
2023-01-09 16:59:48 -0500

    jummbox

---
+++ functions/native_jummbox.py
@@ -0,0 +1,51 @@
+import base64
+import struct
+from functions import data_bytes
+from functions import list_vst
+from functions import params_vst
+from functions import params_vital
+from functions import params_vital_wavetable
+
+rawChipWaves = {}
+rawChipWaves["rounded"] = {"expression": 0.94, "samples": [0,0.2,0.4,0.5,0.6,0.7,0.8,0.85,0.9,0.95,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0.95,0.9,0.85,0.8,0.7,0.6,0.5,0.4,0.2,0,-0.2,-0.4,-0.5,-0.6,-0.7,-0.8,-0.85,-0.9,-0.95,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-0.95,-0.9,-0.85,-0.8,-0.7,-0.6,-0.5,-0.4,-0.2]}
+rawChipWaves["triangle"] = {"expression": 1, "samples": [1/15,0.2,5/15,7/15,0.6,11/15,13/15,1,1,13/15,11/15,0.6,7/15,5/15,0.2,1/15,-1/15,-0.2,-5/15,-7/15,-0.6,-11/15,-13/15,-1,-1,-13/15,-11/15,-0.6,-7/15,-5/15,-0.2,-1/15]}
+rawChipWaves["square"] = {"expression": 0.5, "samples": [1,-1]}
+rawChipWaves["1/4 pulse"] = {"expression": 0.5, "samples": [1,-1,-1,-1]}
+rawChipWaves["1/8 pulse"] = {"expression": 0.5, "samples": [1,-1,-1,-1,-1,-1,-1,-1]}
+rawChipWaves["sawtooth"] = {"expression": 0.65, "samples": [1/31,3/31,5/31,7/31,9/31,11/31,13/31,15/31,17/31,19/31,21/31,23/31,25/31,27/31,29/31,1,-1,-29/31,-27/31,-25/31,-23/31,-21/31,-19/31,-17/31,-15/31,-13/31,-11/31,-9/31,-7/31,-5/31,-3/31,-1/31]}
+rawChipWaves["double saw"] = {"expression": 0.5, "samples": [0,-0.2,-0.4,-0.6,-0.8,-1,1,-0.8,-0.6,-0.4,-0.2,1,0.8,0.6,0.4,0.2]}
+rawChipWaves["double pulse"] = {"expression": 0.4, "samples": [1,1,1,1,1,-1,-1,-1,1,1,1,1,-1,-1,-1,-1]}
+rawChipWaves["spiky"] = {"expression": 0.4, "samples": [1,-1,1,-1,1,0]}
+rawChipWaves["sine"] = {"expression": 0.88, "samples": [8,9,11,12,13,14,15,15,15,15,14,14,13,11,10,9,7,6,4,3,2,1,0,0,0,0,1,1,2,4,5,6]}
+rawChipWaves["flute"] = {"expression": 0.8, "samples": [3,4,6,8,10,11,13,14,15,15,14,13,11,8,5,3]}
+rawChipWaves["harp"] = {"expression": 0.8, "samples": [0,3,3,3,4,5,5,6,7,8,9,11,11,13,13,15,15,14,12,11,10,9,8,7,7,5,4,3,2,1,0,0]}
+rawChipWaves["sharp clarinet"] = {"expression": 0.38, "samples": [0,0,0,1,1,8,8,9,9,9,8,8,8,8,8,9,9,7,9,9,10,4,0,0,0,0,0,0,0,0,0,0]}
+rawChipWaves["soft clarinet"] = {"expression": 0.45, "samples": [0,1,5,8,9,9,9,9,9,9,9,11,11,12,13,12,10,9,7,6,4,3,3,3,1,1,1,1,1,1,1,1]}
+rawChipWaves["alto sax"] = {"expression": 0.3, "samples": [5,5,6,4,3,6,8,7,2,1,5,6,5,4,5,7,9,11,13,14,14,14,14,13,10,8,7,7,4,3,4,2]}
+rawChipWaves["bassoon"] = {"expression": 0.35, "samples": [9,9,7,6,5,4,4,4,4,5,7,8,9,10,11,13,13,11,10,9,7,6,4,2,1,1,1,2,2,5,11,14]}
+rawChipWaves["trumpet"] = {"expression": 0.22, "samples": [10,11,8,6,5,5,5,6,7,7,7,7,6,6,7,7,7,7,7,6,6,6,6,6,6,6,6,7,8,9,11,14]}
+rawChipWaves["electric guitar"] = {"expression": 0.2, "samples": [11,12,12,10,6,6,8,0,2,4,8,10,9,10,1,7,11,3,6,6,8,13,14,2,0,12,8,4,13,11,10,13]}
+rawChipWaves["organ"] = {"expression": 0.2, "samples": [11,10,12,11,14,7,5,5,12,10,10,9,12,6,4,5,13,12,12,10,12,5,2,2,8,6,6,5,8,3,2,1]}
+rawChipWaves["pan flute"] = {"expression": 0.35, "samples": [1,4,7,6,7,9,7,7,11,12,13,15,13,11,11,12,13,10,7,5,3,6,10,7,3,3,1,0,1,0,1,0]}
+rawChipWaves["glitch"] = {"expression": 0.5, "samples": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,-1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,-1,-1,1,1,1,1,1,1,1,1,1,1,1,1,1,-1,-1,-1,1,1,1,1,1,1,1,1,1,1,1,1,-1,-1,-1,-1,1,1,1,1,1,1,1,1,1,1,1,-1,-1,-1,-1,-1,1,1,1,1,1,1,1,1,1,1,-1,-1,-1,-1,-1,-1,1,1,1,1,1,1,1,1,1,-1,-1,-1,-1,-1,-1,-1,1,1,1,1,1,1,1,1,-1,-1,-1,-1,-1,-1,-1,-1,1,1,1,1,1,1,1,1,1,-1,-1,-1,-1,-1,-1,-1,1,1,1,1,1,1,1,1,1,1,-1,-1,-1,-1,-1,-1,1,1,1,1,1,1,1,1,1,1,1,-1,-1,-1,-1,-1,1,1,1,1,1,1,1,1,1,1,1,1,-1,-1,-1,-1,1,1,1,1,1,1,1,1,1,1,1,1,1,-1,-1,-1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,-1,-1]}
+
+def convert(instdata):
+	pluginname = instdata['plugin']
+	plugindata = instdata['plugindata']
+	bb_type = plugindata['type']
+	bb_data = plugindata['data']
+	print(bb_data)
+
+	if bb_data['type'] == 'chip':
+
+		bb_sample = rawChipWaves[bb_data['wave']]['samples']
+		dur_bb = len(bb_sample)
+
+		params_vital.create()
+		params_vital.setvalue('osc_1_on', 1)
+
+		bb_sample = rawChipWaves[bb_data['wave']]['samples']
+		params_vital.replacewave(0, params_vital_wavetable.resizewave(bb_sample))
+
+		vitaldata = params_vital.getdata()
+		list_vst.replace_data(instdata, 'Vital', vitaldata.encode('utf-8'))
---
+++ plugin_input/jummbox.py
@@ -0,0 +1,193 @@
+# SPDX-FileCopyrightText: 2022 Colby Ray
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from functions import data_bytes
+import plugin_input
+import json
+
+global colornum_p
+global colornum_d
+colornum_p = 0
+colornum_d = 0
+
+colors_pitch = [
+[0.00, 0.60, 0.63],
+[0.63, 0.63, 0.00],
+[0.78, 0.31, 0.00],
+[0.00, 0.63, 0.00],
+[0.82, 0.13, 0.82],
+[0.47, 0.47, 0.69],
+[0.54, 0.63, 0.00],
+[0.87, 0.00, 0.10],
+[0.00, 0.63, 0.44],
+[0.57, 0.12, 1.00]]
+
+colors_drums = [
+[0.44, 0.44, 0.44],
+[0.60, 0.40, 0.20],
+[0.29, 0.43, 0.56],
+[0.48, 0.31, 0.60],
+[0.38, 0.47, 0.22]]
+
+inst_names = {
+"chip": "Chip Wave",
+"PWM": "Pulse Width",
+"harmonics": "Harmonics",
+"Picked String": "Picked String",
+"spectrum": "Spectrum",
+"FM": "FM",
+"custom chip": "Custom Chip",
+"noise": "Basic Noise",
+"drumset": "Drum Set"
+}
+
+def getcolor_p():
+    global colornum_p
+    out_color = colors_pitch[colornum_p]
+    colornum_p += 1
+    if colornum_p == 10: colornum_p = 0
+    return out_color
+
+def getcolor_d():
+    global colornum_d
+    out_color = colors_drums[colornum_d]
+    colornum_d += 1
+    if colornum_d == 5: colornum_d = 0
+    return out_color
+
+def calcval(value):
+    global jummbox_beatsPerBar
+    global jummbox_ticksPerBeat
+    return (value*(jummbox_beatsPerBar/jummbox_ticksPerBeat))/2
+
+def parse_instrument(cvpj_inst, bb_instrument, bb_type):
+    cvpj_inst['instdata'] = {}
+    cvpj_inst['instdata']['plugin'] = 'jummbox-single'
+    cvpj_inst['instdata']['plugindata'] = {}
+    cvpj_inst['instdata']['plugindata']['type'] = bb_type
+    cvpj_inst['instdata']['plugindata']['data'] = bb_instrument
+
+    bb_type = bb_instrument['type']
+    bb_volume = bb_instrument['volume']
+    bb_pan = bb_instrument['pan']
+    cvpj_inst['name'] = inst_names[bb_type]
+    cvpj_inst["vol"] = (bb_volume/50)+0.5
+    cvpj_inst["pan"] = bb_pan/100
+
+def parse_channel(channeldata, channum):
+    global cvpj_l_instruments
+    global cvpj_l_instrumentsorder
+    global cvpj_l_notelistindex
+    global cvpj_l_playlist
+    global jummbox_notesize
+    global jummbox_beatsPerBar
+    global jummbox_ticksPerBeat
+
+    bb_color = None
+    bb_type = channeldata['type']
+    bb_instruments = channeldata['instruments']
+    bb_patterns = channeldata['patterns']
+    bb_sequence = channeldata['sequence']
+
+    patterncount = 0
+    if bb_type == 'pitch' or bb_type == 'drum':
+
+        if bb_type == 'pitch': bb_color = getcolor_p()
+        if bb_type == 'drum': bb_color = getcolor_d()
+        cvpj_inst = {}
+        cvpj_inst["pan"] = 0.0
+        cvpj_inst['name'] = 'Channel '+str(channum)
+        cvpj_inst["vol"] = 1.0
+        parse_instrument(cvpj_inst, bb_instruments[0], bb_type)
+        if bb_color != None: cvpj_inst['color'] = bb_color
+        cvpj_l_instruments[str(channum)] = cvpj_inst
+        cvpj_l_instrumentsorder.append(str(channum))
+
+        for pattern in bb_patterns:
+            nid_name = str(channum)+'_'+str(patterncount+1)
+            cvpj_notelist = []
+            notes = pattern['notes']
+            if notes != []:
+                #print('------ NOTE LIST ------', channum+1, patterncount+1)
+                for note in notes:
+                    points = note['points']
+                    pitches = note['pitches']
+                    cvpj_note_pos = (points[-1]['tick'] - points[0]['tick'])
+                    for pitch in pitches:
+                        cvpj_note = {}
+                        cvpj_note['position'] = calcval(points[0]['tick'])
+                        cvpj_note['duration'] = calcval(cvpj_note_pos)
+                        cvpj_note['key'] = pitch-48
+                        cvpj_note['vol'] = points[0]['volume']/100
+                        cvpj_note['instrument'] = str(channum)
+                        cvpj_notelist.append(cvpj_note)
+
+                cvpj_l_notelistindex[nid_name] = {}
+                cvpj_l_notelistindex[nid_name]['notelist'] = cvpj_notelist
+                cvpj_l_notelistindex[nid_name]['color'] = bb_color
+                cvpj_l_notelistindex[nid_name]['name'] = 'Ch '+str(channum+1)+', Pat '+str(patterncount+1)
+            patterncount += 1
+
+        cvpj_l_playlist[str(channum)] = {}
+        cvpj_l_playlist[str(channum)]['color'] = bb_color
+        cvpj_l_playlist[str(channum)]['placements'] = []
+        if bb_type == 'drum': cvpj_l_playlist[str(channum)]['name'] = 'Drums'
+
+        sequencecount = 0
+        for bb_part in bb_sequence:
+            patid = str(channum)+'_'+str(bb_part)
+            cvpj_l_placement = {}
+            cvpj_l_placement['type'] = "instruments"
+            cvpj_l_placement['position'] = calcval(sequencecount*jummbox_notesize)
+            cvpj_l_placement['duration'] = calcval(1)
+            cvpj_l_placement['fromindex'] = patid
+            cvpj_l_playlist[str(channum)]['placements'].append(cvpj_l_placement)
+            sequencecount += 1
+
+
+
+class input_jummbox(plugin_input.base):
+    def __init__(self): pass
+    def is_dawvert_plugin(self): return 'input'
+    def getshortname(self): return 'jummbox'
+    def getname(self): return 'jummbox'
+    def gettype(self): return 'mi'
+    def supported_autodetect(self): return False
+    def parse(self, input_file, extra_param):
+        global cvpj_l_instruments
+        global cvpj_l_instrumentsorder
+        global cvpj_l_notelistindex
+        global cvpj_l_playlist
+        global jummbox_beatsPerBar
+        global jummbox_ticksPerBeat
+
+        bytestream = open(input_file, 'r', encoding='utf8')
+        jummbox_json = json.load(bytestream)
+
+        cvpj_l = {}
+        cvpj_l_instruments = {}
+        cvpj_l_instrumentsorder = []
+        cvpj_l_notelistindex = {}
+        cvpj_l_playlist = {}
+
+        jummbox_channels = jummbox_json['channels']
+        jummbox_beatsPerBar = jummbox_json['beatsPerBar']
+        jummbox_ticksPerBeat = jummbox_json['ticksPerBeat']
+        jummbox_beatsPerMinute = jummbox_json['beatsPerMinute']
+        jummbox_channels = jummbox_json['channels']
+
+        global jummbox_notesize
+        jummbox_notesize = jummbox_beatsPerBar*jummbox_ticksPerBeat
+
+        chancount = 1
+        for jummbox_channel in jummbox_channels:
+            parse_channel(jummbox_channel, chancount)
+            chancount += 1
+
+        cvpj_l['notelistindex'] = cvpj_l_notelistindex
+        cvpj_l['instruments'] = cvpj_l_instruments
+        cvpj_l['instrumentsorder'] = cvpj_l_instrumentsorder
+        cvpj_l['playlist'] = cvpj_l_playlist
+        cvpj_l['bpm'] = jummbox_beatsPerMinute
+
+        return json.dumps(cvpj_l)

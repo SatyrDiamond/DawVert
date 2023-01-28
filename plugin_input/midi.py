8f12e48e4be596c65295d14bf958c967d683daa9
SatyrDiamond <colbydray@gmail.com>
2023-01-28 02:07:47 -0500

    midi values

--- plugin_input/midi.py
+++ plugin_input/midi.py
@@ -9,139 +9,10 @@ from mido import MidiFile
 from functions import note_convert
 from functions import values
 
-MIDIDrumNames = {0:'Drums', 8:'Room Drums', 16:'Power Drums', 24:'Elec Drums', 25:'TR808 Drums', 32:'Jazz Drums', 40:'Brush Drums', 48:'Orchestra Drums', 56:'Sound FX', 127:'MT-32 Drums'}
-MIDIControllerName = {}
-MIDIControllerName[0] = "Bank Select"
-MIDIControllerName[1] = "Modulation Wheel"
-MIDIControllerName[2] = "Breath Controller"
-MIDIControllerName[3] = "_undefined"
-MIDIControllerName[4] = "Foot Pedal"
-MIDIControllerName[5] = "Portamento/Glide Time"
-MIDIControllerName[6] = "Data Entry (MSB)"
-MIDIControllerName[7] = "MIDI Volume"
-MIDIControllerName[8] = "Stereo Balance"
-MIDIControllerName[9] = "_undefined"
-MIDIControllerName[10] = "Pan"
-MIDIControllerName[11] = "Expression Pedal"
-MIDIControllerName[12] = "Effect Controller 1"
-MIDIControllerName[13] = "Effect Controller 2"
-MIDIControllerName[14] = "_undefined"
-MIDIControllerName[15] = "_undefined"
-MIDIControllerName[16] = "General Purpose Controller 1"
-MIDIControllerName[17] = "General Purpose Controller 2"
-MIDIControllerName[18] = "General Purpose Controller 3"
-MIDIControllerName[19] = "General Purpose Controller 4"
-MIDIControllerName[20] = "_undefined"
-MIDIControllerName[21] = "_undefined"
-MIDIControllerName[22] = "_undefined"
-MIDIControllerName[23] = "_undefined"
-MIDIControllerName[24] = "_undefined"
-MIDIControllerName[25] = "_undefined"
-MIDIControllerName[26] = "_undefined"
-MIDIControllerName[27] = "_undefined"
-MIDIControllerName[28] = "_undefined"
-MIDIControllerName[29] = "_undefined"
-MIDIControllerName[30] = "_undefined"
-MIDIControllerName[31] = "_undefined"
-MIDIControllerName[32] = "Bank Select"
-MIDIControllerName[33] = "Modulation Wheel"
-MIDIControllerName[34] = "Breath Controller"
-MIDIControllerName[35] = "_undefined"
-MIDIControllerName[36] = "Foot Pedal"
-MIDIControllerName[37] = "Portamento/Glide Time"
-MIDIControllerName[38] = "Data Entry"
-MIDIControllerName[39] = "Volume"
-MIDIControllerName[40] = "Balance"
-MIDIControllerName[41] = "_undefined"
-MIDIControllerName[42] = "Pan Position"
-MIDIControllerName[43] = "Expression"
-MIDIControllerName[44] = "Effect Control 1"
-MIDIControllerName[45] = "Effect Control 2"
-MIDIControllerName[46] = "_undefined"
-MIDIControllerName[47] = "_undefined"
-MIDIControllerName[48] = "_undefined"
-MIDIControllerName[49] = "_undefined"
-MIDIControllerName[50] = "_undefined"
-MIDIControllerName[51] = "_undefined"
-MIDIControllerName[52] = "_undefined"
-MIDIControllerName[53] = "_undefined"
-MIDIControllerName[54] = "_undefined"
-MIDIControllerName[55] = "_undefined"
-MIDIControllerName[56] = "_undefined"
-MIDIControllerName[57] = "_undefined"
-MIDIControllerName[58] = "_undefined"
-MIDIControllerName[59] = "_undefined"
-MIDIControllerName[60] = "_undefined"
-MIDIControllerName[61] = "_undefined"
-MIDIControllerName[62] = "_undefined"
-MIDIControllerName[63] = "_undefined"
-MIDIControllerName[64] = "Sustain Pedal On/Off"
-MIDIControllerName[65] = "Portamento/Glide On/Off Switch"
-MIDIControllerName[66] = "Sostenuto On/Off Switch"
-MIDIControllerName[67] = "Soft Pedal On/Off Switch"
-MIDIControllerName[68] = "Legato On/Off Switch"
-MIDIControllerName[69] = "Hold Pedal 2"
-MIDIControllerName[70] = "Sound Controller 1"
-MIDIControllerName[71] = "Sound Controller 2 - Filter Resonance"
-MIDIControllerName[72] = "Sound Controller 3 - Amp Envelope Decay"
-MIDIControllerName[73] = "Sound Controller 4 - Amp Envelope Attack"
-MIDIControllerName[74] = "Sound Controller 5 - Filter Cutoff"
-MIDIControllerName[75] = "Sound Controller 6"
-MIDIControllerName[76] = "Sound Controller 7"
-MIDIControllerName[77] = "Sound Controller 8"
-MIDIControllerName[78] = "Sound Controller 9"
-MIDIControllerName[79] = "Sound Controller 10"
-MIDIControllerName[80] = "General Purpose"
-MIDIControllerName[81] = "General Purpose"
-MIDIControllerName[82] = "General Purpose"
-MIDIControllerName[83] = "General Purpose"
-MIDIControllerName[84] = "_undefined"
-MIDIControllerName[85] = "_undefined"
-MIDIControllerName[86] = "_undefined"
-MIDIControllerName[87] = "_undefined"
-MIDIControllerName[88] = "_undefined"
-MIDIControllerName[89] = "_undefined"
-MIDIControllerName[90] = "_undefined"
-MIDIControllerName[91] = "Effect 1 Amount (Reverb)"
-MIDIControllerName[92] = "Effect 2 Amount (Tremelo)"
-MIDIControllerName[93] = "Effect 3 Amount (Chorus)"
-MIDIControllerName[94] = "Effect 4 Amount (Detuning)"
-MIDIControllerName[95] = "Effect 5 Amount (Phaser)"
-MIDIControllerName[96] = "Data Bound Increment (+1)"
-MIDIControllerName[97] = "Data Bound Decrement (-1)"
-MIDIControllerName[98] = "NRPN LSB"
-MIDIControllerName[99] = "NRPN MSB"
-MIDIControllerName[100] = "RPN LSB"
-MIDIControllerName[101] = "RPN MSB"
-MIDIControllerName[102] = "_undefined"
-MIDIControllerName[103] = "_undefined"
-MIDIControllerName[104] = "_undefined"
-MIDIControllerName[105] = "_undefined"
-MIDIControllerName[106] = "_undefined"
-MIDIControllerName[107] = "_undefined"
-MIDIControllerName[108] = "_undefined"
-MIDIControllerName[109] = "_undefined"
-MIDIControllerName[110] = "_undefined"
-MIDIControllerName[111] = "_undefined"
-MIDIControllerName[112] = "_undefined"
-MIDIControllerName[113] = "_undefined"
-MIDIControllerName[114] = "_undefined"
-MIDIControllerName[115] = "_undefined"
-MIDIControllerName[116] = "_undefined"
-MIDIControllerName[117] = "_undefined"
-MIDIControllerName[118] = "_undefined"
-MIDIControllerName[119] = "_undefined"
-MIDIControllerName[120] = "Channel Mute / Sound Off"
-MIDIControllerName[121] = "Reset All Controllers"
-MIDIControllerName[122] = "Local Keyboard On/Off Switch"
-MIDIControllerName[123] = "All MIDI Notes OFF"
-MIDIControllerName[124] = "OMNI Mode OFF"
-MIDIControllerName[125] = "OMNI Mode ON"
-MIDIControllerName[126] = "Mono Mode"
-MIDIControllerName[127] = "Poly Mode"
-
+MIDIControllerName = values.getlist_gm_ctrl_names()
 MIDIInstColors = values.getlist_gm_colors()
 MIDIInstNames = values.getlist_gm_names()
+MIDIDrumSetNames = values.getlist_gm_drumset_names()
 
 def addtoalltables(contents):
     global t_chan_timednote
@@ -323,14 +194,14 @@ class input_midi(plugin_input.base):
                     else:
                         cvpj_instdata['plugindata'] = {'bank':128, 'inst':inst}
 
-                        if inst in MIDIDrumNames and midi_trackname == None:
-                            cvpj_trackdata["name"] = MIDIDrumNames[inst]+' [Trk'+str(t_tracknum)+']'
-                        if inst not in MIDIDrumNames and midi_trackname == None:
+                        if inst in MIDIDrumSetNames and midi_trackname == None:
+                            cvpj_trackdata["name"] = MIDIDrumSetNames[inst]+' [Trk'+str(t_tracknum)+']'
+                        if inst not in MIDIDrumSetNames and midi_trackname == None:
                             cvpj_trackdata["name"] = 'Drums [Trk'+str(t_tracknum)+']'
 
-                        if inst in MIDIDrumNames and midi_trackname != None:
-                            cvpj_trackdata["name"] = midi_trackname+' ('+MIDIDrumNames[inst]+') [Trk'+str(t_tracknum)+']'
-                        if inst not in MIDIDrumNames and midi_trackname != None:
+                        if inst in MIDIDrumSetNames and midi_trackname != None:
+                            cvpj_trackdata["name"] = midi_trackname+' ('+MIDIDrumSetNames[inst]+') [Trk'+str(t_tracknum)+']'
+                        if inst not in MIDIDrumSetNames and midi_trackname != None:
                             cvpj_trackdata["name"] = midi_trackname+'(Drums) [Trk'+str(t_tracknum)+']'
                     if cvpj_trackid not in cvpj_l_instrumentsorder:
                         cvpj_l_instruments[cvpj_trackid] = cvpj_trackdata

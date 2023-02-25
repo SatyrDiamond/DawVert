fc68dcf6163151a1965b0426185785f35c6b9c20
SatyrDiamond <colbydray@gmail.com>
2023-02-25 04:09:02 -0500

    tracker cmds

--- plugin_input/mi_tracker_s3m.py
+++ plugin_input/mi_tracker_s3m.py
@@ -228,11 +228,15 @@ class input_s3m(plugin_input.base):
                                 pattern_row[0]['firstrow'] = 1
                             if packed_what_command_info == 1:
                                 if packed_command == 1:
-                                    pattern_row[1][packed_what_channel][3]['tracker_speed'] = packed_info
-                                    pattern_row[0]['tracker_speed'] = packed_info
+                                    pattern_row[1][packed_what_channel][3]['speed'] = packed_info
+                                    pattern_row[0]['speed'] = packed_info
                                 if packed_command == 3:
-                                    pattern_row[1][packed_what_channel][3]['tracker_break_to_row'] = packed_info
-                                    pattern_row[0]['tracker_break_to_row'] = packed_info
+                                    pattern_row[1][packed_what_channel][3]['break_to_row'] = packed_info
+                                    pattern_row[0]['break_to_row'] = packed_info
+                                if packed_command == 5:
+                                    pattern_row[1][packed_what_channel][2]['slide_down_c'] = packed_info
+                                if packed_command == 6:
+                                    pattern_row[1][packed_what_channel][2]['slide_up_c'] = packed_info
                     firstrow = 0
                     patterntable_single.append(pattern_row)
                     rowcount += 1

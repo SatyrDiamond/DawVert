e401949f56b2c1f7f99755fec235e60eb338f1dd
SatyrDiamond <satyrdiamond@gmail.com>
2023-03-05 01:50:52 -0500

    mariopaint idvals

--- plugin_input/m_mariopaint_msq.py
+++ plugin_input/m_mariopaint_msq.py
@@ -2,52 +2,13 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from functions import colors
+from functions import idvals
 import plugin_input
 import json
 import io
 
 instnames = ['mario','toad','yoshi','star','flower','gameboy','dog','cat','pig','swan','face','plane','boat','car','heart','coin','plant','shyguy','ghost']
 keytable =  [19, 17, 16, 14, 12, 11, 9, 7, 5, 4, 2, 0, -1]
-
-out_names = {'mario': 'Piano (Mario)',
-'toad': 'Tom (Mushroom)',
-'yoshi': 'SMW Yoshi',
-'star': 'Xylophone (Star)',
-'flower': 'Trumpet (Flower)',
-'gameboy': 'Square Wave (GameBoy)',
-'dog': 'Bark (Dog)',
-'cat': 'Meow (Cat)',
-'pig': 'Oink (Pig)',
-'swan': 'String Hit (Swan)',
-'face': 'Face',
-'plane': 'Acoustic Guitar (Plane)',
-'boat': 'Hat (Boat)',
-'car': 'Organ (Car)',
-'heart': 'Bass Guitar (Heart)',
-'coin': 'Piano (Coin)',
-'plant': 'Distorted Guitar (Plant)',
-'shyguy': 'String (Shy Guy)',
-'ghost': 'Harp (Ghost)'}
-
-out_colors = {'mario': [0.92, 0.77, 0.56],
-'toad': [0.98, 0.00, 0.00],
-'yoshi': [0.00, 0.98, 0.02],
-'star': [0.97, 0.98, 0.00],
-'flower': [0.96, 0.50, 0.00],
-'gameboy': [0.76, 0.76, 0.76],
-'dog': [1.00, 1.00, 1.00],
-'cat': [0.98, 0.75, 0.51],
-'pig': [0.96, 0.75, 0.52],
-'swan': [0.75, 0.75, 0.76],
-'face': [0.98, 0.76, 0.51],
-'plane': [1.00, 1.00, 1.00],
-'boat': [0.96, 0.00, 0.01],
-'car': [0.88, 0.53, 0.17],
-'heart': [0.96, 0.00, 0.00],
-'coin': [0.93, 0.75, 0.20],
-'plant': [0.73, 0.00, 0.01],
-'shyguy': [0.64, 0.00, 0.01],
-'ghost': [0.57, 0.96, 0.99]}
 
 def readpart(msq_score_str, n_pos, n_len):
     global notelist
@@ -101,6 +62,8 @@ class input_mariopaint_msq(plugin_input.base):
     def parse(self, input_file, extra_param):
         global notelist
 
+        idvals_mariopaint_inst = idvals.parse_idvalscsv('idvals/mariopaint_inst.csv')
+
         cvpj_l = {}
         cvpj_l_instruments = {}
         cvpj_l_instrumentsorder = []
@@ -150,8 +113,9 @@ class input_mariopaint_msq(plugin_input.base):
 
         for instname in instnames:
             cvpj_inst = {}
-            cvpj_inst["name"] = out_names[instname]
-            cvpj_inst["color"] = colors.moregray(out_colors[instname])
+            cvpj_inst["name"] = idvals.get_idval(idvals_mariopaint_inst, str(instname), 'name')
+            inst_color = idvals.get_idval(idvals_mariopaint_inst, str(instname), 'color')
+            if inst_color != None: cvpj_inst["color"] = colors.moregray(inst_color)
             cvpj_inst["instdata"] = {}
             cvpj_instdata = cvpj_inst["instdata"]
             cvpj_instdata['plugin'] = 'general-midi'
--- plugin_input/m_mariopaint_mss.py
+++ plugin_input/m_mariopaint_mss.py
@@ -3,6 +3,7 @@
 
 from functions import data_bytes
 from functions import colors
+from functions import idvals
 import plugin_input
 import json
 import xml.etree.ElementTree as ET
@@ -10,46 +11,6 @@ import xml.etree.ElementTree as ET
 instnames = ['mario','toad','yoshi','star','flower','gameboy','dog','cat','pig','swan','face','plane','boat','car','heart','coin','plant','shyguy','ghost']
 notekeys = ['A','B','C','D','E','F','G','H','a','b','c','d','e','f','g','h','i']
 keytable = [-3 ,-1 ,0  ,2  ,4  ,5  ,7  ,9  ,11 ,12 ,14 ,16 ,17 ,19 ,21 ,23 ,24 ]
-
-out_names = {'mario': 'Piano (Mario)',
-'toad': 'Tom (Mushroom)',
-'yoshi': 'SMW Yoshi',
-'star': 'Xylophone (Star)',
-'flower': 'Trumpet (Flower)',
-'gameboy': 'Square Wave (GameBoy)',
-'dog': 'Bark (Dog)',
-'cat': 'Meow (Cat)',
-'pig': 'Oink (Pig)',
-'swan': 'String Hit (Swan)',
-'face': 'Face',
-'plane': 'Acoustic Guitar (Plane)',
-'boat': 'Hat (Boat)',
-'car': 'Organ (Car)',
-'heart': 'Bass Guitar (Heart)',
-'coin': 'Piano (Coin)',
-'plant': 'Distorted Guitar (Plant)',
-'shyguy': 'String (Shy Guy)',
-'ghost': 'Harp (Ghost)'}
-
-out_colors = {'mario': [0.92, 0.77, 0.56],
-'toad': [0.98, 0.00, 0.00],
-'yoshi': [0.00, 0.98, 0.02],
-'star': [0.97, 0.98, 0.00],
-'flower': [0.96, 0.50, 0.00],
-'gameboy': [0.76, 0.76, 0.76],
-'dog': [1.00, 1.00, 1.00],
-'cat': [0.98, 0.75, 0.51],
-'pig': [0.96, 0.75, 0.52],
-'swan': [0.75, 0.75, 0.76],
-'face': [0.98, 0.76, 0.51],
-'plane': [1.00, 1.00, 1.00],
-'boat': [0.96, 0.00, 0.01],
-'car': [0.88, 0.53, 0.17],
-'heart': [0.96, 0.00, 0.00],
-'coin': [0.93, 0.75, 0.20],
-'plant': [0.73, 0.00, 0.01],
-'shyguy': [0.64, 0.00, 0.01],
-'ghost': [0.57, 0.96, 0.99]}
 
 def addnotes(n_pos, n_len, inst, txt, chordvolume):
     noteoffset = 0
@@ -98,6 +59,8 @@ class input_mariopaint_mss(plugin_input.base):
         global notelist
         tree = ET.parse(input_file)
         root = tree.getroot()
+
+        idvals_mariopaint_inst = idvals.parse_idvalscsv('idvals/mariopaint_inst.csv')
 
         cvpj_l = {}
         cvpj_l_instruments = {}
@@ -153,8 +116,9 @@ class input_mariopaint_mss(plugin_input.base):
 
         for instname in instnames:
             cvpj_inst = {}
-            cvpj_inst["name"] = out_names[instname]
-            cvpj_inst["color"] = colors.moregray(out_colors[instname])
+            cvpj_inst["name"] = idvals.get_idval(idvals_mariopaint_inst, str(instname), 'name')
+            inst_color = idvals.get_idval(idvals_mariopaint_inst, str(instname), 'color')
+            if inst_color != None: cvpj_inst["color"] = colors.moregray(inst_color)
             cvpj_inst["instdata"] = {}
             cvpj_instdata = cvpj_inst["instdata"]
             cvpj_instdata['plugin'] = 'general-midi'

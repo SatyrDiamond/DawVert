6d7a4ff7a046fb09247d8131016c7442c9f9fc0e
SatyrDiamond <satyrdiamond@gmail.com>
2023-03-14 18:22:30 -0400

    remove unused + xm fix

--- plugin_input/r_piyopiyo.py
+++ plugin_input/r_piyopiyo.py
@@ -118,7 +118,6 @@ class input_piyopiyo(plugin_input.base):
         
         cvpj_l['use_instrack'] = False
         cvpj_l['use_fxrack'] = False
-        cvpj_l['use_placements_notes'] = False
 
         cvpj_l['bpm'] = bpm
         cvpj_l['track_data'] = cvpj_l_trackdata

5fd64584304962029fa825b70c1048d65e21d097
SatyrDiamond <colbydray@gmail.com>
2023-03-21 06:10:01 -0400

    mario paint smp

--- plugin_input/m_mariopaint_smp.py
+++ plugin_input/m_mariopaint_smp.py
@@ -4,19 +4,14 @@
 from functions import colors
 from functions import idvals
 from functions import tracks
+from functions import song
 import plugin_input
 import json
 import io
 
 instnames = ['MARIO','MUSHROOM','YOSHI','STAR','FLOWER','GAMEBOY','DOG','CAT','PIG','SWAN','FACE','PLANE','BOAT','CAR','HEART','PIRANHA','COIN','SHYGUY','BOO','LUIGI','PEACH','FEATHER','BULLETBILL','GOOMBA','BOBOMB','SPINY','FRUIT','ONEUP','MOON','EGG','GNOME']
 smpnames = {'MARIO': "mario", 'MUSHROOM': "toad", 'YOSHI': "yoshi", 'STAR': "star", 'FLOWER': "flower", 'GAMEBOY': "gameboy", 'DOG': "dog", 'CAT': "cat", 'PIG': "pig", 'SWAN': "swan", 'FACE': "face", 'PLANE': "plane", 'BOAT': "boat", 'CAR': "car", 'HEART': "heart", 'PIRANHA': "plant", 'COIN': "coin", 'SHYGUY': "shyguy", 'BOO': "ghost", 'LUIGI': "luigi", 'PEACH': "peach", 'FEATHER': "feather", 'BULLETBILL': "bulletbill", 'GOOMBA': "goomba", 'BOBOMB': "bobomb", 'SPINY': "spiny", 'FRUIT': "fruit", 'ONEUP': "oneup", 'MOON': "moon", 'EGG': "egg", 'GNOME': "gnome"}
-keytable = {'C': 0,
-'D': 2,
-'E': 4,
-'F': 5,
-'G': 7,
-'A': 9,
-'B': 11}
+keytable = {'C': 0, 'D': 2, 'E': 4, 'F': 5, 'G': 7, 'A': 9, 'B': 11}
 
 def makenote(n_pos, notes, vol, notesize):
     for note in notes:
@@ -37,15 +32,7 @@ def makenote(n_pos, notes, vol, notesize):
             if t_txtp == 'b': out_offset = -1
             if t_txtp == 'm': out_mode = smpnote_str.read(1)
 
-        if out_mode == 0:
-            out_note = out_key + out_oct*12 + out_offset
-            notedata = {}
-            notedata['position'] = n_pos*notesize
-            notedata['key'] = out_note
-            notedata['vol'] = vol/100
-            notedata['duration'] = notesize
-            notedata['instrument'] = out_inst
-            notelist.append(notedata)
+        if out_mode == 0: cvpj_notelist.append({'position': n_pos*notesize, 'key': out_key+out_oct*12+out_offset, 'vol': vol/100, 'duration': notesize, 'instrument': out_inst})
 
 class input_mariopaint_smp(plugin_input.base):
     def __init__(self): pass
@@ -55,18 +42,11 @@ class input_mariopaint_smp(plugin_input.base):
     def gettype(self): return 'm'
     def supported_autodetect(self): return False
     def parse(self, input_file, extra_param):
-        global notelist
-
+        global cvpj_notelist
         idvals_mariopaint_inst = idvals.parse_idvalscsv('idvals/mariopaint_inst.csv')
-
         cvpj_l = {}
-        cvpj_l_playlist = {}
-        
-        notelist = []
-
+        cvpj_notelist = []
         smp_values = {}
-
-        notelen = 2
         linecount = 0
         f_smp = open(input_file, 'r')
         lines_smp = f_smp.readlines()
@@ -78,9 +58,7 @@ class input_mariopaint_smp(plugin_input.base):
                     smp_values[smp_nameval[0]] = smp_nameval[1]
                     if smp_nameval[0] == "TEMPO": smp_tempo = float(smp_nameval[1])
                     else: smp_tempo = 200
-                while smp_tempo > 180:
-                    smp_tempo = smp_tempo/2
-                    notelen = notelen/2
+                smp_tempo, notelen = song.get_lower_tempo(smp_tempo, 2, 180)
 
             else: 
                 s_point = line.rstrip().split(',')
@@ -95,13 +73,12 @@ class input_mariopaint_smp(plugin_input.base):
 
             linecount += 1
 
-        tracks.m_playlist_pl(cvpj_l, 1, None, None, [{'type': "instruments", 'position': 0, 'duration': s_pos_out+2, 'notelist': notelist}])
+        tracks.m_playlist_pl(cvpj_l, 1, None, None, [{'type': "instruments", 'position': 0, 'duration': s_pos_out+2, 'notelist': cvpj_notelist}])
 
         for instname in instnames:
             s_inst_name = idvals.get_idval(idvals_mariopaint_inst, smpnames[instname], 'name')
             s_inst_color = idvals.get_idval(idvals_mariopaint_inst, smpnames[instname], 'color')
             if s_inst_color != None: s_inst_color = colors.moregray(s_inst_color)
-
             tracks.m_addinst(cvpj_l, instname, {'plugin': 'general-midi', 'plugindata': {'bank':0, 'inst':instnames.index(instname)}})
             tracks.m_addinst_data(cvpj_l, instname, s_inst_name, s_inst_color, None, None)
 
@@ -110,7 +87,6 @@ class input_mariopaint_smp(plugin_input.base):
         
         cvpj_l['use_instrack'] = False
         cvpj_l['use_fxrack'] = False
-        cvpj_l['use_placements_notes'] = False
         
         cvpj_l['timesig_numerator'] = 4
         cvpj_l['timesig_denominator'] = 4

814e82a1c10bb08e54de66a89d2a9c94f6700dcc
SatyrDiamond <colbydray@gmail.com>
2023-03-21 06:01:05 -0400

    song func

--- functions/song.py
+++ functions/song.py
@@ -6,3 +6,8 @@ def get_lower_tempo(i_tempo, i_notelen, maxtempo):
         i_tempo = i_tempo/2
         i_notelen = i_notelen/2
     return (i_tempo, i_notelen)
+
+
+def add_timemarker_text(cvpj_l, i_position, i_name):
+    if 'timemarkers' not in cvpj_l: cvpj_l['timemarkers'] = []
+    cvpj_l['timemarkers'].append({'position':i_position, 'name': i_name})

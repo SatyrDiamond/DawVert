567f2e152574d1ff58775aa69eb6215d6d532d02
SatyrDiamond <satyrdiamond@gmail.com>
2023-03-21 08:47:22 -0400

    func notess v2

--- functions/song.py
+++ functions/song.py
@@ -13,5 +13,13 @@ def add_timemarker_text(cvpj_l, i_position, i_name):
 
 def add_timemarker_looparea(cvpj_l, i_name, i_start, i_end):
     if 'timemarkers' not in cvpj_l: cvpj_l['timemarkers'] = []
-    if i_name == None: cvpj_l['timemarkers'].append({'name': 'Loop', 'position': i_start, 'end': i_end, 'type': 'loop_area'})
-    else: cvpj_l['timemarkers'].append({'name': i_name, 'position': i_start, 'end': i_end, 'type': 'loop_area'})
+    timemarker_data = {'position': i_start, 'end': i_end, 'type': 'loop_area'}
+    if i_name == None: timemarker_data['name'] = 'Loop'
+    else: timemarker_data['name'] = i_name
+    cvpj_l['timemarkers'].append(timemarker_data)
+
+def add_timemarker_timesig(cvpj_l, i_name, i_position, i_numerator, i_denominator):
+    if 'timemarkers' not in cvpj_l: cvpj_l['timemarkers'] = []
+    timemarker_data = {'position': i_position, 'numerator': i_numerator, 'denominator': i_denominator, 'type': 'timesig'}
+    if i_name != None: timemarker_data['name'] = i_name
+    cvpj_l['timemarkers'].append(timemarker_data)

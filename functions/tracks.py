5f1d92573b0af5e91f3366fcc088ace4ef26663f
SatyrDiamond <colbydray@gmail.com>
2023-03-23 05:33:56 -0400

    sound club 2

--- functions/tracks.py
+++ functions/tracks.py
@@ -3,65 +3,90 @@
 
 # ------------------------ Regular ------------------------
 
-def r_addtrack_inst(cvpj_l, idval, instdata):
+def r_addtrack_inst(cvpj_l, trackid, instdata):
     if 'track_data' not in cvpj_l: cvpj_l['track_data'] = {}
     if 'track_order' not in cvpj_l: cvpj_l['track_order'] = []
     cvpj_inst = {}
     cvpj_inst['type'] = 'instrument'
     cvpj_inst['instdata'] = instdata
-    cvpj_l['track_data'][idval] = cvpj_inst
-    cvpj_l['track_order'].append(idval)
+    cvpj_l['track_data'][trackid] = cvpj_inst
+    cvpj_l['track_order'].append(trackid)
 
-def r_addtrackpl(cvpj_l, idval, placements_data):
-    if 'track_placements' not in cvpj_l: cvpj_l['track_placements'] = {}
-    cvpj_l['track_placements'][idval] = {}
-    cvpj_l['track_placements'][idval]['notes'] = placements_data
-
-def r_addtrack_data(cvpj_l, idval, trk_name, trk_color, trk_vol, trk_pan):
+def r_addtrack_data(cvpj_l, trackid, trk_name, trk_color, trk_vol, trk_pan):
     if 'track_data' in cvpj_l:
-        if idval in cvpj_l['track_data']:
-            cvpj_inst = cvpj_l['track_data'][idval]
+        if trackid in cvpj_l['track_data']:
+            cvpj_inst = cvpj_l['track_data'][trackid]
             cvpj_inst['name'] = trk_name
             if trk_color != None: cvpj_inst['color'] = trk_color
             if trk_vol != None: cvpj_inst['vol'] = trk_vol
             if trk_pan != None: cvpj_inst['pan'] = trk_pan
 
+def r_addinst_param(cvpj_l, trackid, v_name, v_value):
+    if 'track_data' in cvpj_l:
+        if trackid in cvpj_l['track_data']:
+            cvpj_inst = cvpj_l['track_data'][trackid]
+            cvpj_inst[v_name] = v_value
+
 # ------------------------ RegularIndexed ------------------------
 
-def ri_addtrack_inst(cvpj_l, idval, notelistindex, instdata):
+def ri_addtrack_inst(cvpj_l, trackid, notelistindex, instdata):
     if 'track_data' not in cvpj_l: cvpj_l['track_data'] = {}
     if 'track_order' not in cvpj_l: cvpj_l['track_order'] = []
     cvpj_inst = {}
     cvpj_inst['type'] = 'instrument'
     cvpj_inst['instdata'] = instdata
-    cvpj_inst['notelistindex'] = notelistindex
-    cvpj_l['track_data'][idval] = cvpj_inst
-    cvpj_l['track_order'].append(idval)
+    if notelistindex != None: cvpj_inst['notelistindex'] = notelistindex
+    else: cvpj_inst['notelistindex'] = {}
+    cvpj_l['track_data'][trackid] = cvpj_inst
+    cvpj_l['track_order'].append(trackid)
+
+def ri_addinst_nle(cvpj_l, trackid, patid, nle_notelist, nle_name):
+    if 'track_data' in cvpj_l:
+        if trackid in cvpj_l['track_data']:
+            if 'notelistindex' not in cvpj_l['track_data'][trackid]: cvpj_l['track_data'][trackid]['notelistindex'] = {}
+            cvpj_l['track_data'][trackid]['notelistindex'][patid] = {}
+            cvpj_inst_nle = cvpj_l['track_data'][trackid]['notelistindex'][patid]
+            if nle_name != None: cvpj_inst_nle['name'] = nle_name
+            cvpj_inst_nle['notelist'] = nle_notelist
+
+# ------------------------ Regular******** ------------------------
+
+def r_addtrackpl(cvpj_l, trackid, placements_data):
+    if 'track_placements' not in cvpj_l: cvpj_l['track_placements'] = {}
+    cvpj_l['track_placements'][trackid] = {}
+    if placements_data != None: cvpj_l['track_placements'][trackid]['notes'] = placements_data
+
+def r_addtrackpl_laneddata(cvpj_l, trackid, laneddata):
+    if 'track_placements' not in cvpj_l: cvpj_l['track_placements'] = {}
+    cvpj_l['track_placements'][trackid] = {}
+    if laneddata != None: cvpj_l['track_placements'][trackid] = laneddata
 
 # ------------------------ Multiple ------------------------
 
-def m_addinst(cvpj_l, idval, instdata):
+def m_addinst(cvpj_l, trackid, instdata):
     if 'instruments_data' not in cvpj_l: cvpj_l['instruments_data'] = {}
     if 'instruments_order' not in cvpj_l: cvpj_l['instruments_order'] = []
     cvpj_inst = {}
     cvpj_inst['instdata'] = instdata
-    cvpj_l['instruments_data'][idval] = cvpj_inst
-    cvpj_l['instruments_order'].append(idval)
+    cvpj_l['instruments_data'][trackid] = cvpj_inst
+    cvpj_l['instruments_order'].append(trackid)
 
-def m_addinst_data(cvpj_l, idval, trk_name, trk_color, trk_vol, trk_pan):
+def m_addinst_data(cvpj_l, trackid, trk_name, trk_color, trk_vol, trk_pan):
     if 'instruments_data' in cvpj_l:
-        if idval in cvpj_l['instruments_data']:
-            cvpj_inst = cvpj_l['instruments_data'][idval]
+        if trackid in cvpj_l['instruments_data']:
+            cvpj_inst = cvpj_l['instruments_data'][trackid]
             cvpj_inst['name'] = trk_name
             if trk_color != None: cvpj_inst['color'] = trk_color
             if trk_vol != None: cvpj_inst['vol'] = trk_vol
             if trk_pan != None: cvpj_inst['pan'] = trk_pan
 
-def m_addinst_param(cvpj_l, idval, v_name, v_value):
+def m_addinst_param(cvpj_l, trackid, v_name, v_value):
     if 'instruments_data' in cvpj_l:
-        if idval in cvpj_l['instruments_data']:
-            cvpj_inst = cvpj_l['instruments_data'][idval]
+        if trackid in cvpj_l['instruments_data']:
+            cvpj_inst = cvpj_l['instruments_data'][trackid]
             cvpj_inst[v_name] = v_value
+
+# ------------------------ Multiple******** ------------------------
 
 def m_playlist_pl(cvpj_l, idnum, trk_name, trk_color, placements_notes):
     if 'playlist' not in cvpj_l: cvpj_l['playlist'] = {}

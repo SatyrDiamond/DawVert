dcf6d4b913079eb4c1ce25e1c603bbf03798f524
SatyrDiamond <colbydray@gmail.com>
2023-04-04 08:45:35 -0400

    fix

--- plugin_output/cvpj.py
+++ plugin_output/cvpj.py
@@ -12,7 +12,14 @@ class output_cvpj(plugin_output.base):
     def gettype(self): return 'debug'
     def plugin_archs(self): return None
     def getdawcapabilities(self): 
-        return {}
+        return {
+        'fxrack': 'debug',
+        'r_track_lanes': 'debug',
+        'placement_cut': 'debug',
+        'placement_warp': 'debug',
+        'no_placements': 'debug',
+        'pl_audio_events': 'debug',
+        }
     def parse(self, convproj_json, output_file):
         projJ = json.loads(convproj_json)
         with open(output_file, "w") as fileout:

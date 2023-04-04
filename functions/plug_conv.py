f8b56c4aa46c0acd6bd35fa1e87b33e9b157b66b
SatyrDiamond <colbydray@gmail.com>
2023-04-04 08:46:17 -0400

    drops

--- functions/plug_conv.py
+++ functions/plug_conv.py
@@ -18,6 +18,9 @@ from functions import plug_in_fl
 from functions import plug_in_pxtone
 from functions import plug_in_jummbox
 
+def clamp(n, minn, maxn):
+	return max(min(maxn, n), minn)
+
 # -------------------- Instruments --------------------
 def convplug_inst(instdata, dawname, extra_json, nameid, platform_id):
 	global supportedplugins
@@ -57,7 +60,7 @@ def convplug_inst(instdata, dawname, extra_json, nameid, platform_id):
 			plugindata = instdata['plugindata']
 
 			# -------------------- sampler > vst2 (Grace) --------------------
-			if pluginname == 'sampler' and dawname not in supportedplugins['sampler']:
+			if pluginname == 'sampler' and dawname not in supportedplugins['sampler'] and platform_id == 'win':
 				sampler_data = instdata
 				sampler_file_data = instdata['plugindata']
 				wireturn = audio_wav.complete_wav_info(sampler_file_data)
@@ -84,6 +87,58 @@ def convplug_inst(instdata, dawname, extra_json, nameid, platform_id):
 						print('[plug-conv] Unchanged, Plugin Grace not Found')
 				else:
 					print('[plug-conv] Unchanged, VST2 list not found')
+
+			# -------------------- sampler > vst2 (drops) --------------------
+
+			elif pluginname == 'sampler' and dawname not in supportedplugins['sampler'] and platform_id == 'lin':
+				sampler_data = instdata
+				sampler_file_data = instdata['plugindata']
+				wireturn = audio_wav.complete_wav_info(sampler_file_data)
+				vst_inst.drops_init()
+				if 'file' in sampler_file_data: vst_inst.drops_setfile(sampler_file_data['file'])
+
+				if 'filter' in sampler_file_data: 
+					if 'wet' in sampler_file_data['filter']: 
+						if sampler_file_data['filter']['wet'] != 0:
+							filterdata = sampler_file_data['filter']
+							if 'cutoff' in filterdata: vst_inst.drops_setvalue('cutoff', clamp((filterdata['cutoff']/12000), 0, 1))
+							if 'reso' in filterdata: vst_inst.drops_setvalue('resonance', clamp((filterdata['reso']/10), 0, 1))
+							if 'type' in filterdata: 
+								cvpj_filtertype = filterdata['type']
+								if cvpj_filtertype == 'lowpass': vst_inst.drops_setvalue('filter_type', 0)
+								if cvpj_filtertype == 'bandpass': vst_inst.drops_setvalue('filter_type', 1)
+								if cvpj_filtertype == 'highpass': vst_inst.drops_setvalue('filter_type', 2)
+
+				if 'asdrlfo' in sampler_file_data: 
+					if 'volume' in sampler_file_data['asdrlfo']: 
+						if 'envelope' in sampler_file_data['asdrlfo']['volume']: 
+							asdr_env = sampler_file_data['asdrlfo']['volume']['envelope']
+							amountmul = 1
+							if 'amount' in asdr_env: amountmul = clamp(asdr_env['amount'], 0, 1)
+							if 'attack' in asdr_env: vst_inst.drops_setvalue('amp_attack', clamp((asdr_env['attack']/10)*amountmul, 0, 1))
+							if 'decay' in asdr_env: vst_inst.drops_setvalue('amp_decay', clamp((asdr_env['decay']/10)*amountmul, 0, 1))
+							vst_inst.drops_setvalue('amp_sustain', clamp(asdr_env['sustain'], 0, 1))
+							if 'release' in asdr_env: vst_inst.drops_setvalue('amp_release', clamp((asdr_env['release']/10)*amountmul, 0, 1))
+
+						if 'lfo' in sampler_file_data['asdrlfo']['volume']: 
+							asdr_lfo = sampler_file_data['asdrlfo']['volume']['lfo']
+							vst_inst.drops_setvalue('amp_lfo_depth', clamp(asdr_lfo['amount'], 0, 1))
+							if 'speed' in asdr_lfo:
+								if asdr_lfo['speed']['type'] == 'seconds' and asdr_lfo['speed']['time'] != 0:
+									vst_inst.drops_setvalue('amp_lfo_freq', clamp(((1/asdr_lfo['speed']['time'])*0.05), 0, 1))
+							if 'shape' in asdr_lfo: vst_inst.drops_setvalue('amp_lfo_type', vst_inst.drops_shape(asdr_lfo['shape']))
+							if 'attack' in asdr_lfo: vst_inst.drops_setvalue('amp_lfo_fade', clamp(asdr_lfo['attack']/10, 0, 1))
+
+					if 'cutoff' in sampler_file_data['asdrlfo']: 
+						if 'envelope' in sampler_file_data['asdrlfo']['cutoff']: 
+							asdr_env = sampler_file_data['asdrlfo']['cutoff']['envelope']
+							if 'amount' in asdr_env: vst_inst.drops_setvalue('filter_eg_depth', clamp((asdr_env['amount']/7300), 0, 1))
+							if 'attack' in asdr_env: vst_inst.drops_setvalue('filter_attack', clamp((asdr_env['attack']/10), 0, 1))
+							if 'decay' in asdr_env: vst_inst.drops_setvalue('filter_decay', clamp((asdr_env['decay']/10), 0, 1))
+							vst_inst.drops_setvalue('filter_sustain', clamp(asdr_env['sustain'], 0, 1))
+							if 'release' in asdr_env: vst_inst.drops_setvalue('filter_release', clamp((asdr_env['release']/10)*amountmul, 0, 1))
+
+				list_vst.replace_data(instdata, 2, 'lin', 'Drops', 'raw', params_vst.nullbytegroup_make(vst_inst.drops_get()), None)
 
 			# -------------------- sampler-multi > vst2 (Grace) --------------------
 
@@ -223,7 +278,6 @@ def convplug_fx(fxdata, dawname, extra_json, nameid):
 					for pointnum in range(50):
 						pointdata = waveshapepoints[pointnum*4][0]
 						vst_fx.wolfshaper_addpoint(pointnum/49,pointdata,0.5,0)
-					vst_fx.wolfshaper_get()
 					list_vst.replace_data(fxdata, 2, 'any', 'Wolf Shaper', 'raw', params_vst.nullbytegroup_make(vst_fx.wolfshaper_get()), None)
 			else:
 				print('[plug-conv] Unchanged')

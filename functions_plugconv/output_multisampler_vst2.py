d68495458a362b70a2b9da606a44eed83235a118
SatyrDiamond <satyrdiamond@gmail.com>
2023-04-08 20:58:15 -0400

    plugconv fix

--- functions_plugconv/output_multisampler_vst2.py
+++ functions_plugconv/output_multisampler_vst2.py
@@ -11,11 +11,12 @@ def convert_inst(instdata, platform_id):
 		vst2_dll_vstpaths = list_vst.vstpaths()
 		if vst2_dll_vstpaths['2-dll']:
 			if 'Grace' in vst2_dll_vstpaths['2-dll']:
-				regions = msmpl_p_data['regions']
-				gx_root = vst_inst.grace_create_main()
-				for regionparams in regions:
-					vst_inst.grace_create_region(gx_root, regionparams)
-				xmlout = ET.tostring(gx_root, encoding='utf-8')
-				list_vst.replace_data(instdata, 2, 'any', 'Grace', 'raw', xmlout, None)
+				if 'regions' in msmpl_p_data:
+					regions = msmpl_p_data['regions']
+					gx_root = vst_inst.grace_create_main()
+					for regionparams in regions:
+						vst_inst.grace_create_region(gx_root, regionparams)
+					xmlout = ET.tostring(gx_root, encoding='utf-8')
+					list_vst.replace_data(instdata, 2, 'any', 'Grace', 'raw', xmlout, None)
 			else:
 				print('[plug-conv] Unchanged, Plugin Grace not Found')

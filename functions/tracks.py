43030db7c01d6559f2ca9e4ccfb318f3defc7376
SatyrDiamond <colbydray@gmail.com>
2023-04-13 20:18:19 -0400

    caustic auto

--- functions/tracks.py
+++ functions/tracks.py
@@ -172,7 +172,7 @@ def a_addtrack_master_param(cvpj_l, v_name, v_value):
 def a_add_auto_pl(cvpj_l, in_type, in_id, in_name, in_autopoints):
     if 'automation' not in cvpj_l: cvpj_l['automation'] = {}
     if in_type not in cvpj_l['automation']: cvpj_l['automation'][in_type] = {}
-    if in_type in ['track', 'plugin', 'fxmixer']:
+    if in_type in ['track', 'plugin', 'fxmixer', 'slot']:
         if in_id not in cvpj_l['automation'][in_type]: cvpj_l['automation'][in_type][in_id] = {}
         if in_name not in cvpj_l['automation'][in_type][in_id]: cvpj_l['automation'][in_type][in_id][in_name] = []
         pltoadd = cvpj_l['automation'][in_type][in_id]

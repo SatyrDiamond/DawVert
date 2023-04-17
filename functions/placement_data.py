10b4d6ef1cd336cea05f16e7b240bd45fd8d1e7a
SatyrDiamond <colbydray@gmail.com>
2023-04-17 16:30:04 -0400

    fix

--- functions/placement_data.py
+++ functions/placement_data.py
@@ -1,5 +1,7 @@
 # SPDX-FileCopyrightText: 2023 SatyrDiamond
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+from functions import notelist_data
+
 def nl2pl(cvpj_notelist):
     return [{'position': 0, 'duration': notelist_data.getduration(cvpj_notelist), 'notelist': cvpj_notelist}]

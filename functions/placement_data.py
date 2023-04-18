a76b43c0bb1f0d26b35f207008642742ea9db8fe
SatyrDiamond <colbydray@gmail.com>
2023-04-18 05:59:41 -0400

    addfunc

--- functions/placement_data.py
+++ functions/placement_data.py
@@ -3,5 +3,19 @@
 
 from functions import notelist_data
 
+def makepl_n(t_pos, t_dur, t_notelist):
+    pl_data = {}
+    pl_data['position'] = t_pos
+    pl_data['duration'] = t_dur
+    pl_data['notelist'] = t_notelist
+    return pl_data
+
+def makepl_n_mi(t_pos, t_dur, t_fromindex):
+    pl_data = {}
+    pl_data['position'] = t_pos
+    pl_data['duration'] = t_dur
+    pl_data['fromindex'] = t_fromindex
+    return pl_data
+
 def nl2pl(cvpj_notelist):
     return [{'position': 0, 'duration': notelist_data.getduration(cvpj_notelist), 'notelist': cvpj_notelist}]

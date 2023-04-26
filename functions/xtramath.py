62697df7ebdde2120f7a267d9a9e870a9cbbf2a4
SatyrDiamond <satyrdiamond@gmail.com>
2023-04-26 08:49:53 -0400

    rename func

--- functions/xtramath.py
+++ functions/xtramath.py
@@ -7,7 +7,7 @@ def clamp(n, minn, maxn):
 def overlap(start1, end1, start2, end2):
     return max(max((end2-start1), 0) - max((end2-end1), 0) - max((start2-start1), 0), 0)
 
-def float_range(start,stop,step):
+def gen_float_range(start,stop,step):
     istop = int((stop-start) // step)
     for i in range(int(istop)):
         yield start + i * step

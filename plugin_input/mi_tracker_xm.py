1edde14a7bf72c0e8ed97211e66611b498f352c9
SatyrDiamond <satyrdiamond@gmail.com>
2023-04-27 10:21:24 -0400

    change a_add_auto_pl

--- plugin_input/mi_tracker_xm.py
+++ plugin_input/mi_tracker_xm.py
@@ -388,7 +388,7 @@ class input_xm(plugin_input.base):
         
         cvpj_l_playlist = song_tracker.song2playlist(patterntable_all, xm_song_num_channels, t_orderlist, startinststr, [0.16, 0.33, 0.53])
 
-        tracks.a_add_auto_pl(cvpj_l, 'main', None, 'bpm', song_tracker.tempo_auto(patterntable_all, t_orderlist, 6, xm_song_bpm))
+        tracks.a_add_auto_pl(cvpj_l, ['main', 'bpm'], song_tracker.tempo_auto(patterntable_all, t_orderlist, 6, xm_song_bpm))
 
         cvpj_l['info'] = {}
         cvpj_l['info']['title'] = xm_name

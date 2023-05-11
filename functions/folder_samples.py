f05160631c031dda6ef39d0b2feaa47f0427dd8e
SatyrDiamond <satyrdiamond@gmail.com>
2023-05-11 14:41:46 -0400

    update

--- functions/folder_samples.py
+++ functions/folder_samples.py
@@ -7,7 +7,7 @@ def samplefolder(extra_param, songname):
     if 'samplefolder' in extra_param:
         samplefolder = extra_param['samplefolder'] + '/' + songname + '/'
     else:
-        samplefolder = os.getcwd() + '/extracted_samples/' + songname + '/'
-        os.makedirs(os.getcwd() + '/extracted_samples/', exist_ok=True)
+        samplefolder = os.getcwd() + '/__extracted_samples/' + songname + '/'
+        os.makedirs(os.getcwd() + '/__extracted_samples/', exist_ok=True)
     os.makedirs(samplefolder, exist_ok=True)
     return samplefolder

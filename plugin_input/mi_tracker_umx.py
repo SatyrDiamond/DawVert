2991e6e9d81dce97a3c02f4e8453219ab60f4e57
B0ney <40839054+B0ney@users.noreply.github.com>
2023-05-13 15:55:43 +0100

    Add file extension to stripped umx files once properly validated

--- plugin_input/mi_tracker_umx.py
+++ plugin_input/mi_tracker_umx.py
@@ -12,12 +12,12 @@ from plugin_input.mi_tracker_mod import input_mod as mod
 from plugin_input.mi_tracker_it import input_it as it
 from plugin_input.mi_tracker_xm import input_xm as xm
 
-TRACKER_FORMATS = [
-    s3m(),
-    it(),
-    xm(),
+TRACKER_FORMATS = {
+    "s3m": s3m(),
+    "it": it(),
+    "xm": xm(),
     # mod() # mod does not support autodetect
-]
+}
 
 class imput_umx(plugin_input.base):
     def is_dawvert_plugin(self) -> str: 
@@ -42,11 +42,8 @@ class imput_umx(plugin_input.base):
         return umx.is_umx(open(input_file, 'rb'))
     
     def parse(self, input_file: str, extra_param=None) -> Optional[str]:
-        filename = os.path.splitext(os.path.basename(input_file))[0]
-
         # Filename of stripped umx file
-        # stripped_umx = f"temp_{filename}_{random.randint(2**12, 2**32)}"
-        stripped_umx = filename
+        stripped_umx = os.path.splitext(os.path.basename(input_file))[0]
 
         # Successfully validated and stripped umx files will return a UMXInfo class
         # which contains basic metadata about the contained tracker.
@@ -66,15 +63,19 @@ class imput_umx(plugin_input.base):
 
         try:
             # Using UMXInfo.hint() can be misleading, so iterate through a list of trackers
-            # and only parse with that tracker if it's a valid format. 
-            for tracker in TRACKER_FORMATS:
+            # and only parse with that if it's a valid format. 
+            for (extension, tracker) in TRACKER_FORMATS.items():
                 if tracker.detect(stripped_umx):
+                    stripped_umx =  rename(stripped_umx, extension)
+
                     print(f"[info] Detected format: { tracker.getname() }")
                     result = tracker.parse(stripped_umx, extra_param=extra_param)
             
             # An edge case where MOD files don't have the "detect" method.
             # In this circumstance, we can assume that valid UMX files will store a valid MOD file.
             if result is None:
+                stripped_umx = rename(stripped_umx, "mod")
+
                 result = mod().parse(stripped_umx, extra_param=extra_param)
 
         finally:
@@ -82,3 +83,9 @@ class imput_umx(plugin_input.base):
             os.remove(stripped_umx)     
             
         return result
+
+# Rename stripped umx to include file extension.
+def rename(stripped_path: str, extension: str) -> str:
+    os.rename(stripped_path, f"{stripped_path}.{extension}")
+
+    return f"{stripped_path}.{extension}"

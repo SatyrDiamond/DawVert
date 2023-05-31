71386163e32bc58888d443c6232d2990ed5ab474
SatyrDiamond <satyrdiamond@gmail.com>
2023-05-31 18:55:28 -0400

    opl

--- functions_plugconv/output_soundchip_vst2.py
+++ functions_plugconv/output_soundchip_vst2.py
@@ -58,52 +58,52 @@ def convert_inst(instdata, out_daw):
 		xmlout = params_various_inst.opnplug_convert(plugindata)
 		plugin_vst2.replace_data(instdata, 'any', 'OPNplug', 'chunk', data_vc2xml.make(xmlout), None)
 
-	if pluginname == 'opl2':
+	#if pluginname == 'opl2':
 
-		opl2_car = plugindata['op1']
-		opl2_mod = plugindata['op2']
-		print(plugindata)
+	#	opl2_car = plugindata['op2']
+	#	opl2_mod = plugindata['op1']
+	#	print(plugindata)
 
-		outputjson = {}
+	#	outputjson = {}
 
-		outputjson['Program_Index'] = 0
-		outputjson['Carrier_Wave'] = float(opl2_car['waveform']/7)
-		outputjson['Modulator_Wave'] = float(opl2_mod['waveform']/7)
-		outputjson['Carrier_Frequency_Multiplier'] = opl2_car['freqmul']/15
-		outputjson['Modulator_Frequency_Multiplier'] = opl2_mod['freqmul']/15
-		outputjson['Carrier_Attenuation'] = 0.0
-		outputjson['Modulator_Attenuation'] = 0.0
-		outputjson['Tremolo_Depth'] = plugindata['tremolo_depth']
-		outputjson['Vibrato_Depth'] = plugindata['vibrato_depth']
-		outputjson['Carrier_Tremolo'] = opl2_car['tremolo']
-		outputjson['Carrier_Vibrato'] = opl2_car['vibrato']
-		outputjson['Carrier_Sustain'] = 0.0
-		outputjson['Carrier_Keyscale_Rate'] = opl2_car['ksr']
-		outputjson['Modulator_Tremolo'] = opl2_mod['tremolo']
-		outputjson['Modulator_Vibrato'] = opl2_mod['vibrato']
-		outputjson['Modulator_Sustain'] = 0.0
-		outputjson['Modulator_Keyscale_Rate'] = opl2_mod['ksr']
-		outputjson['Carrier_Keyscale_Level'] = 0.0
-		outputjson['Modulator_Keyscale_Level'] = 0.0
-		outputjson['Algorithm'] = (plugindata['fm']*-1)+1
-		outputjson['Modulator_Feedback'] = plugindata['feedback']/7
-		outputjson['Carrier_Attack'] = opl2_car['env_attack']/15
-		outputjson['Carrier_Decay'] = opl2_car['env_decay']/15
-		outputjson['Carrier_Sustain_Level'] = opl2_car['env_sustain']/15
-		outputjson['Carrier_Release'] = opl2_car['env_release']/15
-		outputjson['Modulator_Attack'] = opl2_mod['env_attack']/15
-		outputjson['Modulator_Decay'] = opl2_mod['env_decay']/15
-		outputjson['Modulator_Sustain_Level'] = opl2_mod['env_sustain']/15
-		outputjson['Modulator_Release'] = opl2_mod['env_release']/15
-		outputjson['Carrier_Velocity_Sensitivity'] = 0.0
-		outputjson['Modulator_Velocity_Sensitivity'] = 0.0
-		outputjson['Emulator'] = 0.0
-		if 'perctype' in plugindata: outputjson['Percussion_Mode'] = float(plugindata['perctype']/5)
-		else: outputjson['Percussion_Mode'] = 0.0
-		outputjson['lastLoadFile'] = ""
-		outputjson['selectedIdxFile'] = -1
+	#	outputjson['Program_Index'] = 0
+	#	outputjson['Carrier_Wave'] = float(opl2_car['waveform']/7)
+	#	outputjson['Modulator_Wave'] = float(opl2_mod['waveform']/7)
+	#	outputjson['Carrier_Frequency_Multiplier'] = opl2_car['freqmul']/15
+	#	outputjson['Modulator_Frequency_Multiplier'] = opl2_mod['freqmul']/15
+	#	outputjson['Carrier_Attenuation'] = 0.0
+	#	outputjson['Modulator_Attenuation'] = 0.0
+	#	outputjson['Tremolo_Depth'] = plugindata['tremolo_depth']
+	#	outputjson['Vibrato_Depth'] = plugindata['vibrato_depth']
+	#	outputjson['Carrier_Tremolo'] = opl2_car['tremolo']
+	#	outputjson['Carrier_Vibrato'] = opl2_car['vibrato']
+	#	outputjson['Carrier_Sustain'] = 0.0
+	#	outputjson['Carrier_Keyscale_Rate'] = opl2_car['ksr']
+	#	outputjson['Modulator_Tremolo'] = opl2_mod['tremolo']
+	#	outputjson['Modulator_Vibrato'] = opl2_mod['vibrato']
+	#	outputjson['Modulator_Sustain'] = 0.0
+	#	outputjson['Modulator_Keyscale_Rate'] = opl2_mod['ksr']
+	#	outputjson['Carrier_Keyscale_Level'] = 0.0
+	#	outputjson['Modulator_Keyscale_Level'] = 0.0
+	#	outputjson['Algorithm'] = (plugindata['fm']*-1)+1
+	#	outputjson['Modulator_Feedback'] = plugindata['feedback']/7
+	#	outputjson['Carrier_Attack'] = opl2_car['env_attack']/15
+	#	outputjson['Carrier_Decay'] = opl2_car['env_decay']/15
+	#	outputjson['Carrier_Sustain_Level'] = opl2_car['env_sustain']/15
+	#	outputjson['Carrier_Release'] = opl2_car['env_release']/15
+	#	outputjson['Modulator_Attack'] = opl2_mod['env_attack']/15
+	#	outputjson['Modulator_Decay'] = opl2_mod['env_decay']/15
+	#	outputjson['Modulator_Sustain_Level'] = opl2_mod['env_sustain']/15
+	#	outputjson['Modulator_Release'] = opl2_mod['env_release']/15
+	#	outputjson['Carrier_Velocity_Sensitivity'] = 0.0
+	#	outputjson['Modulator_Velocity_Sensitivity'] = 0.0
+	#	outputjson['Emulator'] = 0.0
+	#	if 'perctype' in plugindata: outputjson['Percussion_Mode'] = float(plugindata['perctype']/5)
+	#	else: outputjson['Percussion_Mode'] = 0.0
+	#	outputjson['lastLoadFile'] = ""
+	#	outputjson['selectedIdxFile'] = -1
 
-		oplvstbytes = json.dumps(outputjson, indent=2).replace("\n", "\r\n").encode('ascii')
-		print(oplvstbytes)
+	#	oplvstbytes = json.dumps(outputjson, indent=2).replace("\n", "\r\n").encode('ascii')
+	#	print(oplvstbytes)
 
-		plugin_vst2.replace_data(instdata, 'any', 'OPL', 'chunk', oplvstbytes, None)
+	#	plugin_vst2.replace_data(instdata, 'any', 'JuceOPLVSTi', 'chunk', oplvstbytes, None)

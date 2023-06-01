939f045d0ae409107031856581fa27b4ba09b8d1
SatyrDiamond <colbydray@gmail.com>
2023-05-31 20:46:24 -0400

    update plug_conv

--- functions_plugconv/output_slicer_vst2.py
+++ functions_plugconv/output_slicer_vst2.py
@@ -7,12 +7,12 @@ import pathlib
 from functions import audio_wav
 from functions import plugin_vst2
 
-from functions_plugparams import params_various_inst
+from functions_plugparams import params_ninjas2
 from functions_plugparams import data_nullbytegroup
 
 def convert_inst(instdata):
 	slicerdata = instdata['plugindata']
-	params_various_inst.ninjas2_init()
-	params_various_inst.ninjas2_slicerdata(slicerdata)
-	ninjas2out = params_various_inst.ninjas2_get()
+	params_ninjas2.initparams()
+	params_ninjas2.slicerdata(slicerdata)
+	ninjas2out = params_ninjas2.getparams()
 	plugin_vst2.replace_data(instdata, 'any', 'Ninjas 2', 'chunk', data_nullbytegroup.make(ninjas2out), None)

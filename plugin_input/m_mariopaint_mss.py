79193dbafe6f79c95cad7f9d4b9d4d1d3199aafa
SatyrDiamond <colbydray@gmail.com>
2023-06-01 14:13:10 -0400

    update

--- plugin_input/m_mariopaint_mss.py
+++ plugin_input/m_mariopaint_mss.py
@@ -1,7 +1,6 @@
 # SPDX-FileCopyrightText: 2023 SatyrDiamond
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from functions import data_bytes
 from functions import colors
 from functions import idvals
 from functions import tracks

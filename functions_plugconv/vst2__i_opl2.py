130e799a2b8027cd2004bad2a2d4437ab369d843
SatyrDiamond <satyrdiamond@gmail.com>
2023-07-02 15:08:13 -0400

    update

--- functions_plugconv/vst2__i_opl2.py
+++ functions_plugconv/vst2__i_opl2.py
@@ -15,51 +15,86 @@ def getparam(paramname):
 	paramval = plugins.get_plug_param(cvpj_l_g, pluginid_g, paramname, 0)
 	return paramval[0]
 
+def opnplug_addvalue(opnplug_params, name, value):
+	temp_xml = ET.SubElement(opnplug_params, 'VALUE')
+	temp_xml.set('name', str(name))
+	temp_xml.set('val', str(value))
+
+def opnplug_addbank(opnplug_params, num, name):
+	bank_xml = ET.SubElement(opnplug_params, 'bank')
+	opnplug_addvalue(bank_xml, 'bank', num)
+	opnplug_addvalue(bank_xml, 'name', name)
+
+opadltxt = ['m1', 'c1', 'm2', 'c2']
+
 def convert(cvpj_l, pluginid, plugintype):
 	global pluginid_g
 	global cvpj_l_g
 	pluginid_g = pluginid
 	cvpj_l_g = cvpj_l
 
-	outputjson = {}
+	adlplug_root = ET.Element("ADLMIDI-state")
+	opnplug_addbank(adlplug_root, 1, 'DawVert')
+	opnplug_addbank(adlplug_root, 0, 'DawVert')
+	opnplug_params = ET.SubElement(adlplug_root, 'instrument')
+	opnplug_addvalue(opnplug_params, "four_op" ,0)
+	opnplug_addvalue(opnplug_params, "pseudo_four_op" ,0)
+	opnplug_addvalue(opnplug_params, "blank" ,0)
+	opnplug_addvalue(opnplug_params, "con12" ,0)
+	opnplug_addvalue(opnplug_params, "con34" ,0)
+	opnplug_addvalue(opnplug_params, "note_offset1" ,0)
+	opnplug_addvalue(opnplug_params, "note_offset2" ,0)
+	opnplug_addvalue(opnplug_params, "fb12" ,getparam('feedback'))
+	opnplug_addvalue(opnplug_params, "fb34" ,0)
+	opnplug_addvalue(opnplug_params, "midi_velocity_offset" ,0)
+	opnplug_addvalue(opnplug_params, "second_voice_detune" ,0)
+	opnplug_addvalue(opnplug_params, "percussion_key_number" ,0)
 
-	outputjson['Algorithm'] = (getparam('fm')*-1)+1
-	outputjson['Carrier_Attack'] = ((getparam('op1_env_attack')/15)*-1)+1
-	outputjson['Carrier_Attenuation'] = 0.0
-	outputjson['Carrier_Decay'] = ((getparam('op1_env_decay')/15)*-1)+1
-	outputjson['Carrier_Frequency_Multiplier'] = ((getparam('op1_freqmul')/15)*-1)+1
-	outputjson['Carrier_Keyscale_Level'] = 0.0
-	outputjson['Carrier_Keyscale_Rate'] = getparam('op1_ksr')
-	outputjson['Carrier_Release'] = ((getparam('op1_env_release')/15)*-1)+1
-	outputjson['Carrier_Sustain'] = 0.0
-	outputjson['Carrier_Sustain_Level'] = ((getparam('op1_env_sustain')/15)*-1)+1
-	outputjson['Carrier_Tremolo'] = getparam('op1_tremolo')
-	outputjson['Carrier_Velocity_Sensitivity'] = 0.0
-	outputjson['Carrier_Vibrato'] = getparam('op1_vibrato')
-	outputjson['Carrier_Wave'] = float(getparam('op1_waveform')/7)
-	outputjson['Emulator'] = 0.0
-	outputjson['lastLoadFile'] = ""
-	outputjson['Modulator_Attack'] = ((getparam('op2_env_attack')/15)*-1)+1
-	outputjson['Modulator_Attenuation'] = 0.0
-	outputjson['Modulator_Decay'] = ((getparam('op2_env_decay')/15)*-1)+1
-	outputjson['Modulator_Feedback'] = getparam('feedback')/7
-	outputjson['Modulator_Frequency_Multiplier'] = ((getparam('op2_freqmul')/15)*-1)+1
-	outputjson['Modulator_Keyscale_Level'] = 0.0
-	outputjson['Modulator_Keyscale_Rate'] = getparam('op2_ksr')
-	outputjson['Modulator_Release'] = ((getparam('op2_env_release')/15)*-1)+1
-	outputjson['Modulator_Sustain'] = 0.0
-	outputjson['Modulator_Sustain_Level'] = ((getparam('op2_env_sustain')/15)*-1)+1
-	outputjson['Modulator_Tremolo'] = getparam('op2_tremolo')
-	outputjson['Modulator_Velocity_Sensitivity'] = 0.0
-	outputjson['Modulator_Vibrato'] = getparam('op2_vibrato')
-	outputjson['Modulator_Wave'] = float(getparam('op2_waveform')/7)
-	outputjson['Percussion_Mode'] = float(getparam('perctype')/5)
-	outputjson['Program_Index'] = 0
-	outputjson['selectedIdxFile'] = -1
-	outputjson['Tremolo_Depth'] = getparam('tremolo_depth')
-	outputjson['Vibrato_Depth'] = getparam('vibrato_depth')
+	for opnum in range(4):
+		optxt = "op"+str(opnum+1)
+		s_opadltxt = opadltxt[opnum]
+		opnplug_addvalue(opnplug_params, s_opadltxt+"attack" ,getparam(optxt+"_env_attack"))
+		opnplug_addvalue(opnplug_params, s_opadltxt+"decay" ,getparam(optxt+"_env_decay"))
+		opnplug_addvalue(opnplug_params, s_opadltxt+"sustain" ,getparam(optxt+"_env_sustain"))
+		opnplug_addvalue(opnplug_params, s_opadltxt+"release" ,getparam(optxt+"_env_release"))
+		opnplug_addvalue(opnplug_params, s_opadltxt+"level" ,getparam(optxt+"_level"))
+		opnplug_addvalue(opnplug_params, s_opadltxt+"ksl" ,getparam(optxt+"_ksr"))
+		opnplug_addvalue(opnplug_params, s_opadltxt+"fmul" ,getparam(optxt+"_freqmul"))
+		opnplug_addvalue(opnplug_params, s_opadltxt+"trem" ,getparam(optxt+"_tremolo"))
+		opnplug_addvalue(opnplug_params, s_opadltxt+"vib" ,getparam(optxt+"_vibrato"))
+		opnplug_addvalue(opnplug_params, s_opadltxt+"sus" ,0)
+		opnplug_addvalue(opnplug_params, s_opadltxt+"env" ,getparam(optxt+"_perc_env"))
+		opnplug_addvalue(opnplug_params, s_opadltxt+"wave" ,getparam(optxt+"_waveform"))
 
-	oplvstbytes = json.dumps(outputjson, indent=2).replace("\n", "\r\n").encode('ascii')
+	opnplug_addvalue(opnplug_params, "delay_off_ms" ,160)
+	opnplug_addvalue(opnplug_params, "delay_on_ms" ,386)
+	opnplug_addvalue(opnplug_params, "bank" ,0)
+	opnplug_addvalue(opnplug_params, "program" ,0)
+	opnplug_addvalue(opnplug_params, "name" ,'')
 
-	plugin_vst2.replace_data(cvpj_l, pluginid, 'any', 'JuceOPLVSTi', 'chunk', oplvstbytes, None)
+	opnplug_selection = ET.SubElement(adlplug_root, 'selection')
+	opnplug_addvalue(opnplug_selection, "part" ,0)
+	opnplug_addvalue(opnplug_selection, "bank" ,0)
+	opnplug_addvalue(opnplug_selection, "program" ,0)
+
+	opnplug_chip = ET.SubElement(adlplug_root, 'chip')
+	opnplug_addvalue(opnplug_chip, "emulator" ,2)
+	opnplug_addvalue(opnplug_chip, "chip_count" ,2)
+	opnplug_addvalue(opnplug_chip, "4op_count" ,0)
+
+	opnplug_global = ET.SubElement(adlplug_root, 'global')
+	opnplug_addvalue(opnplug_global, "volume_model" ,0)
+	opnplug_addvalue(opnplug_global, "deep_tremolo" ,getparam("tremolo_depth"))
+	opnplug_addvalue(opnplug_global, "deep_vibrato" ,getparam("vibrato_depth"))
+
+	opnplug_common = ET.SubElement(adlplug_root, 'common')
+	opnplug_addvalue(opnplug_common, "bank_title" ,'DawVert')
+	opnplug_addvalue(opnplug_common, "part" ,0)
+	opnplug_addvalue(opnplug_common, "master_volume" ,1.0)
+
+	outfile = ET.ElementTree(adlplug_root)
+
+	plugin_vst2.replace_data(cvpj_l, pluginid, 'any', 'ADLplug', 'chunk', data_vc2xml.make(adlplug_root), None)
+
+	plugins.add_plug_data(cvpj_l, pluginid, 'middlenotefix', -12)
 	return True

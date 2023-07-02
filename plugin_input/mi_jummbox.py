5e429344c4cf0601a098c7d1f2907a1532aaf162
SatyrDiamond <colbydray@gmail.com>
2023-07-02 01:02:09 -0400

    update

--- plugin_input/mi_jummbox.py
+++ plugin_input/mi_jummbox.py
@@ -7,6 +7,7 @@ from functions import idvals
 from functions import tracks
 from functions import plugins
 from functions import note_data
+from functions import data_values
 from functions import placement_data
 from functions import song
 import plugin_input
@@ -20,6 +21,29 @@ colornum_d = 0
 colors_pitch = [[0, 0.6, 0.63], [0.63, 0.63, 0], [0.78, 0.31, 0], [0, 0.63, 0], [0.82, 0.13, 0.82], [0.47, 0.47, 0.69], [0.54, 0.63, 0], [0.87, 0, 0.1], [0, 0.63, 0.44], [0.57, 0.12, 1]]
 
 colors_drums = [ [0.44, 0.44, 0.44], [0.6, 0.4, 0.2], [0.29, 0.43, 0.56], [0.48, 0.31, 0.6], [0.38, 0.47, 0.22]]
+
+rawChipWaves = {}
+rawChipWaves["rounded"] = {"expression": 0.94, "samples": [0,0.2,0.4,0.5,0.6,0.7,0.8,0.85,0.9,0.95,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0.95,0.9,0.85,0.8,0.7,0.6,0.5,0.4,0.2,0,-0.2,-0.4,-0.5,-0.6,-0.7,-0.8,-0.85,-0.9,-0.95,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-0.95,-0.9,-0.85,-0.8,-0.7,-0.6,-0.5,-0.4,-0.2]}
+rawChipWaves["triangle"] = {"expression": 1, "samples": [1/15,0.2,5/15,7/15,0.6,11/15,13/15,1,1,13/15,11/15,0.6,7/15,5/15,0.2,1/15,-1/15,-0.2,-5/15,-7/15,-0.6,-11/15,-13/15,-1,-1,-13/15,-11/15,-0.6,-7/15,-5/15,-0.2,-1/15]}
+rawChipWaves["square"] = {"expression": 0.5, "samples": [1,-1]}
+rawChipWaves["1/4 pulse"] = {"expression": 0.5, "samples": [1,-1,-1,-1]}
+rawChipWaves["1/8 pulse"] = {"expression": 0.5, "samples": [1,-1,-1,-1,-1,-1,-1,-1]}
+rawChipWaves["sawtooth"] = {"expression": 0.65, "samples": [1/31,3/31,5/31,7/31,9/31,11/31,13/31,15/31,17/31,19/31,21/31,23/31,25/31,27/31,29/31,1,-1,-29/31,-27/31,-25/31,-23/31,-21/31,-19/31,-17/31,-15/31,-13/31,-11/31,-9/31,-7/31,-5/31,-3/31,-1/31]}
+rawChipWaves["double saw"] = {"expression": 0.5, "samples": [0,-0.2,-0.4,-0.6,-0.8,-1,1,-0.8,-0.6,-0.4,-0.2,1,0.8,0.6,0.4,0.2]}
+rawChipWaves["double pulse"] = {"expression": 0.4, "samples": [1,1,1,1,1,-1,-1,-1,1,1,1,1,-1,-1,-1,-1]}
+rawChipWaves["spiky"] = {"expression": 0.4, "samples": [1,-1,1,-1,1,0]}
+rawChipWaves["sine"] = {"expression": 0.88, "samples": [8,9,11,12,13,14,15,15,15,15,14,14,13,11,10,9,7,6,4,3,2,1,0,0,0,0,1,1,2,4,5,6]}
+rawChipWaves["flute"] = {"expression": 0.8, "samples": [3,4,6,8,10,11,13,14,15,15,14,13,11,8,5,3]}
+rawChipWaves["harp"] = {"expression": 0.8, "samples": [0,3,3,3,4,5,5,6,7,8,9,11,11,13,13,15,15,14,12,11,10,9,8,7,7,5,4,3,2,1,0,0]}
+rawChipWaves["sharp clarinet"] = {"expression": 0.38, "samples": [0,0,0,1,1,8,8,9,9,9,8,8,8,8,8,9,9,7,9,9,10,4,0,0,0,0,0,0,0,0,0,0]}
+rawChipWaves["soft clarinet"] = {"expression": 0.45, "samples": [0,1,5,8,9,9,9,9,9,9,9,11,11,12,13,12,10,9,7,6,4,3,3,3,1,1,1,1,1,1,1,1]}
+rawChipWaves["alto sax"] = {"expression": 0.3, "samples": [5,5,6,4,3,6,8,7,2,1,5,6,5,4,5,7,9,11,13,14,14,14,14,13,10,8,7,7,4,3,4,2]}
+rawChipWaves["bassoon"] = {"expression": 0.35, "samples": [9,9,7,6,5,4,4,4,4,5,7,8,9,10,11,13,13,11,10,9,7,6,4,2,1,1,1,2,2,5,11,14]}
+rawChipWaves["trumpet"] = {"expression": 0.22, "samples": [10,11,8,6,5,5,5,6,7,7,7,7,6,6,7,7,7,7,7,6,6,6,6,6,6,6,6,7,8,9,11,14]}
+rawChipWaves["electric guitar"] = {"expression": 0.2, "samples": [11,12,12,10,6,6,8,0,2,4,8,10,9,10,1,7,11,3,6,6,8,13,14,2,0,12,8,4,13,11,10,13]}
+rawChipWaves["organ"] = {"expression": 0.2, "samples": [11,10,12,11,14,7,5,5,12,10,10,9,12,6,4,5,13,12,12,10,12,5,2,2,8,6,6,5,8,3,2,1]}
+rawChipWaves["pan flute"] = {"expression": 0.35, "samples": [1,4,7,6,7,9,7,7,11,12,13,15,13,11,11,12,13,10,7,5,3,6,10,7,3,3,1,0,1,0,1,0]}
+rawChipWaves["glitch"] = {"expression": 0.5, "samples": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,-1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,-1,-1,1,1,1,1,1,1,1,1,1,1,1,1,1,-1,-1,-1,1,1,1,1,1,1,1,1,1,1,1,1,-1,-1,-1,-1,1,1,1,1,1,1,1,1,1,1,1,-1,-1,-1,-1,-1,1,1,1,1,1,1,1,1,1,1,-1,-1,-1,-1,-1,-1,1,1,1,1,1,1,1,1,1,-1,-1,-1,-1,-1,-1,-1,1,1,1,1,1,1,1,1,-1,-1,-1,-1,-1,-1,-1,-1,1,1,1,1,1,1,1,1,1,-1,-1,-1,-1,-1,-1,-1,1,1,1,1,1,1,1,1,1,1,-1,-1,-1,-1,-1,-1,1,1,1,1,1,1,1,1,1,1,1,-1,-1,-1,-1,-1,1,1,1,1,1,1,1,1,1,1,1,1,-1,-1,-1,-1,1,1,1,1,1,1,1,1,1,1,1,1,1,-1,-1,-1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,-1,-1]}
 
 noteoffset = {}
 noteoffset['B'] = 11
@@ -66,10 +90,15 @@ def calcval(value):
     global jummbox_ticksPerBeat
     return (value*(jummbox_beatsPerBar/jummbox_ticksPerBeat))/2
 
+def addfx(trackid, fxname):
+    pluginid = trackid+'_'+fxname
+    plugins.add_plug(cvpj_l, pluginid, 'native-jummbox', fxname)
+    plugins.add_plug_fxdata(cvpj_l, pluginid, True, 1)
+    tracks.insert_fxslot(cvpj_l, ['track', trackid], 'audio', pluginid)
+    return pluginid
+
 def parse_instrument(channum, instnum, bb_instrument, bb_type, bb_color):
     global idvals_inst_beepbox
-    bb_effects = bb_instrument['effects']
-    bb_type = bb_instrument['type']
     bb_volume = bb_instrument['volume']
 
     if 'preset' in bb_instrument: bb_preset = str(bb_instrument['preset'])
@@ -84,17 +113,94 @@ def parse_instrument(channum, instnum, bb_instrument, bb_type, bb_color):
     if bb_preset in idvals_inst_beepbox:
         gm_inst = idvals.get_idval(idvals_inst_beepbox, bb_preset, 'gm_inst')
 
-    pluginid = plugins.get_id()
-
-    if gm_inst == None:
-        plugins.add_plug(cvpj_l, pluginid, 'native-jummbox', bb_type)
-        plugins.add_plug_data(cvpj_l, pluginid, 'data', bb_instrument)
-        cvpj_instname = inst_names[bb_type]
-    else:
-        plugins.add_plug_gm_midi(cvpj_l, pluginid, 0, gm_inst)
+    if gm_inst != None:
+        plugins.add_plug_gm_midi(cvpj_l, trackid, 0, gm_inst)
         cvpj_instname = idvals.get_idval(idvals_inst_beepbox, bb_preset, 'name')
+    else:
+        bb_inst_effects = bb_instrument['effects']
+        bb_inst_type = bb_instrument['type']
+        plugins.add_plug(cvpj_l, trackid, 'native-jummbox', bb_inst_type)
 
-    tracks.m_create_inst(cvpj_l, trackid, {'pluginid': pluginid})
+        if bb_inst_type == 'chip':
+            bb_inst_wave = bb_instrument['wave']
+            if bb_inst_wave in rawChipWaves:
+                wavesample = rawChipWaves[bb_inst_wave]['samples']
+                plugins.add_wave(cvpj_l, trackid, 'chipwave', wavesample, min(wavesample), max(wavesample))
+
+        if bb_inst_type == 'PWM':
+            plugins.add_plug_param(cvpj_l, trackid, "pulse_width", bb_instrument['pulseWidth']/100, 'float', "Pulse Width")
+
+        if bb_inst_type == 'harmonics':
+            plugins.add_harmonics(cvpj_l, trackid, 'harmonics', [i/100 for i in bb_instrument['harmonics']])
+
+        if bb_inst_type == 'Picked String':
+            plugins.add_harmonics(cvpj_l, trackid, 'harmonics', [i/100 for i in bb_instrument['harmonics']])
+            plugins.add_plug_param(cvpj_l, trackid, "sustain", bb_instrument['stringSustain']/100, 'float', "Sustain")
+
+        if bb_inst_type == 'spectrum':
+            plugins.add_plug_data(cvpj_l, trackid, 'spectrum', bb_instrument['spectrum'])
+
+        if bb_inst_type == 'FM':
+            plugins.add_plug_data(cvpj_l, trackid, 'algorithm', bb_instrument['algorithm'])
+            plugins.add_plug_data(cvpj_l, trackid, 'feedback_type', bb_instrument['feedbackType'])
+            plugins.add_plug_param(cvpj_l, trackid, "feedback_amplitude", bb_instrument['feedbackAmplitude'], 'int', "Feedback Amplitude")
+
+            for opnum in range(4):
+                opdata = bb_instrument['operators'][opnum]
+                opnumtext = 'op'+str(opnum+1)+'_'
+                plugins.add_plug_data(cvpj_l, trackid, opnumtext+'frequency', opdata['frequency'])
+                plugins.add_plug_data(cvpj_l, trackid, opnumtext+'waveform', opdata['waveform'])
+                plugins.add_plug_data(cvpj_l, trackid, opnumtext+'pulseWidth', opdata['pulseWidth'])
+                plugins.add_plug_param(cvpj_l, trackid, opnumtext+"amplitude", opdata['amplitude'], 'int', "")
+
+        if bb_inst_type == 'custom chip':
+            customChipWave = bb_instrument['customChipWave']
+            customChipWave = [customChipWave[str(i)] for i in range(64)]
+            plugins.add_wave(cvpj_l, trackid, 'chipwave', customChipWave, -24, 24)
+
+        if 'eqFilterType' in bb_instrument:
+            if bb_instrument['eqFilterType'] == False:
+                pluginid = addfx(trackid, 'eq')
+                plugins.add_plug_data(cvpj_l, pluginid, 'eq', bb_instrument['eqFilter'])
+                for subfilternum in range(8):
+                    subfname = 'eqSubFilters'+str(subfilternum)
+                    if subfname in bb_instrument:
+                        plugins.add_plug_data(cvpj_l, pluginid, 'subfilter'+str(subfilternum), bb_instrument[subfname])
+            else:
+                pluginid = addfx(trackid, 'filter')
+                plugins.add_plug_param(cvpj_l, pluginid, 'cutoff', bb_instrument['eqSimpleCut'], 'int', "")
+                plugins.add_plug_param(cvpj_l, pluginid, 'reso', bb_instrument['eqSimplePeak'], 'int', "")
+
+        if 'distortion' in bb_inst_effects:
+            pluginid = addfx(trackid, 'distortion')
+            plugins.add_plug_param(cvpj_l, pluginid, 'amount', bb_instrument['distortion']/100, 'float', "")
+            plugins.add_plug_data(cvpj_l, pluginid, 'aliases', bb_instrument['aliases'])
+
+        if 'bitcrusher' in bb_inst_effects:
+            pluginid = addfx(trackid, 'bitcrusher')
+            plugins.add_plug_param(cvpj_l, pluginid, 'octave', bb_instrument['bitcrusherOctave'], 'float', "")
+            plugins.add_plug_param(cvpj_l, pluginid, 'quantization', bb_instrument['bitcrusherQuantization']/100, 'float', "")
+
+        if 'chorus' in bb_inst_effects:
+            pluginid = addfx(trackid, 'chorus')
+            plugins.add_plug_param(cvpj_l, pluginid, 'amount', bb_instrument['chorus']/100, 'float', "")
+
+        if 'echo' in bb_inst_effects:
+            pluginid = addfx(trackid, 'echo')
+            plugins.add_plug_param(cvpj_l, pluginid, 'sustain', bb_instrument['echoSustain']/100, 'float', "")
+            plugins.add_plug_param(cvpj_l, pluginid, 'delay_beats', bb_instrument['echoDelayBeats'], 'int', "")
+
+        if 'reverb' in bb_inst_effects:
+            pluginid = addfx(trackid, 'reverb')
+            plugins.add_plug_param(cvpj_l, pluginid, 'amount', bb_instrument['reverb']/100, 'float', "")
+
+        a_attack = data_values.get_value(bb_instrument, 'fadeInSeconds', 0)
+        a_release = abs(data_values.get_value(bb_instrument, 'fadeOutTicks', 0)/(jummbox_ticksPerBeat*32))
+        plugins.add_asdr_env(cvpj_l, trackid, 'vol', 0, a_attack, 0, 0, 1, a_release, 1)
+
+        cvpj_instname = inst_names[bb_inst_type]
+
+    tracks.m_create_inst(cvpj_l, trackid, {'pluginid': trackid})
     tracks.m_basicdata_inst(cvpj_l, trackid, cvpj_instname, bb_color, None, None)
 
 def parse_notes(channum, bb_notes, bb_instruments):

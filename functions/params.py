bdf7c3bfc7466a303d7dd1b6b2b0992748d70cc2
SatyrDiamond <satyrdiamond@gmail.com>
2023-07-22 08:33:47 -0400

    update

---
+++ functions/params.py
@@ -0,0 +1,34 @@
+# SPDX-FileCopyrightText: 2023 SatyrDiamond
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from functions import data_values
+
+visname = {
+    'bpm': 'Tempo',
+    'vol': 'Volume',
+    'pan': 'Pan',
+    'solo': 'Solo',
+    'enabled': 'On',
+    'pitch': 'Pitch'
+}
+
+def add(cvpj_l, location, p_id, p_value, p_type, **kwargs):
+    param_data = {}
+    param_data['type'] = p_type
+
+    if 'visname' not in kwargs:
+        if p_id in visname: param_data['name'] = visname[p_id]
+        else: param_data['name'] = p_id
+    else: param_data['name'] = kwargs['visname']
+
+    if p_type == 'float': param_data['value'] = float(p_value)
+    if p_type == 'int': param_data['value'] = int(p_value)
+    if p_type == 'bool': param_data['value'] = bool(p_value)
+    if p_type == 'string': param_data['value'] = p_value
+
+    data_values.nested_dict_add_value(cvpj_l, location+['params', p_id], param_data)
+
+def get(cvpj_l, location, p_id, fallbackval):
+    paramdata = data_values.nested_dict_get_value(cvpj_l, location+['params', p_id])
+    if paramdata != None: return paramdata['value'], paramdata['type'], paramdata['name']
+    return fallbackval, 'notfound', ''

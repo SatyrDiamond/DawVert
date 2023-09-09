49ce382d6f3b3faa222b0c68538954d7a6b36b1a
SatyrDiamond <satyrdiamond@gmail.com>
2023-09-09 07:06:30 -0400

    update

--- plugin_output/midi.py
+++ plugin_output/midi.py
@@ -11,6 +11,7 @@ from functions import notelist_data
 from functions import xtramath
 from functions import params
 from functions import colors
+from functions import tracks
 
 unusedchannel = 0
 
@@ -40,7 +41,7 @@ class output_cvpj_f(plugin_output.base):
         }
     def getsupportedplugins(self): return []
     def parse(self, convproj_json, output_file):
-        projJ = json.loads(convproj_json)
+        cvpj_l = json.loads(convproj_json)
 
         idvals_names_gmmidi = idvals.parse_idvalscsv('data_idvals/names_gmmidi.csv')
         idvals_names_gmmidi_drums = idvals.parse_idvalscsv('data_idvals/names_gmmidi_drums.csv')
@@ -56,11 +57,11 @@ class output_cvpj_f(plugin_output.base):
 
         multi_miditrack = []
 
-        if 'track_data' in projJ and 'track_order' in projJ:
+        if 'track_data' in cvpj_l and 'track_order' in cvpj_l:
             #print('[output-midi] Trk# | Ch# | Bnk | Prog | Key | Name')
             print('[output-midi] Trk# | Ch# | Prog | Name')
 
-            for _ in range(len(projJ['track_order'])+1):
+            for _ in range(len(cvpj_l['track_order'])+1):
                 multi_miditrack.append(mido.MidiTrack())
 
             tracknum = 0
@@ -69,115 +70,109 @@ class output_cvpj_f(plugin_output.base):
             midi_numerator = 4
             midi_denominator = 4
 
-            if 'timesig' in projJ: midi_numerator, midi_denominator = projJ['timesig']
-            midi_tempo = mido.bpm2tempo(params.get(projJ, [], 'bpm', 120)[0])
+            if 'timesig' in cvpj_l: midi_numerator, midi_denominator = cvpj_l['timesig']
+            midi_tempo = mido.bpm2tempo(params.get(cvpj_l, [], 'bpm', 120)[0])
 
             multi_miditrack[0].append(mido.MetaMessage('time_signature', numerator=midi_numerator, denominator=midi_denominator, clocks_per_click=24, notated_32nd_notes_per_beat=8, time=0))
 
             multi_miditrack[0].append(mido.MetaMessage('set_tempo', tempo=midi_tempo, time=0))
 
-            for trackid in projJ['track_order']:
-                if trackid in projJ['track_data']:
-                    trackdata = projJ['track_data'][trackid]
+            for cvpj_trackid, cvpj_trackdata, track_placements in tracks.r_track_iter(cvpj_l):
+                midi_defined = False
+                midi_channel = None
+                midi_bank = None
+                midi_program = None
+                midi_key = None
+                midi_trackname = ''
+                midi_trackcolor = None
 
-                    midi_defined = False
-                    midi_channel = None
-                    midi_bank = None
-                    midi_program = None
-                    midi_key = None
-                    midi_trackname = ''
-                    midi_trackcolor = None
+                if 'name' in cvpj_trackdata: midi_trackname = cvpj_trackdata['name']
+                if 'color' in cvpj_trackdata: midi_trackcolor = cvpj_trackdata['color']
 
-                    if 'name' in trackdata: midi_trackname = trackdata['name']
-                    if 'color' in trackdata: midi_trackcolor = trackdata['color']
+                if guess_gmmidi == True:
+                    if midi_trackname != '':
+                        guessedinst = difflib.get_close_matches(midi_trackname, wordlist)
+                        if guessedinst:
+                            midi_defined = True
+                            midi_program = idvals.get_idval(idvals_names_gmmidi, guessedinst[0], 'gm_inst')
 
-                    if guess_gmmidi == True:
-                        if midi_trackname != '':
-                            guessedinst = difflib.get_close_matches(midi_trackname, wordlist)
-                            if guessedinst:
+                if 'instdata' in cvpj_trackdata:
+                    trackinstdata = cvpj_trackdata['instdata']
+                    if 'plugin' in trackinstdata:
+                        if trackinstdata['plugin'] == 'general-midi':
+                            if 'plugindata' in trackinstdata:
                                 midi_defined = True
-                                midi_program = idvals.get_idval(idvals_names_gmmidi, guessedinst[0], 'gm_inst')
+                                if trackinstdata['plugindata']['bank'] < 128:
+                                    midi_program = trackinstdata['plugindata']['inst']
+                                    midi_bank = trackinstdata['plugindata']['bank']
+                                else:
+                                    midi_bank = trackinstdata['plugindata']['bank']-128
 
-                    if 'instdata' in trackdata:
-                        trackinstdata = trackdata['instdata']
-                        if 'plugin' in trackinstdata:
-                            if trackinstdata['plugin'] == 'general-midi':
-                                if 'plugindata' in trackinstdata:
-                                    midi_defined = True
-                                    if trackinstdata['plugindata']['bank'] < 128:
-                                        midi_program = trackinstdata['plugindata']['inst']
-                                        midi_bank = trackinstdata['plugindata']['bank']
-                                    else:
-                                        midi_bank = trackinstdata['plugindata']['bank']-128
+                if 'fxrack_channel' in cvpj_trackdata:
+                    if 16 > cvpj_trackdata['fxrack_channel'] > 1:
+                        midi_channel = cvpj_trackdata['fxrack_channel']-1
 
-                    if 'fxrack_channel' in trackdata:
-                        if 16 > trackdata['fxrack_channel'] > 1:
-                            midi_channel = trackdata['fxrack_channel']-1
+                #print(cvpj_trackdata)
 
-                    #print(trackdata)
+                if midi_channel == None: midi_channel = getunusedchannel()
 
-                    if midi_channel == None: midi_channel = getunusedchannel()
+                print('[output-midi]', end=" ")
 
-                    print('[output-midi]', end=" ")
-
-                    print(str(tracknum+1).rjust(4), end=" | ")
+                print(str(tracknum+1).rjust(4), end=" | ")
                 
-                    if midi_channel == None: print('---', end=" | ")
-                    else: print(str(midi_channel).rjust(3), end=" | ")
+                if midi_channel == None: print('---', end=" | ")
+                else: print(str(midi_channel).rjust(3), end=" | ")
 
-                    #if midi_bank == None: print('---', end=" | ")
-                    #else: print(str(midi_bank).rjust(3), end=" | ")
+                #if midi_bank == None: print('---', end=" | ")
+                #else: print(str(midi_bank).rjust(3), end=" | ")
     
-                    if midi_program == None: print('----', end=" | ")
-                    else: print(str(midi_program).rjust(4), end=" | ")
+                if midi_program == None: print('----', end=" | ")
+                else: print(str(midi_program).rjust(4), end=" | ")
     
-                    #if midi_key == None: print('---', end=" | ")
-                    #else: print(str(midi_key).rjust(3), end=" | ")
-                    print(midi_trackname)
+                #if midi_key == None: print('---', end=" | ")
+                #else: print(str(midi_key).rjust(3), end=" | ")
+                print(midi_trackname)
 
-                    miditrack = multi_miditrack[tracknum+1]
+                miditrack = multi_miditrack[tracknum+1]
 
-                    if midi_trackname != '': miditrack.append(mido.MetaMessage('track_name', name=midi_trackname, time=0))
+                if midi_trackname != '': miditrack.append(mido.MetaMessage('track_name', name=midi_trackname, time=0))
 
-                    if midi_trackcolor != None: 
-                        midi_trackcolor = colors.rgb_float_to_rgb_int(midi_trackcolor)
-                        miditrack.append(mido.MetaMessage('sequencer_specific', data=(83, 105, 103, 110, 1, 255)+midi_trackcolor[::-1])) #from Signal MIDI Editor
+                if midi_trackcolor != None: 
+                    midi_trackcolor = colors.rgb_float_to_rgb_int(midi_trackcolor)
+                    miditrack.append(mido.MetaMessage('sequencer_specific', data=(83, 105, 103, 110, 1, 255)+midi_trackcolor[::-1])) #from Signal MIDI Editor
 
-                    if midi_program != None: miditrack.append(mido.Message('program_change', channel=midi_channel, program=midi_program, time=0))
-                    else: miditrack.append(mido.Message('program_change', channel=midi_channel, program=0, time=0))
+                if midi_program != None: miditrack.append(mido.Message('program_change', channel=midi_channel, program=midi_program, time=0))
+                else: miditrack.append(mido.Message('program_change', channel=midi_channel, program=0, time=0))
 
-                    i_list = {}
+                i_list = {}
 
-                    if trackid in projJ['track_placements']:
-                        if 'notes' in projJ['track_placements'][trackid]:
-                            for cvpj_tr_pl in projJ['track_placements'][trackid]['notes']:
-                                cvpj_tr_pl_pos = cvpj_tr_pl['position']
-                                cvpj_tr_pl_nl = cvpj_tr_pl['notelist']
-                                cvpj_tr_pl_muted = False
-                                if 'muted' in cvpj_tr_pl: cvpj_tr_pl_muted = cvpj_tr_pl['muted']
-                                if cvpj_tr_pl_muted == False:
-                                    for cvpj_tr_pl_n in cvpj_tr_pl_nl:
-                                        #print(cvpj_tr_pl_n)
-                                        cvmi_n_pos = int(cvpj_tr_pl_pos*4 + cvpj_tr_pl_n['position']*4)*30
-                                        cvmi_n_dur = int(cvpj_tr_pl_n['duration']*4)*30
-                                        cvmi_n_key = int(cvpj_tr_pl_n['key'])+60
-                                        cvmi_n_vol = 127
-                                        if 'vol' in cvpj_tr_pl_n: cvmi_n_vol = xtramath.clamp(int(cvpj_tr_pl_n['vol']*127), 0, 127)
-                                        add_cmd(i_list, cvmi_n_pos, ['note_on', cvmi_n_key, cvmi_n_vol])
-                                        add_cmd(i_list, cvmi_n_pos+cvmi_n_dur, ['note_off', cvmi_n_key])
-                                        #print(cvmi_n_pos, cvmi_n_dur, cvmi_n_key)
+                if 'notes' in track_placements:
+                    for cvpj_tr_pl in track_placements['notes']:
+                        cvpj_tr_pl_pos = cvpj_tr_pl['position']
+                        cvpj_tr_pl_nl = cvpj_tr_pl['notelist']
+                        cvpj_tr_pl_muted = False
+                        if 'muted' in cvpj_tr_pl: cvpj_tr_pl_muted = cvpj_tr_pl['muted']
+                        if cvpj_tr_pl_muted == False:
+                            for cvpj_tr_pl_n in cvpj_tr_pl_nl:
+                                cvmi_n_pos = int(cvpj_tr_pl_pos*4 + cvpj_tr_pl_n['position']*4)*30
+                                cvmi_n_dur = int(cvpj_tr_pl_n['duration']*4)*30
+                                cvmi_n_key = int(cvpj_tr_pl_n['key'])+60
+                                cvmi_n_vol = 127
+                                if 'vol' in cvpj_tr_pl_n: cvmi_n_vol = xtramath.clamp(int(cvpj_tr_pl_n['vol']*127), 0, 127)
+                                add_cmd(i_list, cvmi_n_pos, ['note_on', cvmi_n_key, cvmi_n_vol])
+                                add_cmd(i_list, cvmi_n_pos+cvmi_n_dur, ['note_off', cvmi_n_key])
 
-                    i_list = dict(sorted(i_list.items(), key=lambda item: item[0]))
+                i_list = dict(sorted(i_list.items(), key=lambda item: item[0]))
 
-                    prevpos = 0
-                    for i_list_e in i_list:
-                        for midi_notedata in i_list[i_list_e]:
-                            #print(i_list_e, i_list_e-prevpos, i_list[i_list_e])
-                            if midi_notedata[0] == 'note_on': miditrack.append(mido.Message('note_on', channel=midi_channel, note=midi_notedata[1], velocity=midi_notedata[2], time=i_list_e-prevpos))
-                            if midi_notedata[0] == 'note_off': miditrack.append(mido.Message('note_off', channel=midi_channel, note=midi_notedata[1], time=i_list_e-prevpos))
-                            prevpos = i_list_e
+                prevpos = 0
+                for i_list_e in i_list:
+                    for midi_notedata in i_list[i_list_e]:
+                        #print(i_list_e, i_list_e-prevpos, i_list[i_list_e])
+                        if midi_notedata[0] == 'note_on': miditrack.append(mido.Message('note_on', channel=midi_channel, note=midi_notedata[1], velocity=midi_notedata[2], time=i_list_e-prevpos))
+                        if midi_notedata[0] == 'note_off': miditrack.append(mido.Message('note_off', channel=midi_channel, note=midi_notedata[1], time=i_list_e-prevpos))
+                        prevpos = i_list_e
 
                 tracknum += 1
 
-        for tracknum in range(len(projJ['track_order'])+1): midiobj.tracks.append(multi_miditrack[tracknum])
+        for tracknum in range(len(cvpj_l['track_order'])+1): midiobj.tracks.append(multi_miditrack[tracknum])
         midiobj.save(output_file)

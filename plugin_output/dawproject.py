59e23bfe2dd95fa4a12c89a4b13ba8adb484331b
SatyrDiamond <colbydray@gmail.com>
2023-09-21 21:40:07 -0400

    file extension output

--- plugin_output/dawproject.py
+++ plugin_output/dawproject.py
@@ -128,6 +128,7 @@ class output_cvpj(plugin_output.base):
         }
     def getsupportedplugformats(self): return ['vst2', 'vst3', 'clap']
     def getsupportedplugins(self): return []
+    def getfileextension(self): return 'dawproject'
     def parse(self, convproj_json, output_file):
         global NoteStep
         global tracknum

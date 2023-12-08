d3b875452e68cb8499d483cdcdb0e61f8c54cb50
SatyrDiamond <satyrdiamond@gmail.com>
2023-12-08 02:55:25 -0500

    debug

---
+++ __datadef_decode.py
@@ -0,0 +1,34 @@
+import argparse
+import json
+from functions import data_datadef
+import sys
+sys.path.append('../')
+
+aparser = argparse.ArgumentParser()
+aparser.add_argument("-d", default=None)
+aparser.add_argument("-f", default=None)
+aparser.add_argument("-s", default='main')
+
+argsd = vars(aparser.parse_args())
+
+if argsd['d'] != None:
+    df_file = argsd['d']
+    datadef.load_file(df_file)
+    
+if argsd['f'] != None:
+    dat_file = argsd['f']
+    databytes_str = open(dat_file, "rb")
+    databytes = databytes_str.read()
+
+if argsd['s'] != None: 
+    structname = argsd['s']
+
+datadef = data_datadef.datadef(df_file)
+
+datadef.parse(structname,databytes)
+
+if datadef.errored:
+    print('[error] '+datadef.errormeg)
+else:
+    with open('out.json', "w") as fileout:
+        json.dump(datadef.output, fileout, indent=4, sort_keys=True)
---
+++ __datadef_encode.py
@@ -0,0 +1,39 @@
+import argparse
+import json
+from functions import data_datadef
+import sys
+sys.path.append('../')
+
+aparser = argparse.ArgumentParser()
+aparser.add_argument("-d", default=None)
+aparser.add_argument("-j", default=None)
+aparser.add_argument("-s", default='main')
+
+argsd = vars(aparser.parse_args())
+
+datadef = data_datadef.datadef()
+
+if argsd['d'] != None:
+    df_file = argsd['d']
+    datadef.load_file(df_file)
+    
+in_data = {}
+if argsd['j'] != None:
+    dat_file = argsd['j']
+    f = open(argsd['j'], "r")
+    in_data = json.load(f)
+
+if argsd['s'] != None: 
+    structname = argsd['s']
+
+#print(   in_data  )
+
+datadef.create(structname, in_data)
+
+print(   datadef.bytestream.getvalue()   )
+
+if datadef.errored:
+    print('[error] '+datadef.errormeg)
+else:
+    with open('out.raw', "w") as fileout:
+        json.dump(datadef.output, fileout, indent=4, sort_keys=True)

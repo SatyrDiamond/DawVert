de6113cd759ccac40e214c61d00c51147d346035
SatyrDiamond <satyrdiamond@gmail.com>
2024-09-30 19:38:32 -0400

    add missing

---
+++ plugins/externalsearch/ardour.py
@@ -0,0 +1,82 @@
+# SPDX-FileCopyrightText: 2024 SatyrDiamond
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import os
+import plugins
+from os.path import exists
+from pathlib import Path
+import xml.etree.ElementTree as ET
+from objects import globalstore
+
+class plugsearch(plugins.base):
+	def __init__(self): pass
+	def get_shortname(self): return 'ardour'
+	def get_name(self): return 'Ardour'
+	def is_dawvert_plugin(self): return 'externalsearch'
+	def get_prop(self, in_dict): in_dict['supported_os'] = ['win', 'lin']
+	def import_plugins(self):
+		searchpath = None
+		if globalstore.os_platform == 'win': searchpath = os.path.join(globalstore.home_folder, "AppData", "Local", "Ardour8", "cache", "vst")
+		if globalstore.os_platform == 'lin': searchpath = os.path.join(globalstore.home_folder, ".cache", "ardour6", "vst")
+		if searchpath:
+			vst2count = 0
+			vst3count = 0
+			if os.path.exists(searchpath):
+				vstcachelist = os.listdir(searchpath)
+				for vstcache in vstcachelist:
+					vstxmlfile = vstcache
+					vstxmlpath = os.path.join(searchpath, vstxmlfile)
+					vstxmlext = Path(vstxmlfile).suffix
+					vstxmldata = ET.parse(vstxmlpath)
+					vstxmlroot = vstxmldata.getroot()
+
+					if vstxmlext == '.v2i':
+						x_pluginfo = vstxmlroot.findall('VST2Info')[0]
+						vst_arch = vstxmlroot.get('arch')
+						vst_category = x_pluginfo.get('category')
+
+						if vst_arch == 'x86_64' and exists(vstxmlroot.get('binary')):
+							vst2data_fourid = int(x_pluginfo.get('id'))
+							if vst2data_fourid < 0: vst2data_fourid = int(x_pluginfo.get('id')) + 2**32
+
+							if vst_category == 'Instrument': vst2data_type = 'synth'
+							elif vst_category == 'Effect': vst2data_type = 'effect'
+							else: vst2data_type = None
+
+							if vst2data_type:
+								with globalstore.extplug.add('vst2', globalstore.os_platform) as pluginfo_obj: 
+									pluginfo_obj.id = vst2data_fourid
+									pluginfo_obj.name = x_pluginfo.get('name')
+									pluginfo_obj.creator = x_pluginfo.get('creator')
+									pluginfo_obj.path_64bit = vstxmlroot.get('binary')
+									pluginfo_obj.type = vst2data_type
+									pluginfo_obj.version = x_pluginfo.get('version')
+									pluginfo_obj.audio_num_inputs = x_pluginfo.get('n_inputs')
+									pluginfo_obj.audio_num_outputs = x_pluginfo.get('n_outputs')
+									pluginfo_obj.midi_num_inputs = x_pluginfo.get('n_midi_inputs')
+									pluginfo_obj.midi_num_outputs = x_pluginfo.get('n_midi_outputs')
+									vst2count += 1
+
+					if vstxmlext == '.v3i':
+						VST3Info = vstxmlroot.findall('VST3Info')[0]
+						vst3data_id = VST3Info.get('uid')
+						if exists(vstxmlroot.get('bundle')):
+							with globalstore.extplug.add('vst3', globalstore.os_platform) as pluginfo_obj: 
+								pluginfo_obj.id = vst3data_id
+								pluginfo_obj.path_64bit = vstxmlroot.get('bundle')
+								pluginfo_obj.name = VST3Info.get('name')
+								pluginfo_obj.creator = VST3Info.get('vendor')
+								pluginfo_obj.category = VST3Info.get('category')
+								pluginfo_obj.version = VST3Info.get('version')
+								pluginfo_obj.sdk_version = VST3Info.get('sdk-version')
+								pluginfo_obj.url = VST3Info.get('url')
+								pluginfo_obj.email = VST3Info.get('email')
+								pluginfo_obj.audio_num_inputs = VST3Info.get('n_inputs')
+								pluginfo_obj.audio_num_outputs = VST3Info.get('n_outputs')
+								pluginfo_obj.midi_num_inputs = VST3Info.get('n_midi_inputs')
+								pluginfo_obj.midi_num_outputs = VST3Info.get('n_midi_outputs')
+								vst3count += 1
+				print('[ardour] VST2: '+str(vst2count)+', VST3: '+str(vst3count))
+
+		else:
+			return False
---
+++ plugins/externalsearch/muse.py
@@ -0,0 +1,68 @@
+# SPDX-FileCopyrightText: 2024 SatyrDiamond
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import os
+import plugins
+from os.path import exists
+from pathlib import Path
+import xml.etree.ElementTree as ET
+from objects import globalstore
+
+def muse_getvalue(fallback, xmldata, name):
+		outval = xmldata.findall(name)
+		if outval == []: return fallback
+		else: return outval[0].text
+
+class plugsearch(plugins.base):
+	def __init__(self): pass
+	def get_shortname(self): return 'muse'
+	def get_name(self): return 'MusE'
+	def is_dawvert_plugin(self): return 'externalsearch'
+	def get_prop(self, in_dict): in_dict['supported_os'] = ['lin']
+	def import_plugins(self):
+
+		l_path_muse = os.path.join(globalstore.home_folder,".cache", "MusE", "MusE", "scanner")
+		vst2count = 0
+		ladspacount = 0
+
+		muse_g_path_vst = l_path_muse+'/linux_vst_plugins.scan'
+		muse_g_path_ladspa = l_path_muse+'/ladspa_plugins.scan'
+		muse_g_path_dssi = l_path_muse+'/dssi_plugins.scan'
+
+		if os.path.exists(muse_g_path_vst):
+			path_vst_linux = muse_g_path_vst
+			vstxmldata = ET.parse(path_vst_linux)
+			vstxmlroot = vstxmldata.getroot()
+			for x_vst_plug_cache in vstxmlroot:
+				muse_file = x_vst_plug_cache.get('file')
+				if os.path.exists(muse_file):
+					with globalstore.extplug.add('vst2', globalstore.os_platform) as pluginfo_obj: 
+						pluginfo_obj.id = muse_getvalue(None, x_vst_plug_cache, 'uniqueID')
+						pluginfo_obj.name = muse_getvalue(None, x_vst_plug_cache, 'name')
+						pluginfo_obj.creator = muse_getvalue(None, x_vst_plug_cache, 'maker')
+						pluginfo_obj.audio_num_inputs = muse_getvalue(0, x_vst_plug_cache, 'inports')
+						pluginfo_obj.audio_num_outputs = muse_getvalue(0, x_vst_plug_cache, 'outports')
+						pluginfo_obj.num_params = muse_getvalue(0, x_vst_plug_cache, 'ctlInports')
+						vst2count += 1
+
+		if os.path.exists(muse_g_path_ladspa):
+			path_ladspa_linux = muse_g_path_ladspa
+			ladspaxmldata = ET.parse(path_ladspa_linux)
+			ladspaxmlroot = ladspaxmldata.getroot()
+			for x_ladspa_plug_cache in ladspaxmlroot:
+				muse_file = x_ladspa_plug_cache.get('file')
+				muse_label = x_ladspa_plug_cache.get('label')
+				if os.path.exists(muse_file):
+					with globalstore.extplug.add('ladspa', globalstore.os_platform) as pluginfo_obj: 
+						pluginfo_obj.id = muse_getvalue(None, x_ladspa_plug_cache, 'uniqueID')
+						pluginfo_obj.name = muse_getvalue(None, x_ladspa_plug_cache, 'name')
+						pluginfo_obj.inside_id = muse_label
+						pluginfo_obj.creator = muse_getvalue(None, x_ladspa_plug_cache, 'maker')
+						pluginfo_obj.audio_num_inputs = muse_getvalue(0, x_ladspa_plug_cache, 'inports')
+						pluginfo_obj.audio_num_outputs = muse_getvalue(0, x_ladspa_plug_cache, 'outports')
+						pluginfo_obj.num_params = muse_getvalue(0, x_ladspa_plug_cache, 'ctlInports')
+						pluginfo_obj.num_params_out = muse_getvalue(0, x_ladspa_plug_cache, 'ctlOutports')
+						pluginfo_obj.path_unix = muse_file
+						ladspacount += 1
+
+		print('[muse] VST2: '+str(vst2count)+', LADSPA: '+str(ladspacount))

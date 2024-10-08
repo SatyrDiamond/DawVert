47af18d918bd850b6edb1f4257cdba4150e82499
Satyrdiamond <satyrdiamond@gmail.com>
2024-10-08 16:56:46 -0400

    fix priority

--- plugins/plugconv/ableton__v_universal.py
+++ plugins/plugconv/ableton__v_universal.py
@@ -12,7 +12,7 @@ delay_steps = [1,2,3,4,5,6,8,16]
 class plugconv(plugins.base):
 	def __init__(self): pass
 	def is_dawvert_plugin(self): return 'plugconv'
-	def get_priority(self): return -100
+	def get_priority(self): return 100
 	def get_prop(self, in_dict): 
 		in_dict['in_plugins'] = [['universal', None, None]]
 		in_dict['in_daws'] = []

e1f1bd8c8c1c4c7bf64454f8044b1d61d996dd07
Satyrdiamond <satyrdiamond@gmail.com>
2024-10-12 18:41:08 -0400

    move external_make_compat to plugin.py + rename extplug

--- plugins/extplug/foss_castello_reverb.py
+++ plugins/extplug/foss_castello_reverb.py
@@ -2,7 +2,6 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import plugins
-import lxml.etree as ET
 from objects import globalstore
 from functions import data_xml
 from functions_plugin_ext import data_nullbytegroup

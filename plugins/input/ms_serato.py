cd9982b2ae48db0dad49aafa9b90206446087bbb
Satyrdiamond <satyrdiamond@satyrdiamond.org>
2024-10-25 07:56:29 -0400

    add audio event for MultipleScened + dawproject audio stretch fix

--- plugins/input/ms_serato.py
+++ plugins/input/ms_serato.py
@@ -62,6 +62,7 @@ class input_serato(plugins.base):
 	def get_prop(self, in_dict): 
 		in_dict['track_lanes'] = True
 		in_dict['file_ext'] = ['ssp']
+		in_dict['audio_stretch'] = ['rate']
 	def supported_autodetect(self): return False
 	def parse(self, convproj_obj, input_file, dv_config):
 		from objects.file_proj import proj_serato

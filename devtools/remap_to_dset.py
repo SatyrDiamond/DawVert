4c43e75a4057be54c6febfbcfd00135a072da8c7
Satyrdiamond <satyrdiamond@satyrdiamond.org>
2024-11-17 08:14:40 -0500

    update devtool

--- devtools/remap_to_dset.py
+++ devtools/remap_to_dset.py
@@ -94,44 +94,44 @@ dn = 'magical8bitplug2'
 
 infile = "..\\data_ext\\remap\\%s.csv" % dn
 
-pmap_obj = paramremap(infile)
-
-#print(pmap_obj.cvpj)
-
 out_dset = dataset.dataset(None)
 dataset_cat = out_dset.category_add('plugin')
-dataset_object = dataset_cat.objects.create("main")
 
-for n, x in enumerate(pmap_obj.cvpj):
-	param_obj = dataset_object.params.create(x['paramid'])
-	param_obj.type = 'float'
-	param_obj.defv = x['def']
-	param_obj.min = x['min']
-	param_obj.max = x['max']
-	param_obj.name = x['visname']
-	param_obj.extplug_paramid = x['extid']
-	if pmap_obj.usedparamnames['vst3']:
-		if not param_obj.name: param_obj.name = pmap_obj.vst3[n]['visname']
+def create_object(infile, objname):
+	pmap_obj = paramremap(infile)
+	dataset_object = dataset_cat.objects.create(objname)
+	
+	for n, x in enumerate(pmap_obj.cvpj):
+		param_obj = dataset_object.params.create(x['paramid'])
+		param_obj.type = 'float'
+		param_obj.defv = x['def']
+		param_obj.min = x['min']
+		param_obj.max = x['max']
+		param_obj.name = x['visname']
+		param_obj.extplug_paramid = x['extid']
+		if pmap_obj.usedparamnames['vst3']:
+			if not param_obj.name: param_obj.name = pmap_obj.vst3[n]['visname']
+	
+			paramext = param_obj.add_extplug_assoc('vst3')
+			if pmap_obj.vst3[n]['visname']:
+				if pmap_obj.vst3[n]['visname'] != param_obj.name:
+					paramext.name = pmap_obj.vst3[n]['visname']
+			paramext.num = int(pmap_obj.vst3[n]['paramnum'])
+			if paramext.name and param_obj.name: 
+				if paramext.name == param_obj.name[0:24]:
+					paramext.name = None
+	
+		if pmap_obj.usedparamnames['vst2']:
+			if not param_obj.name: param_obj.name = pmap_obj.vst2[n]['visname']
+	
+			paramext = param_obj.add_extplug_assoc('vst2')
+			if pmap_obj.vst2[n]['visname']:
+				if pmap_obj.vst2[n]['visname'] != param_obj.name:
+					paramext.name = pmap_obj.vst2[n]['visname']
+			paramext.num = int(pmap_obj.vst2[n]['paramnum'])
+			if paramext.name and param_obj.name: 
+				if paramext.name == param_obj.name[0:16]:
+					paramext.name = None
 
-		paramext = param_obj.add_extplug_assoc('vst3')
-		if pmap_obj.vst3[n]['visname']:
-			if pmap_obj.vst3[n]['visname'] != param_obj.name:
-				paramext.name = pmap_obj.vst3[n]['visname']
-		paramext.num = int(pmap_obj.vst3[n]['paramnum'])
-		if paramext.name and param_obj.name: 
-			if paramext.name == param_obj.name[0:24]:
-				paramext.name = None
-
-	if pmap_obj.usedparamnames['vst2']:
-		if not param_obj.name: param_obj.name = pmap_obj.vst2[n]['visname']
-
-		paramext = param_obj.add_extplug_assoc('vst2')
-		if pmap_obj.vst2[n]['visname']:
-			if pmap_obj.vst2[n]['visname'] != param_obj.name:
-				paramext.name = pmap_obj.vst2[n]['visname']
-		paramext.num = int(pmap_obj.vst2[n]['paramnum'])
-		if paramext.name and param_obj.name: 
-			if paramext.name == param_obj.name[0:16]:
-				paramext.name = None
-
+create_object(infile, 'main')
 out_dset.write('%s.dset' % dn)

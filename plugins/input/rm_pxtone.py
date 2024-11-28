13d7379bfaee612c3e58105a3ee7539624dad692
Satyrdiamond <satyrdiamond@satyrdiamond.org>
2024-11-28 12:01:52 -0500

    flmobile + ai to nn

--- plugins/input/rm_pxtone.py
+++ plugins/input/rm_pxtone.py
@@ -101,7 +101,8 @@ class input_pxtone(plugins.base):
 			cvpj_instid = 'ptcop_'+str(voicenum)
 			inst_obj = convproj_obj.instrument__add(cvpj_instid)
 			inst_obj.visual.name = voice_obj.name
-			#inst_obj.visual.color.set_float([0.14, 0.00, 0.29])
+			inst_obj.visual.color.set_float([0.14, 0.00, 0.29])
+			inst_obj.visual.color.priority = -1
 
 			cvpj_instvol = 1.0
 

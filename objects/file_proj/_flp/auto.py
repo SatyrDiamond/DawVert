d6fef020c16e3978ebcfdc8021bac22ba2e42600
Satyrdiamond <satyrdiamond@satyrdiamond.org>
2024-12-04 23:14:00 -0500

    fix adpcm + flp auto tension

--- objects/file_proj/_flp/auto.py
+++ objects/file_proj/_flp/auto.py
@@ -308,6 +308,7 @@ class flp_autopoint:
 		self.selected = event_bio.uint8()
 		self.t_sign = event_bio.uint8()
 
+
 class flp_autopoints:
 	def __init__(self):
 		pass
@@ -321,6 +322,13 @@ class flp_autopoints:
 		_unk4 = event_bio.uint32() # 768
 		numpoints = event_bio.uint32() # 0
 		self.points = [flp_autopoint(event_bio) for x in range(numpoints)]
+
+	def __iter__(self):
+		tensions = [x.tension for x in self.points[1:]]
+		for n, x in enumerate(tensions):
+			yield self.points[n], x
+
+
 
 class flp_remotecontrol:
 	def __init__(self):

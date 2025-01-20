dd2c39578f8d99f75e9226c6e34be4fe93352b22
Satyrdiamond <satyrdiamond@satyrdiamond.org>
2025-01-20 01:39:41 -0500

    change plugts from json to xml

--- objects/datastorage/plugts.py
+++ objects/datastorage/plugts.py
@@ -4,6 +4,12 @@
 import os
 import chardet
 from objects.valobjs import triplestr
+import xml.etree.ElementTree as ET
+
+def fixvalue(v, t):
+	if t == 'float': return float(v)
+	elif t == 'int': return int(v)
+	elif t == 'bool': return bool(int(v))
 
 class pltr_pipe:
 	def __init__(self):
@@ -14,6 +20,47 @@ class pltr_pipe:
 		self.valuetype = ''
 		self.name = None
 
+	def read_xml(self, xml_data, isout):
+		self.type = xml_data.tag
+		self.valuetype = xml_data.get('type')
+		if self.type == 'param':
+			if not isout:
+				self.v_from = xml_data.get('to')
+				self.v_to = xml_data.get('from')
+				if not self.v_from: self.v_from = self.v_to
+			else:
+				self.v_to = xml_data.get('to')
+				self.v_from = xml_data.get('from')
+				if not self.v_from: self.v_from = self.v_to
+		if self.type == 'wet':
+			if not isout:
+				self.v_to = xml_data.get('from')
+			else:
+				self.v_to = xml_data.get('from')
+		if xml_data.text: self.value = fixvalue(xml_data.text, self.valuetype)
+		name = xml_data.get('name')
+		if name: self.name = name
+
+	def write_xml(self, xml_data, isout):
+		transform_xml = ET.SubElement(xml_data, self.type)
+		if self.type == 'param':
+			if not isout:
+				if self.v_from != self.v_to: transform_xml.set('to', str(self.v_from))
+				transform_xml.set('from', str(self.v_to))
+			else:
+				transform_xml.set('to', str(self.v_to))
+				if self.v_from != self.v_to: transform_xml.set('from', str(self.v_from))
+		if self.type == 'wet':
+			if not isout:
+				transform_xml.set('from', str(self.v_to))
+			else:
+				transform_xml.set('from', str(self.v_to)) 
+		transform_xml.set('type', str(self.valuetype))
+		if self.value == False: transform_xml.text = str(0)
+		elif self.value == True: transform_xml.text = str(1)
+		else: transform_xml.text = str(self.value)
+		if self.name: transform_xml.set('name', str(self.name))
+
 class pltr_ts:
 	def __init__(self):
 		self.in_type = triplestr()
@@ -22,12 +69,68 @@ class pltr_ts:
 		self.out_data = {}
 		self.proc = []
 
+	def read_xml(self, xml_data):
+		for x_part in xml_data:
+			if x_part.tag == 'input':
+				self.in_type.set_str(x_part.get('plugtype'))
+				for x_inpart in x_part:
+					pipe_obj = pltr_pipe()
+					pipe_obj.read_xml(x_inpart, False)
+					self.in_data[pipe_obj.v_from] = pipe_obj
+				
+			if x_part.tag == 'proc':
+				for x_inpart in x_part:
+					if x_inpart.tag == 'calc':
+						name = x_inpart.get('name')
+						calctype = x_inpart.get('type')
+						self.proc.append(['calc', name, calctype]+[(float(x) if '.' in x else int(x)) for x in x_inpart.text.split(';')])
+						
+			if x_part.tag == 'output':
+				self.out_type.set_str(x_part.get('plugtype'))
+				for x_inpart in x_part:
+					pipe_obj = pltr_pipe()
+					pipe_obj.read_xml(x_inpart, True)
+					self.out_data[pipe_obj.v_from] = pipe_obj
+
+	def write_xml(self, xml_data, name):
+		transform_xml = ET.SubElement(xml_data, "transform")
+		transform_xml.set('name', name)
+
+		input_xml = ET.SubElement(transform_xml, "input")
+		input_xml.set('plugtype', str(self.in_type))
+		for k, v in self.in_data.items():
+			v.write_xml(input_xml, False)
+
+		proc_xml = ET.SubElement(transform_xml, "proc")
+		for v in self.proc:
+			if v[0]=='calc':
+				calc_xml = ET.SubElement(proc_xml, "calc")
+				calc_xml.set('name', v[1])
+				calc_xml.set('type', v[2])
+				calc_xml.text = ';'.join([str(x) for x in v[3:]])
+
+		output_xml = ET.SubElement(transform_xml, "output")
+		output_xml.set('plugtype', str(self.out_type))
+		for k, v in self.out_data.items():
+			v.write_xml(output_xml, True)
+
+
 class plugts:
 	def __init__(self, filename):
 		self.transforms = {}
-		if filename: self.load_text(filename)
+		if filename: self.load_xml(filename)
 
-	def load_text(self, filename):
+	def load_xml(self, filename):
+		parser = ET.XMLParser(encoding='utf-8')
+		xml_data = ET.parse(filename, parser)
+		xml_plugts = xml_data.getroot()
+		for x_part in xml_plugts:
+			if x_part.tag == 'transform':
+				ts_obj = pltr_ts()
+				ts_obj.read_xml(x_part)
+				self.transforms[x_part.get('name')] = ts_obj
+
+	def load_legacy(self, filename):
 		if os.path.exists(filename):
 			with open(filename, 'r') as ptsfile:
 				inside_group = None
@@ -73,7 +176,7 @@ class plugts:
 									pipe_obj.valuetype = 'float'
 								if '&' in path1: 
 									path1, valueval = path1.split('&')
-									pipe_obj.value = int(valueval)!=0
+									pipe_obj.value = valueval=='True'
 									pipe_obj.valuetype = 'bool'
 
 								if '#' in path1: path1 = path1.split('#')[0]
@@ -103,6 +206,12 @@ class plugts:
 
 							#print(inside_group, linesplit)
 
+	def save_xml(self, filename):
+		xml_proj = ET.Element("plugtransform")
 
-		#for x,v in self.transforms.items():
-		#	print(x, v.in_type, v.out_type)
+		for name, transform in self.transforms.items():
+			transform.write_xml(xml_proj, name)
+
+		outfile = ET.ElementTree(xml_proj)
+		ET.indent(outfile, space="    ", level=0)
+		outfile.write(filename)

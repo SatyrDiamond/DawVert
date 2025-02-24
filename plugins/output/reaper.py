5f20965d0fe596d69210fe649c9bf6b689829906
Satyrdiamond <satyrdiamond@satyrdiamond.org>
2025-02-24 02:14:07 -0500

    reaper mute

--- plugins/output/reaper.py
+++ plugins/output/reaper.py
@@ -467,6 +467,8 @@ class output_reaper(plugins.base):
 				rpp_track_obj.peakcol.set(cvpj_color_to_reaper_color(track_obj.visual.color))
 			rpp_track_obj.volpan['vol'] = track_obj.params.get('vol', 1.0).value
 			rpp_track_obj.volpan['pan'] = track_obj.params.get('pan', 0).value
+			rpp_track_obj.mutesolo['mute'] = int(not track_obj.params.get('enabled', 1).value)
+
 			pan_mode = track_obj.datavals.get('pan_mode', '')
 			if pan_mode == 'mono': rpp_track_obj.panmode.set(3)
 			if pan_mode == 'stereo': rpp_track_obj.panmode.set(5)

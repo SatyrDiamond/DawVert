2620d715d6ded1f9b59d5c94ff2ecfa26544da70
Satyrdiamond <satyrdiamond@satyrdiamond.org>
2025-03-20 00:51:26 -0400

    acid

--- plugins/input_y2k/r_sf_acid.py
+++ plugins/input_y2k/r_sf_acid.py
@@ -15,7 +15,7 @@ class input_piyopiyo(plugins.base):
 		return 'sf_acid'
 	
 	def get_name(self):
-		return 'ACID'
+		return 'Sonic Foundry ACID'
 	
 	def get_priority(self):
 		return 0
@@ -23,12 +23,14 @@ class input_piyopiyo(plugins.base):
 	def get_prop(self, in_dict): 
 		in_dict['projtype'] = 'r'
 		in_dict['placement_loop'] = ['loop', 'loop_off', 'loop_adv', 'loop_adv_off']
+		in_dict['fxtype'] = 'groupreturn'
 
 	def parse(self, convproj_obj, dawvert_intent):
 		from objects import colors
 		from objects.file_proj_past import sony_acid as sony_acid
 
 		convproj_obj.type = 'r'
+		convproj_obj.fxtype = 'groupreturn'
 		convproj_obj.set_timings(768, False)
 
 		project_obj = sony_acid.sony_acid_file()
@@ -41,13 +43,22 @@ class input_piyopiyo(plugins.base):
 
 		samplefolder = dawvert_intent.path_samples['extracted']
 
+		for x in range(2):
+			returnid = 'return__'+str(x)
+			track_obj = convproj_obj.track_master.fx__return__add(returnid)
+			track_obj.visual.name = 'FX '+str(x+1)
+
 		for tracknum, track in enumerate(project_obj.tracks):
-			cvpj_trackid = 'track_'+str(tracknum+1)
+			cvpj_trackid = 'track_'+str(tracknum)
 			track_obj = convproj_obj.track__add(cvpj_trackid, 'audio', 1, False)
 			track_obj.visual.name = track.name
 			track_obj.visual.color.set_int(colordata.getcolornum(track.color))
 			track_obj.params.add('vol', track.vol, 'float')
 			track_obj.params.add('pan', track.pan, 'float')
+
+			for send in track.sends:
+				returnid = 'return__'+str(send.id)
+				track_obj.sends.add(returnid, 'send_%i_%i' % (tracknum, send.id), send.vol)
 
 			sample_tempo = track.stretch__tempo
 			sample_beats = track.num_beats
@@ -93,3 +104,24 @@ class input_piyopiyo(plugins.base):
 				sp_obj.stretch.preserve_pitch = True
 
 				sp_obj.pitch = trackpitch + region.pitch + notec
+
+				for env in region.envs:
+					autoloc = None
+					if env.type == 0: autoloc = ['track', cvpj_trackid, 'vol']
+					if env.type == 1: autoloc = ['track', cvpj_trackid, 'pan']
+					if env.type == 2: autoloc = ['send', 'send_%i_%i' % (tracknum, 0), 'amount']
+					if env.type == 3: autoloc = ['send', 'send_%i_%i' % (tracknum, 1), 'amount']
+					if autoloc:
+						autopl_obj = convproj_obj.automation.add_pl_points(autoloc, 'float')
+						autopl_obj.time.set_startend(region.start, region.end)
+						for point in env.points:
+							autopoint_obj = autopl_obj.data.add_point()
+							autopoint_obj.pos = point[0]
+							if point[0] == 2:
+								autopoint_obj.tension = 1
+							if point[0] == -2:
+								autopoint_obj.tension = -1
+							autopoint_obj.type = 'normal'
+							autopoint_obj.value = point[1]
+
+		convproj_obj.automation.set_persist_all(False)

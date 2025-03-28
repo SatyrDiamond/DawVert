8836bbdef2ce09df66ddab6943a11ee7a4604fd3
Satyrdiamond <satyrdiamond@satyrdiamond.org>
2025-03-28 03:52:25 -0400

    qtractor output

---
+++ plugins/output/qtractor.py
@@ -0,0 +1,150 @@
+# SPDX-FileCopyrightText: 2024 SatyrDiamond
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import plugins
+import json
+import uuid
+import os
+import shutil
+from objects import globalstore
+from functions import data_values
+from functions import xtramath
+
+def calcsec(val, ppq):
+	return int((val*50)*ppq)
+
+class output_bandlab(plugins.base):
+	def is_dawvert_plugin(self):
+		return 'output'
+	
+	def get_shortname(self):
+		return 'qtractor'
+	
+	def get_name(self):
+		return 'Qtractor'
+	
+	def gettype(self):
+		return 'r'
+	
+	def get_prop(self, in_dict): 
+		in_dict['audio_filetypes'] = ['wav']
+		in_dict['file_ext'] = 'qtr'
+		in_dict['auto_types'] = ['nopl_ticks']
+		in_dict['audio_stretch'] = ['rate']
+		in_dict['fxtype'] = 'groupreturn'
+		in_dict['projtype'] = 'r'
+		in_dict['time_seconds'] = True
+
+	def parse(self, convproj_obj, dawvert_intent):
+		from objects.file_proj import qtractor as proj_qtractor
+
+		convproj_obj.change_timings(1, True)
+		
+		project_obj = proj_qtractor.qtractor_project()
+
+		bpm = int(convproj_obj.params.get('bpm', 120).value)
+		project_obj.properties.tempo = bpm
+
+		ppq = 960
+
+		sampleref_filename = {}
+		sampleref_filepath = {}
+
+		for sampleref_id, sampleref_obj in convproj_obj.sampleref__iter():
+			uuiddata = str( data_values.bytes__to_uuid( sampleref_id.encode() ) )
+			filename = uuiddata+'.'+sampleref_obj.fileref.file.extension
+			sampleref_filename[sampleref_id] = filename
+			filepath = '/'.join(['.', 'Assets', 'Audio', filename])
+			sampleref_filepath[sampleref_id] = filepath
+			project_obj.files.audio_list[uuiddata] = filepath
+
+		if dawvert_intent.output_mode == 'file':
+			folder = dawvert_intent.output_folder
+			namet = dawvert_intent.output_visname
+			foldpath = os.path.join(folder, namet)
+			
+			os.makedirs(os.path.join(folder, namet, 'Assets'), exist_ok=True)
+			os.makedirs(os.path.join(folder, namet, 'Assets', 'Audio'), exist_ok=True)
+			os.makedirs(os.path.join(folder, namet, 'Assets', 'MIDI'), exist_ok=True)
+
+			for sampleref_id, sampleref_obj in convproj_obj.sampleref__iter():
+				a_in = sampleref_obj.fileref.get_path(None, False)
+				a_out = os.path.join(folder, namet, 'Assets', 'Audio', sampleref_filename[sampleref_id])
+				try:
+					sampleref_obj.copy_resample(None, a_out)
+				except:
+					pass
+
+		for trackid, track_obj in convproj_obj.track__iter():
+
+			tracknotes_midinames = []
+
+			if track_obj.type in ['instrument', 'audio']:
+				qt_track = proj_qtractor.qtractor_track(None)
+				if track_obj.type == 'instrument': qt_track.type = 'midi'
+
+				if track_obj.visual.name: qt_track.name = track_obj.visual.name
+				if track_obj.visual.color: 
+					color = track_obj.visual.color
+					qt_track.view.foreground_color = '#'+color.get_hex()
+					qt_track.view.background_color = '#'+(color*0.6).get_hex()
+
+				if track_obj.type == 'audio':
+					qt_track.type = 'audio'
+					for audiopl_obj in track_obj.placements.pl_audio:
+						qt_clip = proj_qtractor.qtractor_clip(None)
+						qt_clip.properties.start = calcsec(audiopl_obj.time.position_real, ppq)
+						qt_clip.properties.length = calcsec(audiopl_obj.time.duration_real, ppq)
+						qt_clip.properties.fade_in_type = 'InQuad'
+						qt_clip.properties.fade_out_type = 'OutQuad'
+						qt_clip.properties.fade_in = calcsec(audiopl_obj.fade_in.get_dur_seconds(bpm), ppq)
+						qt_clip.properties.fade_out = calcsec(audiopl_obj.fade_out.get_dur_seconds(bpm), ppq)
+						qt_clip.audioclip = proj_qtractor.qtractor_clip_audioclip(None)
+
+						sp_obj = audiopl_obj.sample
+						qt_clip.audioclip.filename = sampleref_filepath[sp_obj.sampleref]
+						qt_clip.audioclip.pitch_shift = pow(2, sp_obj.pitch/12)
+						qt_clip.audioclip.time_stretch = sp_obj.stretch.calc_real_size
+						qt_clip.properties.gain = sp_obj.vol
+						qt_clip.properties.panning = sp_obj.pan
+
+						name = audiopl_obj.visual.name if audiopl_obj.visual.name else ''
+						qt_clip.properties.name = name
+						qt_clip.name = name
+
+						qt_track.clips.append(qt_clip)
+
+				if track_obj.type == 'instrument':
+					qt_track.type = 'midi'
+					for notespl_obj in track_obj.placements.pl_notes:
+						qt_clip = proj_qtractor.qtractor_clip(None)
+						qt_clip.properties.start = calcsec(notespl_obj.time.position_real, ppq)
+						qt_clip.properties.length = calcsec(notespl_obj.time.duration_real, ppq)
+						qt_clip.properties.name = notespl_obj.visual.name
+						qt_clip.midiclip = proj_qtractor.qtractor_clip_midiclip(None)
+
+						notelist = notespl_obj.notelist
+						if notelist not in tracknotes_midinames:
+							tracknotes_midinames.append(notelist)
+							filename = 'track_%s_clip_%i' % (trackid, len(tracknotes_midinames)-1)
+							project_obj.files.midi_list[filename] = '/'.join(['.', 'Assets', 'MIDI', filename+'.mid'])
+							if dawvert_intent.output_mode == 'file':
+								notelist.midi_to(os.path.join(folder, namet, 'Assets', 'MIDI', filename+'.mid'), bpm)
+
+						notelist_id = 'track_%s_clip_%i' % (trackid, tracknotes_midinames.index(notelist))
+
+						filepath = '/'.join(['.', 'Assets', 'MIDI', filename+'.mid'])
+						qt_clip.midiclip.filename = filepath
+
+						name = notespl_obj.visual.name if notespl_obj.visual.name else ''
+						qt_clip.properties.name = name
+						qt_clip.name = name
+
+						qt_track.clips.append(qt_clip)
+
+				project_obj.tracks.append(qt_track)
+
+		if dawvert_intent.output_mode == 'file':
+			namet = dawvert_intent.output_visname
+			outfile = os.path.join(dawvert_intent.output_folder, namet, os.path.basename(dawvert_intent.output_file))
+			project_obj.write_to_file(outfile)

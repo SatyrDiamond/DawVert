6805aeb80af3c9c25c02afbdd8bc7ff5f4c06be6
Satyrdiamond <satyrdiamond@satyrdiamond.org>
2025-04-05 19:14:46 -0400

    bandlab m4a

--- plugins/input/r_bandlab.py
+++ plugins/input/r_bandlab.py
@@ -25,7 +25,7 @@ class input_bandlab(plugins.base):
 		return 0
 	
 	def get_prop(self, in_dict): 
-		in_dict['audio_filetypes'] = ['wav', 'mp3', 'flac']
+		in_dict['audio_filetypes'] = ['wav', 'mp3', 'flac', 'm4a']
 		in_dict['auto_types'] = ['nopl_points']
 		in_dict['audio_stretch'] = ['rate']
 		in_dict['projtype'] = 'r'

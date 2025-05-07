bc74029136b4c2a347a6e14fa7a3189b3f9f112a
Satyrdiamond <satyrdiamond@satyrdiamond.org>
2025-05-07 15:58:30 -0400

    remove soundbridge

--- objects/file_proj/_soundbridge/mathalgo.py
+++
@@ -1,7 +0,0 @@
-# SPDX-FileCopyrightText: 2024 SatyrDiamond
-# SPDX-License-Identifier: GPL-3.0-or-later
-import math
-
-def freq__from_one(in_freq): return 30 * ((2/3)*1000)**in_freq
-
-def freq__to_one(in_freq): return (math.log(max(30, in_freq)/30) / math.log((2/3)*1000))

e425efa04645ec45a17d565febbee863ef5bd62b
Satyrdiamond <satyrdiamond@satyrdiamond.org>
2025-05-27 22:23:03 -0400

    move to devtools

--- devtools/dawvert_adpcm_dec.py
+++ devtools/dawvert_adpcm_dec.py
@@ -3,6 +3,7 @@
 # SPDX-FileCopyrightText: 2024 SatyrDiamond
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+sys.path.append('../')
 import argparse
 from plugins import base as dv_plugins
 from objects import audio_data
@@ -11,19 +12,17 @@ dv_plugins.load_plugindir('audiocodecs', '')
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", default=None)
-parser.add_argument("-ot", default=None)
+parser.add_argument("-it", default=None)
 parser.add_argument("-o", default=None)
 args = parser.parse_args()
 
 in_file = args.i
 out_file = args.o
-out_format = args.ot
+in_format = args.it
 
 file_obj = open(in_file, 'rb')
-ofile_obj = open(out_file, 'wb')
 
 audio_obj = audio_data.audio_obj()
-audio_obj.set_codec('int16')
-audio_obj.pcm_from_bytes(file_obj.read())
-ofile_obj.write( audio_obj.encode_to_codec(out_format) )
+audio_obj.decode_from_codec(in_format, file_obj.read())
+audio_obj.to_file_wav(out_file)
 

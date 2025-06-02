df90130d1b7f61c385ae38042572899e6e0de0df
Satyrdiamond <satyrdiamond@satyrdiamond.org>
2025-06-02 08:23:17 -0400

    pstr

--- plugins/plugconv/to_univ__caustic.py
+++ plugins/plugconv/to_univ__caustic.py
@@ -19,33 +19,7 @@ class plugconv(plugins.base):
 		in_dict['out_daws'] = []
 
 	def convert(self, convproj_obj, plugin_obj, pluginid, dawvert_intent):
-		if plugin_obj.type.check_wildmatch('native', 'caustic', 'mixer_eq'):
-			bass = plugin_obj.params.get('bass', 0).value
-			mid = plugin_obj.params.get('mid', 0).value
-			high = plugin_obj.params.get('high', 0).value
-
-			plugin_obj.replace('universal', 'eq', 'bands')
-			filter_obj, filter_id = plugin_obj.eq_add()
-			filter_obj.freq = 200
-			filter_obj.type.set('low_shelf', None)
-			filter_obj.gain = (bass-1)*6
-			convproj_obj.automation.calc(['plugin', pluginid, 'bass'], 'addmul', -1, 6, 0, 0)
-			convproj_obj.automation.move(['plugin', pluginid, 'bass'], ['n_filter', pluginid, filter_id, 'gain'])
-
-			filter_obj, filter_id = plugin_obj.eq_add()
-			filter_obj.freq = 1000
-			filter_obj.type.set('peak', None)
-			filter_obj.gain = (mid-1)*6
-			convproj_obj.automation.calc(['plugin', pluginid, 'mid'], 'addmul', -1, 6, 0, 0)
-			convproj_obj.automation.move(['plugin', pluginid, 'mid'], ['n_filter', pluginid, filter_id, 'gain'])
-
-			filter_obj, filter_id = plugin_obj.eq_add()
-			filter_obj.freq = 8000
-			filter_obj.type.set('high_shelf', None)
-			filter_obj.gain = (high-1)*6
-			convproj_obj.automation.calc(['plugin', pluginid, 'high'], 'addmul', -1, 6, 0, 0)
-			convproj_obj.automation.move(['plugin', pluginid, 'high'], ['n_filter', pluginid, filter_id, 'gain'])
-			return True
+		return False
 
 		#if plugin_obj.type.check_wildmatch('native', 'caustic', 'master_delay'):
 		#	delay_obj = fx_delay.fx_delay()

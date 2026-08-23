1dc942445ba48cfee851f4eb42c1f809b01c41a9
SatyrDiamond <satyrdiamond@gmail.com>
2026-08-23 08:43:04 -0400

    piyopiyo

--- objects/file_proj_past/piyopiyo.py
+++ objects/file_proj_past/piyopiyo.py
@@ -21,7 +21,12 @@ class piyopiyo_melo_track:
 
 class piyopiyo_song:
 	def __init__(self):
-		pass
+		self.loopend = 0
+		self.loopstart = 0
+		self.musicwait = 1
+		self.notes_data = []
+		self.perc_volume = 0
+		self.tracks = []
 
 	def load_from_file(self, input_file):
 		ebrw_readstr = easybinrw.binread()
@@ -34,15 +39,15 @@ class piyopiyo_song:
 
 		ptr__tracks = ebrw_readstr.int_u32()
 		self.musicwait = ebrw_readstr.int_u32()
-		self.loopstart = ebrw_readstr.int_u32()
-		self.loopend = ebrw_readstr.int_u32()
-		self.records_per_track = ebrw_readstr.int_u32()
 		logger_projparse.info("piyopiyo: MusicWait: " + str(self.musicwait))
+		self.loopstart = ebrw_readstr.int_u32()
 		logger_projparse.info("piyopiyo: Loop Beginning: " + str(self.loopstart))
+		self.loopend = ebrw_readstr.int_u32()
 		logger_projparse.info("piyopiyo: Loop End: " + str(self.loopend))
-		logger_projparse.info("piyopiyo: Records Per Track: " + str(self.records_per_track))
+		records_per_track = ebrw_readstr.int_u32()
+		logger_projparse.info("piyopiyo: Records Per Track: " + str(records_per_track))
 		self.tracks = [piyopiyo_melo_track(ebrw_readstr) for _ in range(3)]
 		self.perc_volume = ebrw_readstr.int_u32()
 		ebrw_readstr.seek(ptr__tracks)
-		self.notes_data = [ [[ebrw_readstr.flags_i24(), ebrw_readstr.int_u8()] for _ in range(self.records_per_track)] for _ in range(4)]
+		self.notes_data = [ [[ebrw_readstr.flags_i24(), ebrw_readstr.int_u8()] for _ in range(records_per_track)] for _ in range(4)]
 		return True
